1e0cbe67fa0bd81d1a8a5fb21fdecac7cf3cba52
adityaapte <aditya.apte@gmail.com>
2012-05-31 16:02:00 -0400

    Script to QA and overwrite planC

---
+++ CERR_core/Utilities/quality_assure_planC.m
@@ -0,0 +1,113 @@
+function quality_assure_planC
+%function quality_assure_planC
+%
+% This function quality assures planC.
+%
+%APA, 04/07/2011
+
+global planC stateS
+indexS = planC{end};
+
+% Quality Assurance
+bug_found = 0;
+
+% Detect and Fix incorrect rasterSegments
+if isfield(planC{indexS.header}(1), 'lastSavedInVer')
+    lastSavedInVer = planC{indexS.header}(1).lastSavedInVer;
+else
+    lastSavedInVer = '';
+end
+
+if ~isempty(str2double(lastSavedInVer)) && (isempty(lastSavedInVer) || str2double(lastSavedInVer) < 4.1) && ~isempty(planC{indexS.structures})
+    reRasterAndUniformize;
+    bug_found = 1;
+end
+
+
+%Check for mesh representation and load meshes into memory
+currDir = cd;
+meshDir = fileparts(which('libMeshContour.dll'));
+cd(meshDir)
+for strNum = 1:length(planC{indexS.structures})
+    if isfield(planC{indexS.structures}(strNum),'meshRep') && ~isempty(planC{indexS.structures}(strNum).meshRep) && planC{indexS.structures}(strNum).meshRep
+        try
+            calllib('libMeshContour','loadSurface',planC{indexS.structures}(strNum).strUID,planC{indexS.structures}(strNum).meshS)
+        catch
+            planC{indexS.structures}(strNum).meshRep    = 0;
+            planC{indexS.structures}(strNum).meshS      = [];
+        end
+    end
+end
+cd(currDir)
+
+stateS.optS = CERROptions;
+
+%Check color assignment for displaying structures
+[assocScanV,relStrNumV] = getStructureAssociatedScan(1:length(planC{indexS.structures}),planC);
+for scanNum = 1:length(planC{indexS.scan})
+    scanIndV = find(assocScanV==scanNum);
+    for i = 1:length(scanIndV)
+        strNum = scanIndV(i);
+        colorNum = relStrNumV(strNum);
+        if isempty(planC{indexS.structures}(strNum).structureColor)
+            color = stateS.optS.colorOrder( mod(colorNum-1, size(stateS.optS.colorOrder,1))+1,:);
+            planC{indexS.structures}(strNum).structureColor = color;
+        end
+    end
+end
+
+%Check dose-grid
+for doseNum = 1:length(planC{indexS.dose})
+    if planC{indexS.dose}(doseNum).zValues(2) - planC{indexS.dose}(doseNum).zValues(1) < 0
+        planC{indexS.dose}(doseNum).zValues = flipud(planC{indexS.dose}(doseNum).zValues);
+        planC{indexS.dose}(doseNum).doseArray = flipdim(planC{indexS.dose}(doseNum).doseArray,3);
+    end
+end
+
+%Check whether uniformized data is in cellArray format.
+if ~isempty(planC{indexS.structureArray}) && iscell(planC{indexS.structureArray}(1).indicesArray)
+    planC = setUniformizedData(planC,planC{indexS.CERROptions});
+    indexS = planC{end};
+end
+
+if length(planC{indexS.structureArrayMore}) ~= length(planC{indexS.structureArray})
+    for saNum = 1:length(planC{indexS.structureArray})
+        if saNum == 1
+            planC{indexS.structureArrayMore} = struct('indicesArray', {[]},...
+                'bitsArray', {[]},...
+                'assocScanUID',{planC{indexS.structureArray}(saNum).assocScanUID},...
+                'structureSetUID', {planC{indexS.structureArray}(saNum).structureSetUID});
+            
+        else
+            planC{indexS.structureArrayMore}(saNum) = struct('indicesArray', {[]},...
+                'bitsArray', {[]},...
+                'assocScanUID',{planC{indexS.structureArray}(saNum).assocScanUID},...
+                'structureSetUID', {planC{indexS.structureArray}(saNum).structureSetUID});
+        end
+    end
+end
+
+%Check DSH Points for old CERR versions
+if ~isfield(planC{indexS.header},'CERRImportVersion') || (isfield(planC{indexS.header},'CERRImportVersion') && isempty(planC{indexS.header}.CERRImportVersion))
+    CERRImportVersion = '0';
+else
+    CERRImportVersion = planC{indexS.header}.CERRImportVersion;
+end
+
+if str2num(CERRImportVersion(1)) < 4
+    for structNum = 1:length(planC{indexS.structures})
+        if ~isempty(planC{indexS.structures}(structNum).DSHPoints)
+            planC = getDSHPoints(planC, stateS.optS, structNum);
+        end
+    end
+end
+
+% Overwrite the existing CERR file if a bug is found and fixed
+if ~isempty(stateS) && isfield(stateS.optS,'overwrite_CERR_File') && stateS.optS.overwrite_CERR_File == 1 && bug_found    
+    try
+        save_planC(planC, stateS.optS, 'passed', stateS.CERRFile); 
+    catch
+        disp('Could not overwrite the exisitng file. Please save manually')
+    end
+end
+
