6c1ee27bf94eef63d433dd5ee7551cf2346f002e
adityaapte <aditya.apte@gmail.com>
2012-06-26 14:00:08 -0400

    updated for doses with unequal grids and transMs

--- CERR_core/Utilities/createGammaDose.m
+++ CERR_core/Utilities/createGammaDose.m
@@ -12,25 +12,63 @@ function gammaM = createGammaDose(doseNum1,doseNum2,dosePercent,distAgreement)
 global planC stateS
 indexS = planC{end};
 
-% Get deltaX, deltaY, deltaZ
-[xDoseVals, yDoseVals, zDoseVals] = getDoseXYZVals(planC{indexS.dose}(doseNum1));
-deltaX = abs(xDoseVals(2) - xDoseVals(1));
-deltaY = abs(yDoseVals(2) - yDoseVals(1));
-deltaZ = abs(zDoseVals(2) - zDoseVals(1));
+% Prepare inputs for gamma calculation
+[newXgrid, newYgrid, newZgrid, doseArray1, doseArray2] = prepareDosesForGamma(doseNum1,doseNum2,1, planC);
+
+
+
+% % Get deltaX, deltaY, deltaZ
+% [xDoseVals, yDoseVals, zDoseVals] = getDoseXYZVals(planC{indexS.dose}(doseNum1));
+% deltaX = abs(xDoseVals(2) - xDoseVals(1));
+% deltaY = abs(yDoseVals(2) - yDoseVals(1));
+% deltaZ = abs(zDoseVals(2) - zDoseVals(1));
+
+deltaX = abs(newXgrid(2) - newXgrid(1));
+deltaY = abs(newYgrid(2) - newYgrid(1));
+deltaZ = abs(newZgrid(2) - newZgrid(1));
+
 
 doseAgreement = dosePercent*max(planC{indexS.dose}(doseNum1).doseArray(:))/100;
-gammaM = gammaDose3d(planC{indexS.dose}(doseNum1).doseArray, planC{indexS.dose}(doseNum2).doseArray, [deltaX deltaY deltaZ], doseAgreement, distAgreement);
+% gammaM = gammaDose3d(planC{indexS.dose}(doseNum1).doseArray, planC{indexS.dose}(doseNum2).doseArray, [deltaX deltaY deltaZ], doseAgreement, distAgreement);
+
+gammaM = gammaDose3d(doseArray1, doseArray2, [deltaX deltaY deltaZ], doseAgreement, distAgreement);
 
 newDoseNum = length(planC{indexS.dose}) + 1;
 
-planC{indexS.dose}(newDoseNum) = planC{indexS.dose}(doseNum1);
+% planC{indexS.dose}(newDoseNum) = planC{indexS.dose}(doseNum1);
+% 
+% planC{indexS.dose}(newDoseNum).doseArray = gammaM;
+% 
+% planC{indexS.dose}(newDoseNum).doseUID = createUID('dose');
+% 
+% planC{indexS.dose}(newDoseNum).fractionGroupID = 'Gamma 3D';
 
+
+%Remove old caching info.
+planC{indexS.dose}(newDoseNum).cachedMask = [];
+planC{indexS.dose}(newDoseNum).cachedColor = [];
+planC{indexS.dose}(newDoseNum).cachedTime = [];
+
+%Set coordinates.
+planC{indexS.dose}(newDoseNum).sizeOfDimension1 = length(newXgrid);
+planC{indexS.dose}(newDoseNum).sizeOfDimension2 = length(newYgrid);
+planC{indexS.dose}(newDoseNum).sizeOfDimension3 = length(newZgrid);
+planC{indexS.dose}(newDoseNum).horizontalGridInterval = newXgrid(2)-newXgrid(1);
+planC{indexS.dose}(newDoseNum).verticalGridInterval = newYgrid(2)-newYgrid(1);
+planC{indexS.dose}(newDoseNum).depthGridInterval = newZgrid(2)-newZgrid(1);
+planC{indexS.dose}(newDoseNum).coord1OFFirstPoint = newXgrid(1);
+planC{indexS.dose}(newDoseNum).coord2OFFirstPoint = newYgrid(1);
+planC{indexS.dose}(newDoseNum).coord3OfFirstPoint = newZgrid(1);
+planC{indexS.dose}(newDoseNum).zValues = newZgrid;
+planC{indexS.dose}(newDoseNum).doseUnits = 'Gy';
 planC{indexS.dose}(newDoseNum).doseArray = gammaM;
-
 planC{indexS.dose}(newDoseNum).doseUID = createUID('dose');
-
 planC{indexS.dose}(newDoseNum).fractionGroupID = 'Gamma 3D';
 
-stateS.doseSet = newDoseNum;
-stateS.doseChanged = 1;
-CERRRefresh
+%Switch to new dose
+sliceCallBack('selectDose', num2str(newDoseNum));
+
+
+% stateS.doseSet = newDoseNum;
+% stateS.doseChanged = 1;
+% CERRRefresh
