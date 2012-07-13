788116f4113a2ace3e1740a8f0546164b239f588
adityaapte <aditya.apte@gmail.com>
2012-07-13 15:34:08 -0400

    Completed coding this function

--- CERR_core/ImageRegistration/register_scans.m
+++ CERR_core/ImageRegistration/register_scans.m
@@ -5,25 +5,38 @@ function basePlanC = register_scans(basePlanC, movPlanC, baseScanNum, movScanNum
 
 indexBaseS = basePlanC{end};
 indexMovS  = movPlanC{end};
+
 % Create .mha file for base scan
-scanUID = basePlanC{indexBaseS.scan}(baseScanNum).scanUID;
+baseScanUID = basePlanC{indexBaseS.scan}(baseScanNum).scanUID;
 randPart = floor(rand*1000);
-baseScanUniqName = [scanUID,randPart];
+baseScanUniqName = [baseScanUID,randPart];
 baseScanFileName = fullfile(getCERRPath,'ImageRegistration','tmpFiles',['baseScan_',baseScanUniqName,'.mha']);
+try
+    delete(baseScanFileName)
+end
 success = createMhaScansFromCERR(baseScanNum, baseScanFileName, basePlanC);
 
 % Create .mha file for moving scan
-scanUID = movPlanC{indexMovS.scan}(movScanNum).scanUID;
+movScanUID = movPlanC{indexMovS.scan}(movScanNum).scanUID;
 randPart = floor(rand*1000);
-movScanUniqName = [scanUID,randPart];
+movScanUniqName = [movScanUID,randPart];
 movScanFileName = fullfile(getCERRPath,'ImageRegistration','tmpFiles',['movScan_',movScanUniqName,'.mha']);
+try
+    delete(movScanFileName)
+end
 success = createMhaScansFromCERR(movScanNum, movScanFileName, movPlanC);
 
 % Create a command file for plastimatch
 cmdFileName = fullfile(getCERRPath,'ImageRegistration','plastimatch_command',[baseScanUniqName,'_',movScanUniqName,'.txt']);
+try
+    delete(cmdFileName)
+end
 
 % Create a file name for storing bspline coefficients
-bspFileName = fullfile(getCERRPath,'ImageRegistration','tmpFiles',[baseScanUniqName,'_',movScanUniqName,'.txt']);
+bspFileName = fullfile(getCERRPath,'ImageRegistration','tmpFiles',['bsp_coeffs_',baseScanUniqName,'_',movScanUniqName,'.txt']);
+try
+    delete(bspFileName)
+end
 
 % Call appropriate command file based on algorithm
 userCmdFile = fullfile(getCERRPath,'ImageRegistration','plastimatch_command','bspline_register_cmd.txt');
@@ -33,15 +46,31 @@ cmdFileC{2,1} = ['fixed=',baseScanFileName];
 cmdFileC{3,1} = ['moving=',movScanFileName];
 cmdFileC{4,1} = ['xform_out=',bspFileName];
 cmdFileC{5,1} = '';
-cmdFileC{6:5+size(ursFileC,1),1} = userCmdFile(:,1);
+cmdFileC(6:5+size(ursFileC,2),1) = ursFileC(:);
 cell2file(cmdFileC,cmdFileName)
 
 % Run plastimatch Registration
-system(['plastimatch register ', cmdFileName])
+system(['plastimatch register ', cmdFileName]);
 
-% Read bspline coefficients
+% Read bspline coefficients file
+[bsp_img_origin,bsp_img_spacing,bsp_img_dim,bsp_roi_offset,bsp_roi_dim,bsp_vox_per_rgn,bsp_coefficients] = read_bsplice_coeff_file(bspFileName);
 
-% Write bspline coefficients to planC{indexS.deformS}
+% Create new deform object
+deformS = initializeCERR('deform');
+deformS.baseScanUID = baseScanUID;
+deformS.movScanUID = movScanUID;
+deformS.deformUID = createUID('deform');
+deformS.bsp_img_origin = bsp_img_origin;
+deformS.bsp_img_spacing = bsp_img_spacing;
+deformS.bsp_img_dim = bsp_img_dim;
+deformS.bsp_roi_offset = bsp_roi_offset;
+deformS.bsp_roi_dim = bsp_roi_dim;
+deformS.bsp_vox_per_rgn = bsp_vox_per_rgn;
+deformS.bsp_coefficients = bsp_coefficients;
 
-
+% Add deform object to both base and moving planC's
+baseDeformIndex = length(basePlanC{indexBaseS.deform}) + 1;
+movDeformIndex  = length(movPlanC{indexMovS.deform}) + 1;
+basePlanC{indexBaseS.deform}(baseDeformIndex)  = deformS;
+movPlanC{indexMovS.deform}(movDeformIndex) = deformS;
 
