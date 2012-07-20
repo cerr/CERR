3821669ac989c50e6d725f9fb641bc82892fd741
adityaapte <aditya.apte@gmail.com>
2012-07-20 14:58:30 -0400

    updated to output direction cosines

--- CERR_core/ImageRegistration/read_bsplice_coeff_file.m
+++ CERR_core/ImageRegistration/read_bsplice_coeff_file.m
@@ -1,4 +1,4 @@
-function [bsp_img_origin,bsp_img_spacing,bsp_img_dim,bsp_roi_offset,bsp_roi_dim,bsp_vox_per_rgn,bsp_coefficients] = read_bsplice_coeff_file(bspFileName)
+function [bsp_img_origin,bsp_img_spacing,bsp_img_dim,bsp_roi_offset,bsp_roi_dim,bsp_vox_per_rgn,bsp_direction_cosines,bsp_coefficients] = read_bsplice_coeff_file(bspFileName)
 % function [bsp_img_origin,bsp_img_spacing,bsp_img_dim,bsp_roi_offset,bsp_roi_dim,bsp_vox_per_rgn,bsp_coefficients] = read_bsplice_coeff_file(bspFileName)
 %
 % APA, 07/13/2012
@@ -35,4 +35,9 @@ bsp_vox_per_rgn = bspCoeffC{7};
 [~,remStr] = strtok(bsp_vox_per_rgn,'=');
 bsp_vox_per_rgn = str2num(remStr(2:end));
 
-bsp_coefficients = cellfun(@str2num,bspCoeffC(8:end));
+% Direction cosines
+bsp_direction_cosines = bspCoeffC{8};
+[~,remStr] = strtok(bsp_direction_cosines,'=');
+bsp_direction_cosines = str2num(remStr(2:end));
+
+bsp_coefficients = cellfun(@str2num,bspCoeffC(9:end));
