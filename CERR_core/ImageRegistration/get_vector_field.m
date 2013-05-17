86f81593a269a33df447fa83df661208c7ef88b6
Aditya Apte <aditya.apte@gmail.com>
2013-05-17 13:43:01 -0400

    Updated to use tmpdir to store intermediate i/o files.

--- CERR_core/ImageRegistration/get_vector_field.m
+++ CERR_core/ImageRegistration/get_vector_field.m
@@ -13,19 +13,23 @@ indexFromS = fromPlanC{end};
 
 toScanUID = toPlanC{indexToS.scan}(toScanNum).scanUID;
 fromScanUID = fromPlanC{indexFromS.scan}(fromScanNum).scanUID;
-if baseScanUID == fromScanUID
-    calc_inv_vf_flag = 1;
-else
+if isequal(baseScanUID, fromScanUID)
     calc_inv_vf_flag = 0;
+else
+    calc_inv_vf_flag = 1;
 end
 
 % Create b-spline coefficients file
-bspFileName = fullfile(getCERRPath,'ImageRegistration','tmpFiles',['bsp_coeffs_',baseScanUID,'_',toScanUID,'.txt']);
+%bspFileName = fullfile(getCERRPath,'ImageRegistration','tmpFiles',['bsp_coeffs_',baseScanUID,'_',movScanUID,'.txt']);
+bspFileName = fullfile(tempdir,'tmpFiles',['bsp_coeffs_',baseScanUID,'_',movScanUID,'.txt']);
 success     = write_bspline_coeff_file(bspFileName,deformS.algorithmParamsS);
 
 % Obtain Vf from b-splice coefficients
-vfFileName = fullfile(getCERRPath,'ImageRegistration','tmpFiles',['vf_',baseScanUID,'_',movScanUID,'.mha']);
+%vfFileName = fullfile(getCERRPath,'ImageRegistration','tmpFiles',['vf_',baseScanUID,'_',movScanUID,'.mha']);
+vfFileName = fullfile(tempdir,'tmpFiles',['vf_',baseScanUID,'_',movScanUID,'.mha']);
 system(['plastimatch xf-convert --input ',escapeSlashes(bspFileName), ' --output ', escapeSlashes(vfFileName), ' --output-type vf'])
+%system(['plastimatch convert --xf ',escapeSlashes(bspFileName), ' --output-vf=', escapeSlashes(vfFileName)])
+delete(bspFileName)
 
 if calc_inv_vf_flag
     % Get dims, origin, spacing for toScan
@@ -42,11 +46,12 @@ if calc_inv_vf_flag
     system(['vf_invert --input ', escapeSlashes(vfFileName), ' --output ', escapeSlashes(vfFileName), ' --dims="',num2str(img_size),'" --origin="',num2str(offset),'" --spacing="',num2str(resolution),'"'])
 end
 
-%infoS  = mha_read_header(vfFileName);
-%vf = mha_read_volume(infoS);
+% infoS  = mha_read_header(vfFileName);
+% vf = mha_read_volume(infoS);
 [vf,infoS] = readmha(vfFileName);
-vf = flipdim(permute(vf,[2,1,3]),3);
+%vf = flipdim(permute(vf,[2,1,3]),3);
+delete(vfFileName)
 
-vf_out(:,:,:,1) = flipdim(permute(vf(:,:,:,1),[2,1,3]),3);
-vf_out(:,:,:,2) = flipdim(permute(vf(:,:,:,2),[2,1,3]),3);
-vf_out(:,:,:,3) = flipdim(permute(vf(:,:,:,3),[2,1,3]),3);
+vf_out(:,:,:,1) = flipdim(permute(vf(:,:,:,1),[2,1,3]),3)/10;
+vf_out(:,:,:,2) = flipdim(permute(-vf(:,:,:,2),[2,1,3]),3)/10;
+vf_out(:,:,:,3) = flipdim(permute(-vf(:,:,:,3),[2,1,3]),3)/10;
