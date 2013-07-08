69476948b9bb02e0751ebe495ec5413bf6f9784a
Aditya Apte <aditya.apte@gmail.com>
2013-07-08 16:10:07 -0400

    Added planC as an optional input argument

--- CERR_core/Utilities/getStructurSegmentCoords.m
+++ CERR_core/Utilities/getStructurSegmentCoords.m
@@ -1,11 +1,16 @@
-function pointsM = getStructurSegmentCoords(structNum)
-% function pointsM = getStructurSegmentCoords(structNum)
+function pointsM = getStructurSegmentCoords(structNum, planC)
+% function pointsM = getStructurSegmentCoords(structNum, planC)
 %
 % This function returns x,y,z coordinates of all the segments belonging to
 % the passed structNum
 %
 % APA, 8/7/2013
 
+if ~exist('planC','var')
+    global planC
+end
+
+indexS = plaanC{end};
 pointsM = [];
 for i=length(planC{indexS.structures}(structNum).contour)
     for segNum = 1:length(planC{indexS.structures}(structNum).contour(i).segments)
