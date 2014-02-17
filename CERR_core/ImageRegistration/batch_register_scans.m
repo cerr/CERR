5b85fb8d7007a2989b761e20ceae369dc7c2cf61
Aditya Apte <aditya.apte@gmail.com>
2014-02-17 18:14:22 -0500

    finished implementing

--- CERR_core/ImageRegistration/batch_register_scans.m
+++ CERR_core/ImageRegistration/batch_register_scans.m
@@ -13,26 +13,41 @@ for baseNum = 1:length(baseScansC)
     planC = loadPlanC(baseScansC{baseNum},tempdir);
     planC = updatePlanFields(planC);
     planC = quality_assure_planC(baseScansC{baseNum},planC);
+    uid = createUID('scan');
     
+    indexS = planC{end};
+    planC{indexS.deform}(:) = [];
+    planC{indexS.scan}.scanUID = uid;
+
     for movNum = 1:length(movScansC)
         
         % Load moving planC as planD
         planD = loadPlanC(movScansC{movNum},tempdir);
         planD = updatePlanFields(planD);
         planD = quality_assure_planC(movScansC{movNum},planD);
+        uid = createUID('scan');
+        
+        indexSD = planD{end};
+        planD{indexSD.deform}(:) = [];
+        planD{indexSD.scan}.scanUID = uid;
         
         % Register planD to planC
         baseScanNum = 1;
         movScanNum  = 1;
-        algorithm   = '';
-        [planC, planD] = register_scans(planC, planD, baseScanNum, movScanNum, algorithm);
+        algorithm = 'BSPLINE PLASTIMATCH';
+        baseMask3M = [];
+        movMask3M = [];
+        threshold_bone = [];
+        [planC, planD] = register_scans(planC, planD, baseScanNum, movScanNum, algorithm, baseMask3M, movMask3M, threshold_bone);        
         
         % Save base and moving scans
         planC = save_planC(planC,[],'passed',baseScansC{baseNum});
-        planD = save_planC(planD,[],'passed',movScansC{movNum});
+        planD = save_planC(planD,[],'passed',movScansC{movNum});        
+        
+        clear planD
         
     end
-    
+    clear planC
 end
 
 
