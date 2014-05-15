f79fa0788b17bcd7c888dded310faf42b8c425c1
Aditya Apte <aditya.apte@gmail.com>
2014-05-15 14:01:02 -0400

    updated version and date

--- CERR_core/Documentation/CERRCurrentVersion.m
+++ CERR_core/Documentation/CERRCurrentVersion.m
@@ -21,6 +21,6 @@ function [Version, date] = CERRCurrentVersion
 % You should have received a copy of the GNU General Public License
 % along with CERR.  If not, see <http://www.gnu.org/licenses/>.
 
-Version  = '4.4';
-date =  '15-Jul-2013';
+Version  = '4.6';
+date =  '15-May-2014';
 
