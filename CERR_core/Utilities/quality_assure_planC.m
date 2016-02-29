b257c3187fb2ef5afd2ef6d739ba9b606b73dd1a
Apte <aptea@mskcc.org>
2016-02-29 11:08:05 -0500

    Commented out the "mesh" part.

--- CERR_core/Utilities/quality_assure_planC.m
+++ CERR_core/Utilities/quality_assure_planC.m
@@ -31,22 +31,22 @@ if ~isempty(str2double(lastSavedInVer)) && (isempty(lastSavedInVer) || str2doubl
 end
 
 
-%Check for mesh representation and load meshes into memory
-currDir = cd;
-%meshDir = fileparts(which('libMeshContour.dll'));
-meshDir = fullfile(getCERRPath,'bin','MeshInterp');
-cd(meshDir)
-for strNum = 1:length(planC{indexS.structures})
-    if isfield(planC{indexS.structures}(strNum),'meshRep') && ~isempty(planC{indexS.structures}(strNum).meshRep) && planC{indexS.structures}(strNum).meshRep
-        try
-            calllib('libMeshContour','loadSurface',planC{indexS.structures}(strNum).strUID,planC{indexS.structures}(strNum).meshS)
-        catch
-            planC{indexS.structures}(strNum).meshRep    = 0;
-            planC{indexS.structures}(strNum).meshS      = [];
-        end
-    end
-end
-cd(currDir)
+% %Check for mesh representation and load meshes into memory
+% currDir = cd;
+% %meshDir = fileparts(which('libMeshContour.dll'));
+% meshDir = fullfile(getCERRPath,'bin','MeshInterp');
+% cd(meshDir)
+% for strNum = 1:length(planC{indexS.structures})
+%     if isfield(planC{indexS.structures}(strNum),'meshRep') && ~isempty(planC{indexS.structures}(strNum).meshRep) && planC{indexS.structures}(strNum).meshRep
+%         try
+%             calllib('libMeshContour','loadSurface',planC{indexS.structures}(strNum).strUID,planC{indexS.structures}(strNum).meshS)
+%         catch
+%             planC{indexS.structures}(strNum).meshRep    = 0;
+%             planC{indexS.structures}(strNum).meshS      = [];
+%         end
+%     end
+% end
+% cd(currDir)
 
 if ~isfield(stateS,'optS')
     stateS.optS = CERROptions;
