28f220f5f8ded8c5da3b6ba80dbceaa9228d6014
adityaapte <aditya.apte@gmail.com>
2016-04-18 12:05:20 -0400

    offsets for directionality

---
+++ CERR_core/PlanMetrics/heterogenity_metrics/getOffsets.m
@@ -0,0 +1,43 @@
+function offsetsM = getOffsets(dirctn)
+% function offsetsM = getOffsets(dirctn)
+%
+%This function returns offsets for the passed directionality
+%
+% APA, 04/04/2016
+
+
+% All 13 directional offsets
+offsetsM = [ 1  0  0;
+             0  1  0;
+             1  1  0;
+             1 -1  0;
+             1  0  1;
+             0  1  1;
+             1  1  1;
+             1 -1  1;
+             0  0  1;
+            -1  0  1;
+            -1 -1  1;
+             0 -1  1;
+             1 -1  1];
+
+switch dirctn
+    
+    case 1
+        
+        return;
+        
+    case 2
+        
+        offsetsM(5:end,:) = [];
+        
+        return;
+        
+    otherwise
+        error('The input direction is not implemented yet.')
+end
+
+
+         
+
+
