9c7496c76d6eb5461f156bbbc2c926e4f187fad0
adityaapte <aditya.apte@gmail.com>
2016-05-23 17:09:44 -0400

    discretization adapted from from graycomatrix

--- CERR_core/PlanMetrics/heterogenity_metrics/imquantize_cerr.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/imquantize_cerr.m
@@ -1,12 +1,23 @@
-function q=imquantize_cerr(x,L)
+function q = imquantize_cerr(x,L)
 % image quantization 
-x01 = quantile(x(:),0.01);
-x99 = quantile(x(:),0.99);
-x(x<x01) = x01;
-x(x>x99) = x99;
+
+% outlier removal
+% x01 = quantile(x(:),0.01);
+% x99 = quantile(x(:),0.99);
+% x(x<x01) = x01;
+% x(x>x99) = x99;
+
+% % from El Naqa's code.
 xmax=max(x(:));
 xmin=min(x(:));
-range=xmax-xmin;
-scale=(L-1)/range;
-q=round(x*scale)/scale;
+% range=xmax-xmin;
+% scale=(L-1)/range;
+% q1=round(x*scale)/scale;
+
+% matlab's discretization from graycomatrix
+slope = (L-1) / (xmax - xmin);
+intercept = 1 - (slope*(xmin));
+%q = floor(imlincomb(slope,x,intercept,'double'));
+q = round(imlincomb(slope,x,intercept,'double'));
+
 return
