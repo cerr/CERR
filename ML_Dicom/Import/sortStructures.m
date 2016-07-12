eb127a7a18387b66d6f6cd3cceb1844889f8cb40
adityaapte <aditya.apte@gmail.com>
2016-07-12 15:35:59 -0400

    coordinate transformation for the RTSTRUCTs associated with oblique MR slices. Needs to be tested on more cases.

--- ML_Dicom/Import/sortStructures.m
+++ ML_Dicom/Import/sortStructures.m
@@ -40,9 +40,26 @@ segmentCell = cell(length(zmesh), 1);
 [segmentCell{1:end}] = deal(segmentTemplate);
 contourTemplate = struct('segments', segmentCell);
 
+% get a list of sopInstanceUIDs
+sopInstanceC = {};
+for slcNum = 1:length(structS.contour)
+    sopInstanceC{slcNum} = structS.contour(slcNum).sopInstanceUID;
+end
+sopInstanceC = sopInstanceC(index);
+
+modality = planC{indexS.scan}(scanInd).scanInfo(1).imageType;
+
 for j=1:length(zmesh) % loop through the number of CT
-    locate_point=find(voiZ==zmesh(j)); % search for a match between Z-location of current CT and voiZ
-    if isempty(locate_point)
+    
+    if strcmpi(modality,'mr')
+        % APA: use sopInstanceUID to find the matching slice for MR scan
+        sopInstanceUID = planC{indexS.scan}(scanInd).scanInfo(j).DICOMHeaders.SOPInstanceUID;
+        locate_point = strmatch(sopInstanceUID,sopInstanceC);
+    else    
+        locate_point=find(voiZ==zmesh(j)); % search for a match between Z-location of current CT and voiZ
+    end
+
+    if isempty(locate_point) && ~strcmpi(modality,'mr')
         
         [locate_point]=find(voiZ>zmesh(j)-slicethickness(j)./2 & voiZ<zmesh(j)+slicethickness(j)./2);
         
@@ -50,6 +67,7 @@ for j=1:length(zmesh) % loop through the number of CT
         %                 voi_thickness = max(diff(voiZ));
         %                 [locate_point]=find(voiZ >= zmesh(j)-voi_thickness./2 & voiZ <= zmesh(j)+voi_thickness./2);
         %             end
+        
         
         if ~isempty(locate_point)
             % if a match is found the VOI segment was defined of a
@@ -94,7 +112,7 @@ for j=1:length(zmesh) % loop through the number of CT
         else %can not find contours in current slice, try larger radius.
             
         end
-    else
+    elseif ~isempty(locate_point)
         % if match is found it's because this segment(s) of the VOI was(were) defined at the Z-location
         % of the current CT
         for k=1:length(locate_point)
@@ -102,6 +120,14 @@ for j=1:length(zmesh) % loop through the number of CT
             %                 slice=slice+1;
             segment = structS.contour(index(locate_point(k))).segments;
             segment(:,3) = zmesh(j);
+            % for oblique MR scans, flip along scan's yOffset and add
+            % the offset from the first slice
+            if strcmpi(modality,'mr') && numel(unique(planC{indexS.scan}(scanInd)...
+                    .scanInfo(1).DICOMHeaders.ImageOrientationPatient)) > 2
+                deltaY = planC{indexS.scan}(scanInd).scanInfo(j).yOffset - ...
+                    planC{indexS.scan}(scanInd).scanInfo(1).yOffset;
+                segment(:,2) = deltaY + planC{indexS.scan}(scanInd).scanInfo(j).yOffset - segment(:,2);
+            end
             contourTemplate(j).segments(end+1).points = segment;
         end
     end
