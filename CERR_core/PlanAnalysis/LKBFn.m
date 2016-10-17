94ad96618eef5c7c670e5499ed3be46e81d3383b
aditiiyer <iyera@mskcc.org>
2016-10-17 14:23:01 -0400

    Modified for use with outcomeModelsGUI

--- CERR_core/PlanAnalysis/LKBFn.m
+++ CERR_core/PlanAnalysis/LKBFn.m
@@ -1,27 +1,15 @@
-function [EUDv,ntcpV,conf] = LKBFn(EUDv,paramS,varargin)
-
-global planC;
+function ntcp = LKBFn(paramS,doseBinsV,volHistV)
 
 %Get parameters
 D50 = paramS.D50;
 m = paramS.m;
 a = paramS.a;
 
-% EUD for selected struct/dose
-if isempty(EUDv)
-    structNum = varargin{1};
-    doseNum = varargin{2};
-    scale = varargin{3};
-    [planC, doseBinsV, volsHistV] = getDVHMatrix(planC,structNum,doseNum);
-    doseBinsV = scale * doseBinsV;
-    EUDv = calc_EUD(doseBinsV, volsHistV, a);
-end
+%EUD for selected struct/dose
+EUD = calc_EUD(doseBinsV, volHistV, a);
 
 %Compute NTCP
-tmpv = (EUDv - D50)/(m*D50);
-ntcpV = 1/2 * (1 + erf(tmpv/2^0.5));
-
-%Model confidence
-conf = []; %%TO DO
+tmpv = (EUD - D50)/(m*D50);
+ntcp = 1/2 * (1 + erf(tmpv/2^0.5));
 
 end
