6838117797d384d2854a010074248c4177b25d65
adityaapte <aditya.apte@gmail.com>
2016-11-30 19:31:10 -0500

    speed up by usig a different approach. Avoided expensive ismsmber calls.

--- CERR_core/PlanMetrics/heterogenity_metrics/calcRLM.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/calcRLM.m
@@ -43,10 +43,12 @@ function rlmOut = calcRLM(quantizedM, offsetsM, nL, rlmType)
 %
 % APA, 09/12/2016
 
-% Default to building cooccurrence by combining all offsets
+% Default to building RLM by combining all offsets
 if ~exist('rlmType','var')
     rlmType = 1;
 end
+
+quantizedM = padarray(quantizedM,[1 1 1],0,'both');
 
 % Apply pading of 1 row/col/slc. This assumes offsets are 1. Need to
 % parameterize this in case of offsets other than 2. Rarely used for
@@ -54,9 +56,6 @@ end
 numColsPad = 1;
 numRowsPad = 1;
 numSlcsPad = 1;
-
-% Get number of voxels per slice
-[numRows, numCols, numSlices] = size(quantizedM);
 
 % Pad quantizedM
 q = padarray(quantizedM,[numRowsPad numColsPad numSlcsPad],0,'both');
@@ -69,8 +68,8 @@ q = uint16(q); % q is the quantized image
 % Number of offsets
 numOffsets = size(offsetsM,1);
 
-% Max run length in units of voxels
-maxRunLen = 1000;
+% Max run length in units of voxels (consider parameterizing)
+maxRunLen = 500;
 
 % Initialize the run-length matrix
 rlmM = zeros(nL,maxRunLen);
@@ -86,106 +85,42 @@ for off = 1:numOffsets
     % Selected offset
     offset = offsetsM(off,:);
     
-    % Get the image
-    slc1M = q(numRowsPad+(1:numRows),numColsPad+(1:numCols),...
-        numSlcsPad+(1:numSlices));
-    
-    % Get the circshifted image
-    slc2M = circshift(q,offset);
-    slc2M = slc2M(numRowsPad+(1:numRows),numColsPad+(1:numCols),numSlcsPad+(1:numSlices));
-    
-    % loop over all gray levels    
+    % loop over all gray levels        
     for level = 1:nL
         
         % Take difference between the original and the circshifted image to
         % figure out start and the end of runs.
-        diffM = (slc1M==level) - (slc2M==level);
+        diffM = (q==level) - (circshift(q,offset)==level);
         
         % 1's represent start of runs        
-        startIndV = find(diffM(:) == 1);
+        startM = diffM == 1;
         
-        % -1's represent end of runs
-        endIndV = find(diffM(:) == -1);
+        startIndV = find(startM);
         
-        % Get row,col,slice for the run-starts
-        [rV,cV,sV] = ind2sub([numRows, numCols, numSlices],startIndV);
+        prevM = uint16(q == level);
+        convergedM = ~startM;        
         
-        lenV = [];
-        % This is where the run lengths are accumulated. Loop over all the 
-        % starting indices to calculate run lengths.
-        for i = 1:length(startIndV)
+        while ~all(convergedM(:))
+                        
+            nextM = circshift(prevM,-offset);
             
-            % ind is the current starting index in the loop
-            ind = startIndV(i);
+            addM = prevM + nextM;
             
-            % Figure out the voxel index for the end of the run.
+            newConvergedM = addM == prevM;
             
-            numRowSteps = inf;
-            if offset(1) > 0
-                numRowSteps = numRows - rV(i);
-            elseif offset(1) < 0
-                numRowSteps = rV(i) - 1;
-            end
+            toUpdateM = ~convergedM & newConvergedM;
             
-            numColSteps = inf;
-            if offset(2) > 0
-                numColSteps = numCols - cV(i);
-            elseif offset(2) < 0
-                numColSteps = cV(i) - 1;
-            end
+            prevM = nextM;
+            prevM(startIndV) = addM(startIndV);
             
-            numSlcSteps = inf;
-            if offset(3) > 0
-                numSlcSteps = numSlices - sV(i);
-            elseif offset(3) < 0
-                numSlcSteps = sV(i) - 1;
-            end
+            lenV = addM(toUpdateM);
             
-            numStepsToEnd = min([numRowSteps numColSteps numSlcSteps]);
+            % accumulate lengths into run length matrix
+            rlmM(level,:) = rlmM(level,:) + accumarray(lenV,1,[maxRunLen 1])';    
             
-            rowEnd = rV(i) + offset(1)*numStepsToEnd;
-            colEnd = cV(i) + offset(2)*numStepsToEnd;
-            slcEnd = sV(i) + offset(3)*numStepsToEnd;
+            convergedM = convergedM | newConvergedM;
             
-            endIndex = rowEnd + numRows*(colEnd-1) + numRows*numCols*(slcEnd-1);            
-            
-            % Figure out the step size in terms of linear index
-            deltaIndex = offset(1) + numRows*offset(2) + numRows*numCols*offset(3);
-            
-            % these are all available indices to choose from
-            availableIndV = ind+deltaIndex:deltaIndex:endIndex;
-            
-            % currentEndV contains the end indices for the current
-            % situation.
-            if ~ismember(endIndex,endIndV)
-                currentEndV = [endIndV; endIndex];
-                endFlag = 1;
-            else
-                currentEndV = endIndV;
-                endFlag = 0;
-            end
-            
-            % See if any of the currentEndV belong to availableIndV.
-            assocIndV = find(ismember(currentEndV,availableIndV));
-            
-            % assocIndV will be empty for boundary starting index. Hence,
-            % count that as 1, otherwise get the closest end index.
-            if ~isempty(assocIndV)
-                % try to speedup, get rid of "min"
-                [~,minInd] = min((ind - currentEndV(assocIndV)).^2);
-                endInd = currentEndV(assocIndV(minInd));
-                len = abs(endInd-ind)/abs(deltaIndex);
-                if endInd == endIndex && endFlag
-                    len = len + 1;
-                end
-            else
-                len = 1;
-            end
-            lenV = [lenV len];
         end
-        
-        % accumulate lengths into run length matrix
-        rlmM(level,:) = rlmM(level,:) + accumarray(lenV',1,[maxRunLen 1])';
         
     end
     
@@ -194,6 +129,7 @@ for off = 1:numOffsets
         rlmOut{off} = rlmM;
     end
     
+        
 end
 
 % assign rlmM to rlmOut for rlmType == 1
@@ -202,4 +138,3 @@ if rlmType == 1
 end
 
 return;
-
