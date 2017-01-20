c3cea11fc96da083d43185e8a883a9fd1e9a2ed1
adityaapte <aditya.apte@gmail.com>
2017-01-20 16:45:34 -0500

    converted to a function.

--- CERR_core/PlanMetrics/heterogenity_metrics/getShapeParams.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/getShapeParams.m
@@ -1,12 +1,13 @@
+function shapeS = getShapeParams(structNum,planC)
 % getShapeParams.m
 
 % APA, 07/06/2016
 
-global planC
-indexS = planC{end};
+if ~exist('planC','var')
+    global planC
+end
 
-% define structure number
-structNum = 33;
+indexS = planC{end};
 
 % Get associated scan
 scanNum = getStructureAssociatedScan(structNum,planC);
@@ -24,40 +25,34 @@ triMesh = delaunay(xSurfV,ySurfV,zSurfV);
 TR = triangulation(triMesh, xSurfV', ySurfV', zSurfV');
 [tri, Xb] = freeBoundary(TR);
 
-% plot surface
-figure, trisurf(tri, Xb(:,1), Xb(:,2), Xb(:,3), 'EdgeColor', 'cyan',...
-    'FaceColor', 'cyan','FaceAlpha', 0.2);
+% % plot surface
+% figure, trisurf(tri, Xb(:,1), Xb(:,2), Xb(:,3), 'EdgeColor', 'cyan',...
+%     'FaceColor', 'cyan','FaceAlpha', 0.2);
 
 % Calculate surface area from triangular mesh
-area = trimeshSurfaceArea(Xb,tri);
+shapeS.surfArea = trimeshSurfaceArea(Xb,tri);
 
-% Calculate Volume of the structure
-vol = getStructureVol(structNum, planC);
+% Calculate Volume of the structure, eq. (22) Aerts Nature suppl.
+shapeS.vol = getStructureVol(structNum, planC);
 
 % Compactness 1 (V/(pi*A^(3/2)), eq. (15) Aerts Nature suppl.
-Compactness1 = vol / (pi*area^1.5);
+shapeS.Compactness1 = shapeS.vol / (pi*shapeS.surfArea^1.5);
 
 % Compactness 2 (36*pi*V^2/A^3), eq. (16) Aerts Nature suppl.
-Compactness2 = 36*pi*vol^2/area^3;
+shapeS.Compactness2 = 36*pi*shapeS.vol^2/shapeS.surfArea^3;
 
 % max3dDiameter , eq. (17) Aerts Nature suppl.
 
 % Spherical disproportion (A/(4*pi*R^2) , eq. (18) Aerts Nature suppl.
-R = (vol*3/4/pi)^(1/3);
-spherDisprop = area / (4*pi*R^2);
+R = (shapeS.vol*3/4/pi)^(1/3);
+shapeS.spherDisprop = shapeS.surfArea / (4*pi*R^2);
 
 % Sphericity , eq. (19) Aerts Nature suppl.
-sphericity = pi^(1/3) * (6*vol)^(2/3) / area;
+shapeS.sphericity = pi^(1/3) * (6*shapeS.vol)^(2/3) / shapeS.surfArea;
 
 % Surface area , eq. (20) Aerts Nature suppl.
 
 % Surface to volume ratio , eq. (21) Aerts Nature suppl.
-surfToVolRatio = area / vol;
-
-% volume , eq. (22) Aerts Nature suppl.
-vol;
-
-
-
+shapeS.surfToVolRatio = shapeS.surfArea / shapeS.vol;
 
 
