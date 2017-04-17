7fc729c87a3541a51b654593c2a596ae560f2a28
adityaapte <aditya.apte@gmail.com>
2017-04-17 00:29:32 -0400

    implemented the remaining ngldm scalar features

--- CERR_core/PlanMetrics/heterogenity_metrics/ngldmToScalarFeatures.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/ngldmToScalarFeatures.m
@@ -1,5 +1,5 @@
-function featuresS = ngldmToScalarFeatures(s)
-% function featuresS = ngldmToScalarFeatures(s)
+function featuresS = ngldmToScalarFeatures(s,numVoxels)
+% function featuresS = ngldmToScalarFeatures(s,numVoxels)
 % 
 % APA, 03/16/2017
 
@@ -7,6 +7,8 @@ function featuresS = ngldmToScalarFeatures(s)
 Ns = sum(s(:));
 Nn = size(s,2);
 Ng = size(s,1);
+lenV = 1:Nn;
+levV = 1:Ng;
 
 % Low dependence emphasis
 sLdeM = bsxfun(@rdivide,s,(1:Nn).^2);
@@ -29,29 +31,51 @@ sLdlgeM = bsxfun(@rdivide,bsxfun(@rdivide,s,(1:Nn).^2)',(1:Ng).^2);
 featuresS.ldlge = sum(sLdlgeM(:))/Ns;
 
 % Low dependence high grey level emphasis
-
+sLdhgeM = bsxfun(@times,bsxfun(@rdivide,s,(1:Nn).^2)',(1:Ng).^2);
+featuresS.ldhge = sum(sLdhgeM(:))/Ns;
 
 % High dependence low grey level emphasis
+sHdlgeM = bsxfun(@rdivide,bsxfun(@times,s,(1:Nn).^2)',(1:Ng).^2);
+featuresS.hdlge = sum(sHdlgeM(:))/Ns;
 
 % High dependence high grey level emphasis
+sHdhgeM = bsxfun(@times,bsxfun(@times,s,(1:Nn).^2)',(1:Ng).^2);
+featuresS.hdhge = sum(sHdhgeM(:))/Ns;
 
 % Grey level non-uniformity
+featuresS.gln = sum(sum(s,2).^2)/Ns;
 
 % Grey level non-uniformity normalised
+featuresS.glnNorm = sum(sum(s,2).^2)/Ns^2;
 
 % Dependence count non-uniformity
+featuresS.dcn = sum(sum(s,1).^2)/Ns;
 
 % Dependence count non-uniformity normalised
+featuresS.dcnNorm = sum(sum(s,1).^2)/Ns^2;
 
 % Dependence count percentage
+featuresS.dcp = Ns/numVoxels;
 
 % Grey level variance
+iPij = bsxfun(@times,s'/sum(s(:)),levV);
+mu = sum(iPij(:));
+iMinusMuPij = bsxfun(@times,s'/sum(s(:)),(levV-mu).^2);
+featuresS.glv = sum(iMinusMuPij(:));
 
 % Dependence count variance
+jPij = bsxfun(@times,s/sum(s(:)),lenV);
+mu = sum(jPij(:));
+jMinusMuPij = bsxfun(@times,s/sum(s(:)),(lenV-mu).^2);
+featuresS.dcv = sum(jMinusMuPij(:));
 
 % Dependence count entropy
+p = s(:)/sum(s(:));
+featuresS.entropy = -sum(p .* log2(p+1e-10));
 
 % Dependence count energy
+p = s(:)/sum(s(:));
+featuresS.energy = sum(p .^2);
 
 
 
