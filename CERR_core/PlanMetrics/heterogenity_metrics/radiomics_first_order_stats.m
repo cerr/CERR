117c08b52e8de1aec9a9196fc55755269dc86d09
adityaapte <aditya.apte@gmail.com>
2017-04-17 00:32:14 -0400

    additional features as per IBSI

--- CERR_core/PlanMetrics/heterogenity_metrics/radiomics_first_order_stats.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/radiomics_first_order_stats.m
@@ -28,12 +28,12 @@ if ~isempty(planC{indexS.scan}(scanNum).scanInfo(1).CTOffset)
     maskScan3M = double(maskScan3M) - planC{indexS.scan}(scanNum).scanInfo(1).CTOffset;
 end
 
-% Get Pixel-size
-[xUnifV, yUnifV, zUnifV] = getUniformScanXYZVals(planC{indexS.scan}(scanNum));
-PixelSpacingXi = abs(xUnifV(2)-xUnifV(1));
-PixelSpacingYi = abs(yUnifV(2)-yUnifV(1));
-PixelSpacingZi = abs(zUnifV(2)-zUnifV(1));
-VoxelVol = PixelSpacingXi*PixelSpacingYi*PixelSpacingZi;
+% % Get Pixel-size
+% [xUnifV, yUnifV, zUnifV] = getUniformScanXYZVals(planC{indexS.scan}(scanNum));
+% PixelSpacingXi = abs(xUnifV(2)-xUnifV(1));
+% PixelSpacingYi = abs(yUnifV(2)-yUnifV(1));
+% PixelSpacingZi = abs(zUnifV(2)-zUnifV(1));
+% VoxelVol = PixelSpacingXi*PixelSpacingYi*PixelSpacingZi;
 
 % Iarray = Data.Image(~isnan(Data.Image));     %    Array of Image values
 indStructV = maskStruct3M(:) == 1;
@@ -42,7 +42,7 @@ Iarray = maskScan3M(indStructV);
 else
     
     Iarray = planC;
-    VoxelVol = structNum;
+    % VoxelVol = structNum;
 end
 
 
@@ -80,9 +80,42 @@ N = ceil( RadiomicsFirstOrder.range/Step);  %
 RadiomicsFirstOrder.rms           = sqrt(sum(Iarray.^2)/length(Iarray));
 
 %   Total Energy ( integraal(a^2) )
-RadiomicsFirstOrder.totalEnergy   = VoxelVol*sum(Iarray.^2);
+RadiomicsFirstOrder.totalEnergy   = sum(Iarray.^2);
 
 %   Mean deviation (also called mean absolute deviation)
-RadiomicsFirstOrder.MD            = mad(Iarray);
+RadiomicsFirstOrder.meanAbsDev            = mad(Iarray);
+
+% Median absolute deviation
+RadiomicsFirstOrder.medianAbsDev = sum(abs(Iarray-RadiomicsFirstOrder.median)) / numel(Iarray);
+
+%   P10
+p10 = prctile(Iarray,10);
+RadiomicsFirstOrder.P10 = p10;
+
+%   P90
+p90 = prctile(Iarray,90);
+RadiomicsFirstOrder.P90 = p90;
+
+Iarray10_90 = Iarray(Iarray >= p10 & Iarray <= p90);
+
+%   Robust Mean Absolute Deviation
+RadiomicsFirstOrder.robustMeanAbsDev  = mad(Iarray10_90);
+
+%   Robust Median Absolute Deviation
+RadiomicsFirstOrder.robustMedianAbsDev  = sum(abs(Iarray10_90-median(Iarray10_90)))...
+    / numel(Iarray10_90);
+
+% Inter-Quartile Range (IQR)
+% P75 - P25
+p75 = prctile(Iarray,75);
+p25 = prctile(Iarray,25);
+RadiomicsFirstOrder.interQuartileRange = p75 - p25;
+
+% Quartile coefficient of Dispersion
+RadiomicsFirstOrder.coeffDispersion = (p75-p25)/(p75+p25);
+
+% Coefficient of variation
+RadiomicsFirstOrder.coeffVariation = RadiomicsFirstOrder.std / RadiomicsFirstOrder.mean;
+
 
 end
