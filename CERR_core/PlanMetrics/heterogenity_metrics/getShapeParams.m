f2737604f480ba66df4df014305197fa7d0bee58
adityaapte <aditya.apte@gmail.com>
2017-04-16 23:03:02 -0400

    re-factored to include hole-filtering and sampling

--- CERR_core/PlanMetrics/heterogenity_metrics/getShapeParams.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/getShapeParams.m
@@ -1,26 +1,93 @@
-function shapeS = getShapeParams(structNum,planC)
+function shapeS = getShapeParams(structNum,planC,rcsV)
+% function shapeS = getShapeParams(structNum,planC,rcsV)
+%
+% This function computes shape features for the passed input.
+%
+% INPUTS:
+% structNum: The structure index in planC
+% planC: CERR's planC data structure
+% rcsV: number of rows/colums/slices to use for resampling
+%
 % getShapeParams.m
 
 % APA, 07/06/2016
 
-if ~exist('planC','var')
-    global planC
+if numel(structNum)
+    if ~exist('planC','var')
+        global planC
+    end
+    
+    indexS = planC{end};
+    
+    % Get associated scan
+    scanNum = getStructureAssociatedScan(structNum,planC);
+    
+    % Get surface points
+    [xValsV, yValsV, zValsV] = getUniformScanXYZVals(planC{indexS.scan}(scanNum));
+    yValsV = fliplr(yValsV);
+    mask3M = getUniformStr(structNum,planC);
+    
+else
+    mask3M = structNum;
+    xValsV = planC{1};
+    yValsV = planC{2};
+    zValsV = planC{3};
+    %voxelVol = 2; %abs((xVals(1)-xVals(2))*(yVals(1)-yVals(2))*(zVals(1)-zVals(2)));
+    %volume = planC{4};
 end
 
-indexS = planC{end};
+% Get voxel size
+voxelSiz(1) = abs(yValsV(2) - yValsV(1));
+voxelSiz(2) = abs(xValsV(2) - xValsV(1));
+voxelSiz(3) = abs(zValsV(2) - zValsV(1));
+voxelVolume = prod(voxelSiz);
 
-% Get associated scan
-scanNum = getStructureAssociatedScan(structNum,planC);
 
-% Get surface points
-[xVals, yVals, zVals] = getUniformScanXYZVals(planC{indexS.scan}(scanNum));
-mask3M = getUniformStr(structNum,planC);
+volume = voxelVolume * sum(mask3M(:));
+
+% Fill holes
+mask3M = imfill(mask3M,'holes');
+filledVolume = voxelVolume * sum(mask3M(:));
+
+% Add a row/col/slice to account for half a voxel
+mask3M = padarray(mask3M,[1 1 1],'replicate');
+xValsV = [xValsV(1)-voxelSiz(1) xValsV xValsV(end)+voxelSiz(1)];
+yValsV = [yValsV(1)-voxelSiz(2) yValsV yValsV(end)+voxelSiz(2)];
+zValsV = [zValsV(1)-voxelSiz(3) zValsV zValsV(end)+voxelSiz(3)];
+
+% Resample the structure mask
+if exist('rcsV','var')
+    % min/max voxel coordinates for resampling
+    minX = min(xValsV)+voxelSiz(2)/2;
+    maxX = max(xValsV)-voxelSiz(2)/2;
+    minY = min(yValsV)+voxelSiz(1)/2;
+    maxY = max(yValsV)-voxelSiz(1)/2;
+    minZ = min(zValsV)+voxelSiz(3)/2;
+    maxZ = max(zValsV)-voxelSiz(3)/2;
+    % new x/y/z grid using the resampled size
+    xValsNewV = linspace(minX,maxX,rcsV(2));
+    yValsNewV = linspace(minY,maxY,rcsV(1));
+    zValsNewV = linspace(minZ,maxZ,rcsV(3));
+    [Xm,Ym,Zm] = meshgrid(xValsNewV,yValsNewV,zValsNewV);
+    %xFieldV = [min(xValsV) max(xValsV)];
+    %yFieldV = [min(yValsV) max(yValsV)];
+    %zFieldV = [min(zValsV) max(zValsV)];
+    %xyzUpC = {xValsNewV,yValsNewV,zValsNewV,volume};
+    %outOfBoundsVal = 0;
+    % resample the structure mask
+    mask3M = interp3(xValsV, yValsV, zValsV, mask3M, Xm, Ym, Zm,'nearest');
+    xValsV = xValsNewV;
+    yValsV = yValsNewV;
+    zValsV = zValsNewV;
+end
+
+% Get the surface points for the structure mask
 surfPoints = getSurfacePoints(mask3M);
-xSurfV = xVals(surfPoints(:,2));
-ySurfV = yVals(surfPoints(:,1));
-zSurfV = zVals(surfPoints(:,3));
+xSurfV = xValsV(surfPoints(:,2));
+ySurfV = yValsV(surfPoints(:,1));
+zSurfV = zValsV(surfPoints(:,3));
 
-% Check if coplanar structure
+% Check if it's a coplanar structure
 if length(unique(zSurfV)) < 2
     shapeS.surfArea = NaN;
     shapeS.vol = NaN;
@@ -32,7 +99,7 @@ if length(unique(zSurfV)) < 2
     return;
 end
 
-% Generate surfacemesh
+% Generate surface mesh
 triMesh = delaunay(xSurfV,ySurfV,zSurfV);
 TR = triangulation(triMesh, xSurfV', ySurfV', zSurfV');
 [tri, Xb] = freeBoundary(TR);
@@ -45,26 +112,32 @@ TR = triangulation(triMesh, xSurfV', ySurfV', zSurfV');
 shapeS.surfArea = trimeshSurfaceArea(Xb,tri);
 
 % Calculate Volume of the structure, eq. (22) Aerts Nature suppl.
-shapeS.vol = getStructureVol(structNum, planC);
+% if ndims(structNum) == 1
+%     shapeS.vol = getStructureVol(structNum, planC);
+% else
+%     shapeS.vol = volume; %sum(mask3M(:)) * voxelVol;
+% end
+shapeS.volume = volume;
+shapeS.filledVolume = filledVolume;
 
 % Compactness 1 (V/(pi*A^(3/2)), eq. (15) Aerts Nature suppl.
-shapeS.Compactness1 = shapeS.vol / (pi*shapeS.surfArea^1.5);
+shapeS.Compactness1 = shapeS.volume / (pi*shapeS.surfArea^1.5);
 
 % Compactness 2 (36*pi*V^2/A^3), eq. (16) Aerts Nature suppl.
-shapeS.Compactness2 = 36*pi*shapeS.vol^2/shapeS.surfArea^3;
+shapeS.Compactness2 = 36*pi*shapeS.volume^2/shapeS.surfArea^3;
 
 % max3dDiameter , eq. (17) Aerts Nature suppl.
 
 % Spherical disproportion (A/(4*pi*R^2) , eq. (18) Aerts Nature suppl.
-R = (shapeS.vol*3/4/pi)^(1/3);
+R = (shapeS.volume*3/4/pi)^(1/3);
 shapeS.spherDisprop = shapeS.surfArea / (4*pi*R^2);
 
 % Sphericity , eq. (19) Aerts Nature suppl.
-shapeS.sphericity = pi^(1/3) * (6*shapeS.vol)^(2/3) / shapeS.surfArea;
+shapeS.sphericity = pi^(1/3) * (6*shapeS.volume)^(2/3) / shapeS.surfArea;
 
 % Surface area , eq. (20) Aerts Nature suppl.
 
 % Surface to volume ratio , eq. (21) Aerts Nature suppl.
-shapeS.surfToVolRatio = shapeS.surfArea / shapeS.vol;
+shapeS.surfToVolRatio = shapeS.surfArea / shapeS.volume;
 
 
