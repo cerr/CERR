7d91d6c8b0e7861758a1d030d72cd9b26ab3bfc5
adityaapte <aditya.apte@gmail.com>
2017-04-17 00:26:27 -0400

    Size Zone matrix calculation

---
+++ CERR_core/PlanMetrics/heterogenity_metrics/calcSZM.m
@@ -0,0 +1,53 @@
+function szmM = calcSZM(quantizedM, nL, szmType)
+% function szmM = calcSZM(quantizedM, nL, szmType)
+%
+% This function calculates the Size-Zone matrix for the passed quantized
+% image.
+%
+% INPUTS:
+%       quantizedM: quantized 3d matrix obtained, for example, by
+%       imquantize_cerr.m
+%       nL: Number of gray levels.
+%       rlmType: flag, 1 or 2.
+%                   1: 3D zones
+%                   2: 2D zones
+% OUTPUT:
+%       szmM: size-zone matrix of size (nL x L)
+%
+% EXAMPLE:
+%
+% numRows = 10;
+% numCols = 10;
+% numSlcs = 1;
+% 
+% % number of gray levels
+% nL = 3;
+% 
+% % create an image with random numbers
+% imgM = randi(nL,numRows,numCols,numSlcs);
+% 
+% % set option to add run lengths from all directions
+% szmType = 1;
+% 
+% % call the rlm calculator
+% szmType = calcSZM(imgM, nL, szmType);
+%
+%
+% APA, 03/30/2017
+
+if szmType == 1
+    numNeighbors = 26;
+else
+    numNeighbors = 8;
+end
+
+szmM = sparse(nL,numel(quantizedM));
+for level = 1:nL
+    connM = bwlabeln(quantizedM==level, numNeighbors);
+    for siz = 1:max(connM(:))
+        regionM = connM == siz;
+        regionSiz = sum(regionM(:));
+        szmM(level,regionSiz) = szmM(level,regionSiz) + 1;
+    end
+end
+
