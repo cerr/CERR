ffa2a19676056e92674b9fbfe7cbddef159aa2fe
aditiiyer <iyera@mskcc.org>
2017-04-27 15:05:49 -0400

    Added check for existing deformation & map display flag

--- CERR_core/ImageRegistration/calc_ICE.m
+++ CERR_core/ImageRegistration/calc_ICE.m
@@ -1,12 +1,15 @@
-function dist3M = calc_ICE(scan1PlanC,scan2PlanC,strNum1,scanNum2,varargin)
+function dist3M = calc_ICE(scan1PlanC,scan2PlanC,strNum1,scanNum2,showMap,varargin)
 % Registers selected scans and returns inverse consistency error in registration
 % AI 4/25/17
+% AI 4/27/17 - Added check for existing deformation
+%              Added flag for ICE map display            
 % ============================================================================================
 % Inputs
 % scan1PlanC  : CERR archive with base plan
 % scan2PlanC  : CERR archive with moving plan
 % strNum1     : Structure no. on base plan
 % scanNum2    : Moving scan no. in scan2PlanC
+% showMap     : Flag to display ICE map as dose
 % --Optional --
 % varargin{1} : algorithm
 % varargin{2} : baseMask3M
@@ -19,8 +22,8 @@ function dist3M = calc_ICE(scan1PlanC,scan2PlanC,strNum1,scanNum2,varargin)
 % =============================================================================================
 
 %Define defaults for optional inputs
-minargs = 4;
-maxargs = 8;
+minargs = 5;
+maxargs = 9;
 narginchk(minargs,maxargs)
 nVarargs = nargin - minargs;
 algorithm = 'BSPLINE PLASTIMATCH';
@@ -31,14 +34,22 @@ optC = {algorithm, baseMask3M, movMask3M, boneThreshold};
 [optC{1:nVarargs}] = varargin{:};
 [algorithm,baseMask3M,movMask3M,boneThreshold] = optC{:};
 
+%Get base scan associated with stucture
+scanNum1 = getStructureAssociatedScan(strNum1,scan1PlanC); 
+
+%Get deformation index (fwd transformation)
+indexS = scan1PlanC{end};
+scan1UID = scan1PlanC{indexS.scan}(scanNum1).scanUID;
+scan2UID = scan2PlanC{indexS.scan}(scanNum2).scanUID;
+deformS = scan1PlanC{indexS.deform};
+deform1Idx  = getDeformIdx(scan1UID,scan2UID,deformS);
+if isempty(deform1Idx)
 %Register moving to base scan (fwd transform)
-scanNum1 = getStructureAssociatedScan(strNum1,scan1PlanC); % Get base scan associated with stucture
 [scan1PlanC, scan2PlanC] = register_scans(scan1PlanC, scan2PlanC, scanNum1, scanNum2, ...
     algorithm, baseMask3M, movMask3M, boneThreshold);
-
-%Get deformation for fwd transformation (scan2 to scan1)
-indexS = scan1PlanC{end};
-deform1S = scan1PlanC{indexS.deform}(end);
+deform1Idx = length(scan1PlanC{indexS.deform});
+end
+deform1S = scan1PlanC{indexS.deform}(deform1Idx);
 
 %Get structure coordinates on scan1
 rasterSegments = getRasterSegments(strNum1,scan1PlanC);
@@ -58,10 +69,15 @@ end
 [x2DeformV, y2DeformV, z2DeformV] = getDeformationAt(deform1S,scan2PlanC,scanNum2,scan1PlanC,scanNum1,pts1M);
 pts2M = pts1M + [x2DeformV, y2DeformV, z2DeformV];
 
-%Get deformation for inverse transformation
-[scan2PlanC, scan1PlanC] = register_scans(scan2PlanC, scan1PlanC, scanNum2, scanNum1, ...
-    algorithm, baseMask3M, movMask3M, boneThreshold); %Register base to moving scan (inverse transform)
-deform2S = scan1PlanC{indexS.deform}(end);
+%Get deformation index for inverse transformation
+deform2Idx  = getDeformIdx(scan2UID,scan1UID,deformS);
+if isempty(deform2Idx)
+    %Register base to moving scan (inverse transform)
+    [scan2PlanC, scan1PlanC] = register_scans(scan2PlanC, scan1PlanC, scanNum2, scanNum1, ...
+        algorithm, baseMask3M, movMask3M, boneThreshold);
+    deform2Idx = length(scan1PlanC{indexS.deform});
+end
+deform2S = scan1PlanC{indexS.deform}(deform2Idx);
 
 
 %Get registered coordinates on scan1 (inverse transform)
@@ -84,6 +100,22 @@ for slcNum = 1:length(uniqueSlices)
     dist3M(:,:,uniqueSlices(slcNum)) = reshape(distMaskV,rows,cols);
     start = numPts;
 end
+
+%Display ICE map
+if showMap
+    
+    %Display as dose (TO DO)
+    
+end
+
+%-----------------------------------------------------------------------
+    function deformIdx  = getDeformIdx(scan1UID,scan2UID,deformS) 
+        
+        baseMapV = strcmp(scan1UID,{deformS.baseScanUID});
+        movMapV = strcmp(scan2UID,{deformS.movScanUID});
+        deformIdx = find(baseMapV & movMapV,1,'last');
+        
+    end
 
 
 end
