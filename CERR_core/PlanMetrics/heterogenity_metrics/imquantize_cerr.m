97b73c2ae654ca18f1e7272f62fa3249fab9261e
adityaapte <aditya.apte@gmail.com>
2018-02-26 18:10:30 -0500

    updated to quantize using a fixed bin width.

--- CERR_core/PlanMetrics/heterogenity_metrics/imquantize_cerr.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/imquantize_cerr.m
@@ -1,44 +1,39 @@
-function q = imquantize_cerr(x,L,xmin,xmax)
-% image quantization 
+function q = imquantize_cerr(x,nL,xmin,xmax,binwidth)
+% function q = imquantize_cerr(x,nL,xmin,xmax,binwidth)
+% 
+% Function to quantize an image. The image can be quantized in the
+% following two ways based on :
+% 1> Number of bins: Specify the first two input arguments. i.e. the image 
+% matrix x and the number of levels nL. The min/max can be passed using the 
+% xmin, xmax arguments. If they are empty or not passed, the min/max are 
+% computed from the image x.
+% 2> Bin width: the nL, xmin, xmax are input as empty. Specify the 
+% bin width using the binwidth argument
+%
+% APA, 2/26/2018
 
-% % old code: El naqa
-% xmax=max(x(:));
-% xmin=min(x(:));
-% range=xmax-xmin;
-% scale=(L-1)/range;
-% q=round(x*scale)/scale;
-% return
-
-% % outlier removal
-% x01 = quantile(x(:),0.01);
-% x99 = quantile(x(:),0.99);
-% x(x<x01) = x01;
-% x(x>x99) = x99;
-
-if ~exist('xmin','var')
-    % % from El Naqa's code.
+if ~exist('xmin','var') || exist('xmin','var') && isempty(xmin)
     xmax=max(x(:));
     xmin=min(x(:));
-    % range=xmax-xmin;
-    % scale=(L-1)/range;
-    % q1=round(x*scale)/scale;
-    % xmax=85;
-    % xmin=-160;
 else
     x(x<xmin) = xmin;
     x(x>xmax) = xmax;
 end
 
-% matlab's discretization from graycomatrix
-slope = (L-1) / (xmax - xmin);
-intercept = 1 - (slope*(xmin));
-%q = floor(imlincomb(slope,x,intercept,'double'));
-q = round(imlincomb(slope,x,intercept,'double'));
-
-% edgesL = linspace(xmin,-100,2);
-% edgesP = linspace(-80,80,26);
-% edgesU = linspace(100,xmax,2);
-% edges = [edgesL, edgesP, edgesU, max(x(:))];
-% q = discretize(x,edges);
+if ~isempty(nL)
+    % matlab's discretization from graycomatrix
+    slope = (nL-1) / (xmax - xmin);
+    intercept = 1 - (slope*(xmin));
+    q = round(imlincomb(slope,x,intercept,'double'));
+    
+elseif exist('binwidth','var') && ~isempty(binwidth)
+    
+    edgeMin = xmin - rem(xmin,binwidth);
+    edgemax = xmax + rem(xmax,binwidth);
+    edgeV = edgeMin:binwidth:edgemax;
+    q = discretize(x,edgeV);
+else
+    error('Specify the number of bins or the binwidth.')
+end
 
 return
