c6a214f78fb8b47d2b38f6199c9259e4a798dc9a
aditiiyer <iyera@mskcc.org>
2018-03-28 15:34:24 -0400

    Added function to compute BED

---
+++ CERR_core/PlanMetrics/calc_BED.m
@@ -0,0 +1,37 @@
+function BED = calc_BED(paramS,varargin)
+%BED = calc_BED(paramS)
+%Ref: Comparison Between Mechanistic Radiobiological Modeling Vs. 
+%Fowler BED Equation in Evaluating Lung Cancer Radiotherapy Outcome 
+%for a Broad Range of Fractionation, J Jeong et al., AAPM 2017
+%-----------------------------------------------------------------------
+% INPUTS
+% paramS : Parameter dictionary with fields:
+%          d  - Fraction size
+%          n  - No. fractions
+%          T  - No. treatment days
+%          alpha
+%          abRatio
+%-----------------------------------------------------------------------
+% AI 12/4/17
+
+%Define constants  
+Tk = paramS.Tk.val;         %Kick-off time of repopulation (days)
+Tp =  paramS.Tp.val;        %Potential tumor doubling time (days)
+
+
+alpha = paramS.alpha.val;    
+abRatio = paramS.abRatio.val;  %alpha/beta for tumor
+d = paramS.frxSize.val;
+n = paramS.numFractions.val;
+T = floor(n/5)*7 + mod(n,5);
+
+
+%Compute BED
+BED = n*d*(1 + d/abRatio);
+if T > Tk
+  BED = BED -  log(2) * (T-Tk)/(alpha*Tp);
+end
+   
+    
+
+end
