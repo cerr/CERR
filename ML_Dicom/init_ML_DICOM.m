42db79c8f77db29e3f03ee6b401b12075701558a
adityaapte <aditya.apte@gmail.com>
2018-05-11 11:20:33 -0400

    corrected paths for the deployed version

--- ML_Dicom/init_ML_DICOM.m
+++ ML_Dicom/init_ML_DICOM.m
@@ -32,15 +32,15 @@ function initFlag = init_ML_DICOM
 initFlag = 1;
 
 if isdeployed
-    path1 = [getCERRPath,'bin\dcm4che-2.0.25\lib\dcm4che-core-2.0.25.jar'];
-    path2 = [getCERRPath,'bin\dcm4che-2.0.25\lib\log4j-1.2.16.jar'];
-    path3 = [getCERRPath,'bin\dcm4che-2.0.25\lib\slf4j-api-1.6.1.jar'];
-    path4 = [getCERRPath,'bin\dcm4che-2.0.25\lib\slf4j-log4j12-1.6.1.jar'];
-    path5 = [getCERRPath,'bin\dcm4che-2.0.25\lib\dcm4che-image-2.0.25.jar'];
-    path6 = [getCERRPath,'bin\dcm4che-2.0.25\lib\dcm4che-imageio-2.0.25.jar'];
-    path7 = [getCERRPath,'bin\dcm4che-2.0.25\lib\dcm4che-imageio-rle-2.0.25.jar'];
-    path8 = [getCERRPath,'bin\dcm4che-2.0.25\lib\dcm4che-iod-2.0.25.jar'];
-    path9 = [getCERRPath,'bin\dcm4che-2.0.25\lib\dcm4che-net-2.0.25.jar'];
+    path1 = [getCERRPath,'bin\dcm4che-2.0.27\lib\dcm4che-core-2.0.27.jar'];
+    path2 = [getCERRPath,'bin\dcm4che-2.0.27\lib\log4j-1.2.16.jar'];
+    path3 = [getCERRPath,'bin\dcm4che-2.0.27\lib\slf4j-api-1.6.1.jar'];
+    path4 = [getCERRPath,'bin\dcm4che-2.0.27\lib\slf4j-log4j12-1.6.1.jar'];
+    path5 = [getCERRPath,'bin\dcm4che-2.0.27\lib\dcm4che-image-2.0.27.jar'];
+    path6 = [getCERRPath,'bin\dcm4che-2.0.27\lib\dcm4che-imageio-2.0.27.jar'];
+    path7 = [getCERRPath,'bin\dcm4che-2.0.27\lib\dcm4che-imageio-rle-2.0.27.jar'];
+    path8 = [getCERRPath,'bin\dcm4che-2.0.27\lib\dcm4che-iod-2.0.27.jar'];
+    path9 = [getCERRPath,'bin\dcm4che-2.0.27\lib\dcm4che-net-2.0.27.jar'];
     javaaddpath({path1,path2,path3,path4,path5,path6,path7,path8,path9});
     return
 end
@@ -83,7 +83,7 @@ else
 end
 
 if isempty(path1) || isempty(path2) || isempty(path3) || isempty(path4) || isempty(path5) || isempty(path6) || isempty(path7) || isempty(path8) || isempty(path9)
-    warndlg('File "dcm4che-core-2.0.25.jar" is not added to MATLAB path. Add the folder "dcm4che-2.0.25" to MATLAB path and start again');
+    warndlg('File "dcm4che-core-2.0.27.jar" is not added to MATLAB path. Add the folder "dcm4che-2.0.27" to MATLAB path and start again');
     initFlag = 0;
     return;
 else
