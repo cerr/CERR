c57d0a53777eaf99719aa0440144c93718bdcfcf
rkpandya <rkpandya@syr.edu>
2018-06-25 14:55:48 -0400

    read additional params passed

--- Unit_Testing/tests_for_cerr/pyFeatureExtraction.py
+++ Unit_Testing/tests_for_cerr/pyFeatureExtraction.py
@@ -12,13 +12,22 @@ from radiomics import getTestCase, imageoperations, featureextractor, getTestCas
 from radiomics.featureextractor import RadiomicsFeaturesExtractor
 
 
-def extract(imagePath, maskPath, paramfilepath, preprocessingFilter):
+def extract(imagePath, maskPath, paramfilepath, preprocessingFilter, tempDir):
   r"""
   RKP - 3/20/2018
   Wrapper class for calculation of a radiomics features using pyradiomics.
 
   """
+  # reading original image directly
+  #imageName, maskName = getTestCase('brain1')
+  if imagePath is None or maskPath is None:  # Something went wrong, in this case PyRadiomics will also log an error
+    print('Error getting test matrix!')
+    exit()
+  # image = sitk.ReadImage(imageName)
+  # mask = sitk.ReadImage(maskName)
 
+  image = sitk.ReadImage(imagePath)
+  mask = sitk.ReadImage(maskPath)
 
   image, mask = RadiomicsFeaturesExtractor().loadImage(imagePath, maskPath)
 
@@ -32,12 +41,16 @@ def extract(imagePath, maskPath, paramfilepath, preprocessingFilter):
     resultdict = {}
     extractor = featureextractor.RadiomicsFeaturesExtractor(params)
     result = extractor.execute(imagePath, maskPath)
-    sigmaValues = [1.0]
+    sigmaValues = [3.0]
     for logImage, imageTypeName, inputKwargs in imageoperations.getLoGImage(image, sigma=sigmaValues):
       resultArray = sitk.GetArrayFromImage(logImage)
       resultArray = matlab.double(resultArray.tolist())
-      # imgName = decompositionName + '.nrrd'
-      # sitk.WriteImage(decompositionImage, imgName)
+      imgName = tempDir + imageTypeName + '.nrrd'
+      try:
+        sitk.WriteImage(logImage, imgName)
+      except Exception as e:
+        print("Couldn't write image to file (%s)." % e)
+
       #resultdict[decompositionName] = resultArray
     return resultArray
 
@@ -80,7 +93,7 @@ def main():
   imagePath = 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\scan.nrrd'
   maskPath = 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\mask.nrrd'
   paramFilePath = 'W:\\Rutu\\CERR-testing\\Unit_Testing\\tests_for_cerr\pyradParams.yaml'
-  result = extract(imagePath, maskPath, paramFilePath, 'LoG')
+  result = extract(imagePath, maskPath, paramFilePath, 'LoG', 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\')
 
 
 main()
