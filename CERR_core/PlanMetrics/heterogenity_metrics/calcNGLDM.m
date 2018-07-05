daf28ff68718c3c0056d22eeba8b7347ec47b003
adityaapte <aditya.apte@gmail.com>
2018-07-04 23:04:35 -0400

    Reverted back to using all the voxels within the ROI as per IBSI definition.

--- CERR_core/PlanMetrics/heterogenity_metrics/calcNGLDM.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/calcNGLDM.m
@@ -51,13 +51,13 @@ slcWindow = uint32(slcWindow);
 start_ind = reshape(bsxfun(@plus,[1:m-rowWindow+1]',[0:n-colWindow]*m),[],1); %//'
 
 %// Row indices
-% lin_row = permute(bsxfun(@plus,start_ind,[0:rowWindow-1])',[1 3 2]);  %//'
-lin_row = permute(bsxfun(@plus,start_ind,[0 patchSizeV(1) rowWindow-1])',[1 3 2]);  %//'
+lin_row = permute(bsxfun(@plus,start_ind,[0:rowWindow-1])',[1 3 2]);  %//'
+%lin_row = permute(bsxfun(@plus,start_ind,[0 patchSizeV(1) rowWindow-1])',[1 3 2]);  %//'
 
 %// Get linear indices based on row and col indices and get desired output
 % imTmpM = A(reshape(bsxfun(@plus,lin_row,[0:ncols-1]*m),nrows*ncols,[]));
-% indM = reshape(bsxfun(@plus,lin_row,(0:colWindow-1)*m),rowWindow*colWindow,[]);
-indM = reshape(bsxfun(@plus,lin_row,[0 patchSizeV(2) colWindow-1]*m),3*3,[]);
+indM = reshape(bsxfun(@plus,lin_row,(0:colWindow-1)*m),rowWindow*colWindow,[]);
+%indM = reshape(bsxfun(@plus,lin_row,[0 patchSizeV(2) colWindow-1]*m),3*3,[]);
 
 % [Fx,Fy] = gradient(q);
 %Fx = abs(Fx);
@@ -68,7 +68,7 @@ indM = reshape(bsxfun(@plus,lin_row,[0 patchSizeV(2) colWindow-1]*m),3*3,[]);
 
 % Initialize the s (NGTDM) matrix
 maxNbhoodSz = prod(2*patchSizeV+1)-1;
-maxNbhoodSz = 8;
+%maxNbhoodSz = 8;
 s = zeros(numGrLevels,maxNbhoodSz+1);
 
 tic
