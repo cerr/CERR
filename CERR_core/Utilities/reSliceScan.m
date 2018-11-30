348ae03f60c2722c504e3bc9c4f3a934eb6e8b23
adityaapte <aditya.apte@gmail.com>
2018-11-30 11:35:38 -0500

    Added flag to use sinc sampling

--- CERR_core/Utilities/reSliceScan.m
+++ CERR_core/Utilities/reSliceScan.m
@@ -1,12 +1,13 @@
-function planC = reSliceScan(scanNum,dSag,dCor,dTrans,planC)
-%function planC = reSliceScan(scanNum,dSag,dCor,dTrans,planC)
+function planC = reSliceScan(scanNum,dSag,dCor,dTrans,sincFlag,planC)
+%function planC = reSliceScan(scanNum,dSag,dCor,dTrans,sincFlag,planC)
 %
 %This function re-slices the scan scanNum according to resolution dSag,dCor,dTrans
 %INPUT: scanNum: Scan Index (1 if only one scan is present)
-%       dSag   : Slice spacing in Sagittal direction
-%       dCor   : Slice spacing in Coronal direction
-%       dTrans : Slice spacing in Transverse direction
-%       planC  : Optional input
+%       dSag     : Slice spacing in Sagittal direction
+%       dCor     : Slice spacing in Coronal direction
+%       dTrans   : Slice spacing in Transverse direction
+%       sincFlag : Apply sinc sampling
+%       planC    : Optional input
 %
 %EXAMPLE:
 %       scanNum = 1;
@@ -16,24 +17,24 @@ function planC = reSliceScan(scanNum,dSag,dCor,dTrans,planC)
 %APA, 12/02/2009
 %
 % Copyright 2010, Joseph O. Deasy, on behalf of the CERR development team.
-% 
+%
 % This file is part of The Computational Environment for Radiotherapy Research (CERR).
-% 
+%
 % CERR development has been led by:  Aditya Apte, Divya Khullar, James Alaly, and Joseph O. Deasy.
-% 
+%
 % CERR has been financially supported by the US National Institutes of Health under multiple grants.
-% 
-% CERR is distributed under the terms of the Lesser GNU Public License. 
-% 
+%
+% CERR is distributed under the terms of the Lesser GNU Public License.
+%
 %     This version of CERR is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
 %     the Free Software Foundation, either version 3 of the License, or
 %     (at your option) any later version.
-% 
+%
 % CERR is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 % without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 % See the GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with CERR.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -90,10 +91,20 @@ for strNum = 1:length(structNumV)
     planC{indexS.structures}(structNumV(strNum)).contour = newContourS;
 end
 
+% Sinc upsample
+if sincFlag
+    numCols = length(newXVals);
+    numRows = length(newYVals);
+    numSlcs = length(newZVals);
+    scanArray3M = single(planC{indexS.scan}(scanNum).scanArray) - ...
+        planC{indexS.scan}(scanNum).scanInfo(1).CTOffset;
+    newScanArray = fft_upsample3d(scanArray3M,[numRows numCols numSlcs]);
+end
 %try
 
-    for slcNum = 1:newNumSlcs
-        %Interpolate in z direction
+for slcNum = 1:newNumSlcs
+    %Interpolate in z direction
+    if ~sincFlag
         [slc, sliceXVals, sliceYVals] = getCTOnSlice(scanNum, newZVals(slcNum), 3, planC);
         slc = finterp2(sliceXVals, sliceYVals, double(slc), newXVals, newYVals, 1,0);
         slc = reshape(slc, [length(newYVals) length(newXVals)]);
@@ -109,76 +120,78 @@ end
                 slc = single(slc);
         end
         newScanArray(:,:,slcNum) = slc;
-        if slcNum == 1
-            newScanInfo(slcNum) = oldScanInfo;
-        else
-            newScanInfo = dissimilarInsert(newScanInfo, oldScanInfo, slcNum);
-        end
-        newScanInfo(slcNum).grid1Units = newGridInterval1;
-        newScanInfo(slcNum).grid2Units = newGridInterval2;
-        newScanInfo(slcNum).sizeOfDimension1 = length(newYVals);
-        newScanInfo(slcNum).sizeOfDimension2 = length(newXVals);
-        newScanInfo(slcNum).zValue = newZVals(slcNum);
-        newScanInfo(slcNum).sliceThickness = sliceThickness;
-        newScanInfo(slcNum).voxelThickness = sliceThickness;
-        
-        %Interpolate structures on to this slice (Nearest Neighbor Interpolation)
-%         newContourS = struct('segments',[]);
-%         for i = 1:newNumSlcs
-%             newContourS(i).segments(1).points = deal([]);
-%         end
-        for strNum = 1:length(structNumV)
-            contourS = contourC{structNumV(strNum)}(nearestSlicesV(slcNum));
-            % planC{indexS.structures}(structNumV(strNum)).contour = newContourS;
-            for segNum = 1:length(contourS.segments)
-                if ~isempty(contourS.segments(segNum).points)
-                    newPointsM = contourS.segments(segNum).points;
-                    newPointsM(:,3) = newPointsM(:,3).^0 * newZVals(slcNum);
-                    planC{indexS.structures}(structNumV(strNum)).contour(slcNum).segments(segNum).points = newPointsM;
-                else
-                    planC{indexS.structures}(structNumV(strNum)).contour(slcNum).segments(segNum).points = [];
-                end
-            end
-            % contourLen = length(planC{indexS.structures}(structNumV(strNum)).contour);
-        end
-        
     end
-
-    planC{indexS.scan}(scanNum).scanInfo  = newScanInfo;
-    planC{indexS.scan}(scanNum).scanArray = newScanArray;
     
-    %Restore old transformation matrix    
-    planC{indexS.scan}(scanNum).transM = transM;    
-
-    %Re-Raster and reUniformize
-    planC{indexS.scan}(scanNum).uniformScanInfo             = [];
-    planC{indexS.scan}(scanNum).scanArraySuperior           = [];
-    planC{indexS.scan}(scanNum).scanArrayInferior           = [];
-    planC{indexS.structureArray}(scanNum).indicesArray      = [];
-    planC{indexS.structureArray}(scanNum).bitsArray         = [];
-    planC{indexS.structureArrayMore}(scanNum).indicesArray  = [];
-    planC{indexS.structureArrayMore}(scanNum).bitsArray     = [];
-
-
-    %Re-Generate raster segments
-    for strNum = 1:length(structNumV)
-        planC{indexS.structures}(structNumV(strNum)).rasterized = 0;
-        planC{indexS.structures}(structNumV(strNum)).rasterSegments = [];
+    if slcNum == 1
+        newScanInfo(slcNum) = oldScanInfo;
+    else
+        newScanInfo = dissimilarInsert(newScanInfo, oldScanInfo, slcNum);
     end
-    planC = getRasterSegs(planC,structNumV);
+    newScanInfo(slcNum).grid1Units = newGridInterval1;
+    newScanInfo(slcNum).grid2Units = newGridInterval2;
+    newScanInfo(slcNum).sizeOfDimension1 = length(newYVals);
+    newScanInfo(slcNum).sizeOfDimension2 = length(newXVals);
+    newScanInfo(slcNum).zValue = newZVals(slcNum);
+    newScanInfo(slcNum).sliceThickness = sliceThickness;
+    newScanInfo(slcNum).voxelThickness = sliceThickness;
+    
+    %Interpolate structures on to this slice (Nearest Neighbor Interpolation)
+    %         newContourS = struct('segments',[]);
+    %         for i = 1:newNumSlcs
+    %             newContourS(i).segments(1).points = deal([]);
+    %         end
+    for strNum = 1:length(structNumV)
+        contourS = contourC{structNumV(strNum)}(nearestSlicesV(slcNum));
+        % planC{indexS.structures}(structNumV(strNum)).contour = newContourS;
+        for segNum = 1:length(contourS.segments)
+            if ~isempty(contourS.segments(segNum).points)
+                newPointsM = contourS.segments(segNum).points;
+                newPointsM(:,3) = newPointsM(:,3).^0 * newZVals(slcNum);
+                planC{indexS.structures}(structNumV(strNum)).contour(slcNum).segments(segNum).points = newPointsM;
+            else
+                planC{indexS.structures}(structNumV(strNum)).contour(slcNum).segments(segNum).points = [];
+            end
+        end
+        % contourLen = length(planC{indexS.structures}(structNumV(strNum)).contour);
+    end
+    
+end
 
-    %uniformize
-    planC = setUniformizedData(planC,planC{indexS.CERROptions},scanNum);
+planC{indexS.scan}(scanNum).scanInfo  = newScanInfo;
+planC{indexS.scan}(scanNum).scanArray = newScanArray;
+
+%Restore old transformation matrix
+planC{indexS.scan}(scanNum).transM = transM;
+
+%Re-Raster and reUniformize
+planC{indexS.scan}(scanNum).uniformScanInfo             = [];
+planC{indexS.scan}(scanNum).scanArraySuperior           = [];
+planC{indexS.scan}(scanNum).scanArrayInferior           = [];
+planC{indexS.structureArray}(scanNum).indicesArray      = [];
+planC{indexS.structureArray}(scanNum).bitsArray         = [];
+planC{indexS.structureArrayMore}(scanNum).indicesArray  = [];
+planC{indexS.structureArrayMore}(scanNum).bitsArray     = [];
+
+
+%Re-Generate raster segments
+for strNum = 1:length(structNumV)
+    planC{indexS.structures}(structNumV(strNum)).rasterized = 0;
+    planC{indexS.structures}(structNumV(strNum)).rasterSegments = [];
+end
+planC = getRasterSegs(planC,structNumV);
+
+%uniformize
+planC = setUniformizedData(planC,planC{indexS.CERROptions},scanNum);
 
 % catch
-% 
-%     %Restore old transformation matrix    
+%
+%     %Restore old transformation matrix
 %     planC{indexS.scan}(scanNum).transM = transM;
-%     
+%
 %     %Restore old contours
 %     for strNum = 1:length(structNumV)
 %         planC{indexS.structures}(structNumV(strNum)).contour = contourC{structNumV(strNum)};
 %     end
-%     
+%
 % end
 
