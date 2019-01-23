3610ed70ce79fced64565255045bf2f19c69dea4
aditiiyer <iyera@mskcc.org>
2019-01-23 15:32:13 -0500

    Bug fix for rigid registration

--- CERR_core/ImageRegistration/register_scans.m
+++ CERR_core/ImageRegistration/register_scans.m
@@ -227,11 +227,7 @@ switch upper(algorithm)
         end
         
         % Rigid step
-        userCmdFile = fullfile(getCERRPath,'ImageRegistration','plastimatch_command','bspline_register_cmd_rigid.txt');
         ursFileC = file2cell(userCmdFile);
-        cmdFileC{1,1} = '[GLOBAL]';
-        cmdFileC{end+1,1} = ['fixed=',escapeSlashes(baseScanFileName)];
-        cmdFileC{end+1,1} = ['moving=',escapeSlashes(movScanFileName)];
         cmdFileC{end+1,1} = ['xform_out=',escapeSlashes(bspFileName_rigid)];
         cmdFileC{end+1,1} = '';
         cmdFileC(end+1:end+size(ursFileC,2),1) = ursFileC(:);
@@ -252,34 +248,36 @@ switch upper(algorithm)
             delete(cmdFileName_dir);
         end
         
-%         % Read output file
-%         fileC = file2cell(bspFileName_rigid);
-%         indParam = strfind(fileC{4},'Parameters:');
-%         rigidParamsV = str2num(fileC{4}(indParam+11:end));
-%         indParam = strfind(fileC{5},'FixedParameters:');
-%         fixedParamsV = str2num(fileC{5}(indParam+16:end));
-%         translationM = eye(4);
-%         translationM(1:3,1:3) = reshape(rigidParamsV(1:9),3,3)';
-%         translationM(1:3,4) = rigidParamsV(10:12)/10;
-%         %translationM(4,1:3) = rigidParamsV(10:12)/10;
-%         transM = translationM;
-%         transM(2:3,4) = -transM(2:3,4);
-%         %         translationM(1:3,4) = -rigidParamsV(4:6)/10;
-%         %         translationM(3,4) = -translationM(3,4);
-%         %         Rx = eye(4);
-%         %         Rx([2 3],[2 3]) = [cos(rigidParamsV(1)) sin(rigidParamsV(1)); -sin(rigidParamsV(1)) cos(rigidParamsV(1))];
-%         %         Ry = eye(4);
-%         %         Ry([1 3],[1 3]) = [cos(rigidParamsV(2)) -sin(rigidParamsV(2)); sin(rigidParamsV(2)) cos(rigidParamsV(2))];
-%         %         Rz = eye(4);
-%         %         Rz([1 2],[1 2]) = [cos(rigidParamsV(3)) sin(rigidParamsV(3)); -sin(rigidParamsV(3)) cos(rigidParamsV(3))];
-%         %         bakTransM = eye(4);
-%         %         bakTransM(1:3,4) = fixedParamsV/10;
-%         %         bakTransM(3,4) = -bakTransM(3,4);
-%         %         fwTransM = eye(4);
-%         %         fwTransM(1:3,4) = -fixedParamsV/10;
-%         %         fwTransM(3,4) = -fwTransM(3,4);
-%         %         transM = bakTransM*Rx*Ry*Rz*fwTransM*translationM;
-%         movPlanC{indexMovS.scan}(movScanNum).transM = transM;
+        %         % Read output file
+        %         fileC = file2cell(bspFileName_rigid);
+        %         indParam = strfind(fileC{4},'Parameters:');
+        %         rigidParamsV = str2num(fileC{4}(indParam+11:end));
+        %         indParam = strfind(fileC{5},'FixedParameters:');
+        %         fixedParamsV = str2num(fileC{5}(indParam+16:end));
+        %         translationM = eye(4);
+        %         translationM(1:3,1:3) = reshape(rigidParamsV(1:9),3,3)';
+        %         translationM(1:3,4) = rigidParamsV(10:12)/10;
+        %         %translationM(4,1:3) = rigidParamsV(10:12)/10;
+        %         transM = translationM;
+        %         transM(2:3,4) = -transM(2:3,4);
+        %         %         translationM(1:3,4) = -rigidParamsV(4:6)/10;
+        %         %         translationM(3,4) = -translationM(3,4);
+        %         %         Rx = eye(4);
+        %         %         Rx([2 3],[2 3]) = [cos(rigidParamsV(1)) sin(rigidParamsV(1)); -sin(rigidParamsV(1)) cos(rigidParamsV(1))];
+        %         %         Ry = eye(4);
+        %         %         Ry([1 3],[1 3]) = [cos(rigidParamsV(2)) -sin(rigidParamsV(2)); sin(rigidParamsV(2)) cos(rigidParamsV(2))];
+        %         %         Rz = eye(4);
+        %         %         Rz([1 2],[1 2]) = [cos(rigidParamsV(3)) sin(rigidParamsV(3)); -sin(rigidParamsV(3)) cos(rigidParamsV(3))];
+        %         %         bakTransM = eye(4);
+        %         %         bakTransM(1:3,4) = fixedParamsV/10;
+        %         %         bakTransM(3,4) = -bakTransM(3,4);
+        %         %         fwTransM = eye(4);
+        %         %         fwTransM(1:3,4) = -fixedParamsV/10;
+        %         %         fwTransM(3,4) = -fwTransM(3,4);
+        %         %         transM = bakTransM*Rx*Ry*Rz*fwTransM*translationM;
+        %         movPlanC{indexMovS.scan}(movScanNum).transM = transM;
+        
+        bspFileName = bspFileName_rigid;
         
     case 'BSPLINE ITK'
         
