fd8211a7484ac20e7c98efd9c109478f9e6d7038
adityaapte <aditya.apte@gmail.com>
2019-02-06 17:53:51 -0500

    updated to omit non-structure fields

--- CERR_core/PlanMetrics/featureStructToMat.m
+++ CERR_core/PlanMetrics/featureStructToMat.m
@@ -11,25 +11,27 @@ for patNum = 1:length(featureS)
     numFeats = 0;
     allFieldC = [];
     for iField = 1:length(fieldC)
-        featFieldC = fieldnames(featureS(1).(fieldC{iField}));
-        if strcmpi(featFieldC{1},'AvgS')
-            featV = full(struct2array(featureS(patNum).(fieldC{iField}).AvgS));
-            allFieldC = [allFieldC;...
-                strcat(fieldC{iField},'_',fieldnames(featureS(patNum).(fieldC{iField}).AvgS))];
-        elseif strcmpi(featFieldC{1},'peak')
-            featureS(patNum).(fieldC{iField}) = rmfield(featureS(patNum).(fieldC{iField}),'radius');
-            featureS(patNum).(fieldC{iField}) = rmfield(featureS(patNum).(fieldC{iField}),'radiusUnit');
-            featV = full(struct2array(featureS(patNum).(fieldC{iField})));
-            allFieldC = [allFieldC;...
-                strcat(fieldC{iField},'_',fieldnames(featureS(patNum).(fieldC{iField})))];
-        else
-            featV = full(struct2array(featureS(patNum).(fieldC{iField})));
-            allFieldC = [allFieldC;...
-                strcat(fieldC{iField},'_',fieldnames(featureS(patNum).(fieldC{iField})))];
+        if isstruct(featureS(1).(fieldC{iField}))
+            featFieldC = fieldnames(featureS(1).(fieldC{iField}));
+            if strcmpi(featFieldC{1},'AvgS')
+                featV = full(struct2array(featureS(patNum).(fieldC{iField}).AvgS));
+                allFieldC = [allFieldC;...
+                    strcat(fieldC{iField},'_',fieldnames(featureS(patNum).(fieldC{iField}).AvgS))];
+            elseif strcmpi(featFieldC{1},'peak')
+                featureS(patNum).(fieldC{iField}) = rmfield(featureS(patNum).(fieldC{iField}),'radius');
+                featureS(patNum).(fieldC{iField}) = rmfield(featureS(patNum).(fieldC{iField}),'radiusUnit');
+                featV = full(struct2array(featureS(patNum).(fieldC{iField})));
+                allFieldC = [allFieldC;...
+                    strcat(fieldC{iField},'_',fieldnames(featureS(patNum).(fieldC{iField})))];
+            else
+                featV = full(struct2array(featureS(patNum).(fieldC{iField})));
+                allFieldC = [allFieldC;...
+                    strcat(fieldC{iField},'_',fieldnames(featureS(patNum).(fieldC{iField})))];
+            end
+            numNewFts = numFeats + length(featV);
+            %disp([numFeats numNewFts])
+            featureM(patNum,numFeats+1:numNewFts) = featV;
+            numFeats = numNewFts;
         end
-        numNewFts = numFeats + length(featV);
-        %disp([numFeats numNewFts])
-        featureM(patNum,numFeats+1:numNewFts) = featV;
-        numFeats = numNewFts;
     end
 end
