cfde619fc32aa0f2c65ab276e006952278939eeb
rkpandya <rkpandya@syr.edu>
2019-03-28 14:27:11 -0400

    Bug Fix, and code updated to match the new version of pyradiomics

--- Unit_Testing/tests_for_cerr/testWaveletFilterWithPyrad.m
+++ Unit_Testing/tests_for_cerr/testWaveletFilterWithPyrad.m
@@ -36,13 +36,19 @@ scanType = 'wavelet';
 dirString = 'HHH';
 
 %%pyradiomics generated wavelet filtered images saved to tempDir     
-teststruct = PyradWrapper(testM, maskBoundingBox3M, scanType);
+teststruct = PyradWrapper(testM, maskBoundingBox3M, scanType, dirString);
+
+pyrad_wavelet_filename = fullfile(tmpDir, strcat('wavelet-', dirString, '.nrrd'));
+[pyWav3M, infoS] = nrrd_read(pyrad_wavelet_filename);
+%log3M = flipdim(data3M,3);
+
+pyWav3M = double(pyWav3M);
+
+pyWav3M = permute(pyWav3M, [2 1 3]);
 
 
 %% CERR wavelet images
 scanArray3M = flip(testM,3);
-
-
 
 if mod(size(scanArray3M,3),2) > 0
     scanArray3M(:,:,end+1) = 0*scanArray3M(:,:,1);
@@ -53,14 +59,30 @@ if mod(size(scanArray3M,3),2) > 0
 end
 scanArray3M = flip(scanArray3M,3);
 
-pyfield = strcat('wavelet_', dirString);
-pyradiomicsarray = getfield(teststruct, pyfield);
 
-%pyradiomicsarray = teststruct.wavelet_HHH;
-pydata = double(py.array.array('d',py.numpy.nditer(pyradiomicsarray)));
-data = reshape(pydata,[20 20 6]);
-        
-waveletDiffV = (scanArray3M - data) ./ scanArray3M * 100
+%% comparison
+diff3M = (scanArray3M - pyWav3M)./(pyWav3M+1e-5);
+quantile99Diff = quantile(abs(diff3M(abs(pyWav3M)>0.1)),0.99);
+disp(['Percentage difference between CERR and ITK: ', num2str(quantile99Diff), '%'])
+
+figure, 
+slc = 3;
+subplot(1,3,1), imagesc(testM(:,:,slc)), title('Original Image')
+subplot(1,3,2), imagesc(scanArray3M(:,:,slc)), title('CERR Wavelet')
+subplot(1,3,3), imagesc(pyWav3M(:,:,slc)), title('Pyradiomics Wavelet')
+
+
+
+
+
+% pyfield = strcat('wavelet_', dirString);
+% pyradiomicsarray = getfield(teststruct, pyfield);
+% 
+% %pyradiomicsarray = teststruct.wavelet_HHH;
+% pydata = double(py.array.array('d',py.numpy.nditer(pyradiomicsarray)));
+% data = reshape(pydata,[20 20 6]);
+%         
+waveletDiffV = (scanArray3M - pyWav3M) ./ scanArray3M * 100
 
 
 
