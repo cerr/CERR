0cb16293a19a9e71da45f87ff5c643b3ea03a588
adityaapte <aditya.apte@gmail.com>
2019-04-01 14:29:58 -0400

    updated to handle image types.

--- CERR_core/PlanMetrics/featureStructToMat.m
+++ CERR_core/PlanMetrics/featureStructToMat.m
@@ -1,37 +1,60 @@
 function [featureM,allFieldC] = featureStructToMat(featureS)
 % function [featureM,allFieldC] = featureStructToMat(featureS)
-% 
+%
 % Extracts features values from structure array containing feature to a 2D
 % matrix.
 %
+% INPUT:
+%   featureS structure array (output from batchExtractRadiomics.m)
+%   containing radiomics features for the structures of interest.
+%       featureS = 
+%           1×100 struct array with fields:
+%               struct_roi1
+%               struct_roi2
+%               fileName
+%
+% OUTPUT:
+%  featureM: nxm matrix containing m radiomics features for n samples.
+%  allFieldC: names of m radiomics features.
+%
 % APA, 7/1/2018
 
-fieldC = fieldnames(featureS);
-for patNum = 1:length(featureS)
+imgC = fieldnames(featureS);
+for patNum = 1:length(featureS) % patients loop
     numFeats = 0;
     allFieldC = [];
-    for iField = 1:length(fieldC)
-        if isstruct(featureS(1).(fieldC{iField}))
-            featFieldC = fieldnames(featureS(1).(fieldC{iField}));
-            if strcmpi(featFieldC{1},'AvgS')
-                featV = full(struct2array(featureS(patNum).(fieldC{iField}).AvgS));
-                allFieldC = [allFieldC;...
-                    strcat(fieldC{iField},'_',fieldnames(featureS(patNum).(fieldC{iField}).AvgS))];
-            elseif strcmpi(featFieldC{1},'peak')
-                featureS(patNum).(fieldC{iField}) = rmfield(featureS(patNum).(fieldC{iField}),'radius');
-                featureS(patNum).(fieldC{iField}) = rmfield(featureS(patNum).(fieldC{iField}),'radiusUnit');
-                featV = full(struct2array(featureS(patNum).(fieldC{iField})));
-                allFieldC = [allFieldC;...
-                    strcat(fieldC{iField},'_',fieldnames(featureS(patNum).(fieldC{iField})))];
-            else
-                featV = full(struct2array(featureS(patNum).(fieldC{iField})));
-                allFieldC = [allFieldC;...
-                    strcat(fieldC{iField},'_',fieldnames(featureS(patNum).(fieldC{iField})))];
+    for iImg = 1:length(imgC) % imageType loop
+        imgType = imgC{iImg};
+        if ~isstruct(featureS(1).(imgType))
+            continue;
+        end
+        featureForImgTypeS = [featureS.(imgType)];
+        fieldC = fieldnames(featureForImgTypeS);
+        for iField = 1:length(fieldC) % radiomics features loop
+            if isstruct(featureForImgTypeS(1).(fieldC{iField}))
+                featFieldC = fieldnames(featureForImgTypeS(1).(fieldC{iField}));                
+                if strcmpi(featFieldC{1},'AvgS')
+                    featV = full(struct2array(featureForImgTypeS(patNum).(fieldC{iField}).AvgS));
+                    allFieldC = [allFieldC;...
+                        strcat(imgType,'_',fieldC{iField},'_',...
+                        fieldnames(featureForImgTypeS(patNum).(fieldC{iField}).AvgS))];
+                elseif strcmpi(featFieldC{1},'peak')
+                    featureForImgTypeS(patNum).(fieldC{iField}) = ...
+                        rmfield(featureForImgTypeS(patNum).(fieldC{iField}),'radius');
+                    featureForImgTypeS(patNum).(fieldC{iField}) = ...
+                        rmfield(featureForImgTypeS(patNum).(fieldC{iField}),'radiusUnit');
+                    featV = full(struct2array(featureS(patNum).(fieldC{iField})));
+                    allFieldC = [allFieldC; strcat(imgType,'_',...
+                        fieldC{iField},'_',fieldnames(featureForImgTypeS(patNum).(fieldC{iField})))];
+                else
+                    featV = full(struct2array(featureForImgTypeS(patNum).(fieldC{iField})));
+                    allFieldC = [allFieldC; strcat(imgType,'_',fieldC{iField},...
+                        '_',fieldnames(featureForImgTypeS(patNum).(fieldC{iField})))];
+                end
+                numNewFts = numFeats + length(featV);
+                featureM(patNum,numFeats+1:numNewFts) = featV;
+                numFeats = numNewFts;
             end
-            numNewFts = numFeats + length(featV);
-            %disp([numFeats numNewFts])
-            featureM(patNum,numFeats+1:numNewFts) = featV;
-            numFeats = numNewFts;
         end
     end
 end
