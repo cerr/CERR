c687c69475e450aad9231d79d8c6b1662c809fb0
rkpandya <rkpandya@syr.edu>
2019-04-04 16:24:35 -0400

    changes to accommodate feature extraction on a wavelet or LoG filtered image

--- Unit_Testing/tests_for_cerr/pyFeatureExtraction.py
+++ Unit_Testing/tests_for_cerr/pyFeatureExtraction.py
@@ -38,21 +38,33 @@ def extract(imagePath, maskPath, paramfilepath, preprocessingFilter, tempDir, di
 
 #if wavelet or LoG specified in argument, return only the preprocessed image arrays
   if preprocessingFilter == 'LoG':
-    resultdict = {}
+
     extractor = featureextractor.RadiomicsFeaturesExtractor(params)
     result = extractor.execute(imagePath, maskPath)
     sigmaValues = [3.0]
     for logImage, imageTypeName, inputKwargs in imageoperations.getLoGImage(image, mask, sigma=sigmaValues):
-      resultArray = sitk.GetArrayFromImage(logImage)
-      resultArray = matlab.double(resultArray.tolist())
-      imgName = tempDir + imageTypeName + '.nrrd'
+      #resultArray = sitk.GetArrayFromImage(logImage)
+      #resultArray = matlab.double(resultArray.tolist())
+      logImagePath = tempDir + imageTypeName + '.nrrd'
       try:
-        sitk.WriteImage(logImage, imgName)
+        sitk.WriteImage(logImage, logImagePath)
       except Exception as e:
         print("Couldn't write image to file (%s)." % e)
 
-      #resultdict[decompositionName] = resultArray
-    return resultArray
+    #return resultArray
+
+    extractor = featureextractor.RadiomicsFeaturesExtractor(params)
+
+    result = extractor.execute(logImagePath, maskPath)
+    for key, val in six.iteritems(result):
+      strval = str(key)
+      if strval.find("-") == -1:
+        continue
+      else:
+        strval = strval.replace("-", "_")
+        strval = strval.replace("-", "_")
+        result[strval] = result.pop(key)
+    return result
 
   elif preprocessingFilter == 'wavelet':
     resultdict = {}
@@ -61,31 +73,29 @@ def extract(imagePath, maskPath, paramfilepath, preprocessingFilter, tempDir, di
       waveletName = 'wavelet-' + dirStr
       if decompositionName == waveletName:
 
-        resultArray = sitk.GetArrayFromImage(decompositionImage)
-        resultArray = matlab.double(resultArray.tolist())
-        imgName = tempDir + decompositionName + '.nrrd'
+        #resultArray = sitk.GetArrayFromImage(decompositionImage)
+        #resultArray = matlab.double(resultArray.tolist())
+        waveletImagePath = tempDir + decompositionName + '.nrrd'
         try:
-          sitk.WriteImage(decompositionImage, imgName)
+          sitk.WriteImage(decompositionImage, waveletImagePath)
         except Exception as e:
           print("Couldn't write image to file (%s)." % e)
 
-    #   resultArray=sitk.GetArrayFromImage(decompositionImage[0][0])
-    #   #imgName = inputImageName+'.nrrd'
-    #   #sitk.WriteImage(decompositionImage,imgName)
-    #   resultArray = matlab.double(resultArray.tolist())
-    #   #resultdict[inputImageName] = resultArray
-    # for key, val in six.iteritems(resultArray):
-    #   strval = str(key)
-    #   if strval.find("-") == -1:
-    #     continue
-    #   else:
-    #     strval = strval.replace("-", "_")
-    #     strval = strval.replace("-", "_")
-    #     resultdict[strval] = resultdict.pop(key)
 
-    #return resultdict
-    return resultArray
-#perform feature extraction on original images and return the result
+    #return resultArray
+    extractor = featureextractor.RadiomicsFeaturesExtractor(params)
+
+    result = extractor.execute(waveletImagePath, maskPath)
+    for key, val in six.iteritems(result):
+      strval = str(key)
+      if strval.find("-") == -1:
+        continue
+      else:
+        strval = strval.replace("-", "_")
+        strval = strval.replace("-", "_")
+        result[strval] = result.pop(key)
+    return result
+
   else:    
     extractor = featureextractor.RadiomicsFeaturesExtractor(params)
 
@@ -106,7 +116,7 @@ def main():
   imagePath = 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\scan.nrrd'
   maskPath = 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\mask.nrrd'
   paramFilePath = 'W:\\Rutu\\CERR-testing\\Unit_Testing\\tests_for_cerr\pyradParams.yaml'
-  result = extract(imagePath, maskPath, paramFilePath, '', 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\', 'HHH')
+  result = extract(imagePath, maskPath, paramFilePath, 'LoG', 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\', '')
 
 
 main()
