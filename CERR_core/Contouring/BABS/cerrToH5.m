a23f33d28a86e6cf7e2e4233f1036795b75533f1
rkpandya <rkpandya@syr.edu>
2019-04-30 12:49:24 -0400

    Cleanup, Bug Fix

--- CERR_core/Contouring/BABS/cerrToH5.m
+++ CERR_core/Contouring/BABS/cerrToH5.m
@@ -1,4 +1,14 @@
 function cerrToH5(cerrPath, fullSessionPath)
+% Usage: cerrToH5(cerrPath, fullSessionPath)
+%
+% This function converts a 3d scan from planC to H5 file format 
+%
+% RKP, 3/21/2019
+%
+%INPUTS:
+%   cerrPath          : Path to the original CERR file to be converted
+%   fullSessionPath   : Path to write the H5 file
+
 
 planCfiles = dir(fullfile(cerrPath,'*.mat'));
 %create subdir within fullSessionPath for h5 files
@@ -24,46 +34,6 @@ for p=1:length(planCfiles)
             disp('dataset already exists in destination folder')
         end
     end
-
-% %%
-% % Use this to export masks for structures named in strucList
-% % mask will contain seperate binary arrays (need to combine later)
-%     strucList = ["BrainStem", "Chiasm", "Eye_L", "Eye_R", "Lens_R","Lens_L", "Cord", ...
-%                  "Esophagus", "Larynx", "Mandible", "Oral_Cav","OptNrv_L", ...
-%                  "OptNrv_R", "Parotid_L", "Parotid_R", "Submand_L", "Submand_R"];
-% 
-%     class_num = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
-%     %maskFile = fullfile(planCfiles(p).folder,strcat('MASK_',strrep(planCfiles(p).name,'.mat','.h5')));
-%     %fullSessionPath add below
-%     maskFile = fullfile(planCfiles(p).folder,strcat('MASK_',strrep(planCfiles(p).name,'.mat','.h5')));
-%     
-%     try 
-%         for l = 1:length(strucList)
-%             maskName = strcat('/','mask_',num2str(class_num(l)));
-%             h5create(maskFile, maskName, size(scan3M));
-%         end
-%     catch ME
-%         if (strcmp(ME.identifier, 'MATLAB:imagesci:h5create:datasetAlreadyExists'))
-%             disp('dataset already exists in destination folder')
-%         end
-%     end
-%     
-%     for l = 1:length(strucList)
-%         for structNum = 1:length(planC{4})
-%             strucName = planC{4}(structNum).structureName;
-%             if contains(upper(strucName), upper(strucList(l))) == 1 && contains(upper(strucName),'Z') == 0
-%                 m = zeros(size(scan3M));
-%                 [rasterSegments, planC, isError] = getRasterSegments(structNum,planC);
-%                 [mask3M, uniqueSlices] = rasterToMask(rasterSegments, scanNum, planC);
-%                 maskName = strcat('/','mask_',num2str(class_num(l)))
-%                 strucName
-%                 for slice = 1:length(uniqueSlices)
-%                     m(:,:,uniqueSlices(slice)) = mask3M(:,:,slice);
-%                 end
-%                 h5write(maskFile, maskName, uint8(m));
-%             end
-%         end
-%     end
 
 end
 
--- CERR_core/Contouring/BABS/joinH5CERR.m
+++ CERR_core/Contouring/BABS/joinH5CERR.m
@@ -1,27 +1,17 @@
 function res = joinH5CERR(segResultCERRPath, cerrPath, outputH5Path, configFilePath)
-% function res = joinH5CERR(segResultCERRPath, cerrPath, outputH5Path, configFilePath)
+% Usage: res = joinH5CERR(segResultCERRPath, cerrPath, outputH5Path, configFilePath)
 %
 % This function merges the segmentations from the respective algorithm back
 % into the original CERR file
 %
-% INPUT: 
-% cerrPath - path to the original CERR file to be segmented
-% segResultCERRPath - path to write CERR RTSTRUCT for resulting segmentation.
-% outputH5Path - path to the segmented structures saved in h5 file format
-% configFilePath - path to the config file of the specific algorithm being
-% used for segmentation
-% 
-%
-%
 % RKP, 3/21/2019
-
-
-
-% segResultCERRPath = 'E:\testing pipeline\results';
-% cerrPath =   'E:\testing pipeline\testing_purpose\session134717.186335.3568\ctCERR';
-% outputH5Path = 'E:\testing pipeline\testing_purpose\session134717.186335.3568\outputH5';
-% outputDicomPath = 'E:\testing pipeline\results';
-% config_file_path = '/lila/home/pandyar1/MR_Prostate_config.json';
+%
+% INPUTS:
+%   cerrPath          : Path to the original CERR file to be segmented
+%   segResultCERRPath : Path to write CERR RTSTRUCT for resulting segmentation.
+%   outputH5Path      : Path to the segmented structures saved in h5 file format
+%   configFilePath    : Path to the config file of the specific algorithm being
+%                       used for segmentation
 
 
 H5Files = dir(fullfile(outputH5Path,'*.h5'));
@@ -64,7 +54,7 @@ end
     
     for i = 1 : length(res.loadStructures)          
             maskM = flippedMask == i;
-            planC = maskToCERRStructure(maskM, isUniform, scanNum, res.loadStructures(i).structureName, planC);
+            planC = maskToCERRStructure(maskM, isUniform, scanNum, res.structToIndxMap(i).structureName, planC);
     end
         
     finalPlanCfilename = fullfile(segResultCERRPath, strrep(strrep(file.name, 'MASK_', ''),'h5', 'mat'))
