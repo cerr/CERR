15aea4952f6173f44757b50b9619d06e445ad643
rkpandya <rkpandya@syr.edu>
2019-05-16 10:59:27 -0400

    Wrapper and supporting files for Heart segmentation model using DeepLab and PyTorch

---
+++ CERR_core/Contouring/models/heart/modeling/backbone/__init__.py
@@ -0,0 +1,7 @@
+from modeling.backbone import resnet
+
+def build_backbone(backbone, output_stride, BatchNorm):
+    if backbone == 'resnet':
+        return resnet.ResNet101(output_stride, BatchNorm)
+    else:
+        raise NotImplementedError
