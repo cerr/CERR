e6ce1574e0f12dc33b98f41246f08b2817edd294
adityaapte <aditya.apte@gmail.com>
2019-06-24 16:31:08 -0400

    Knuth mean and variance

---
+++ CERR_core/Utilities/knuthMeanVar.m
@@ -0,0 +1,48 @@
+function [mu,varOut] = knuthMeanVar(val,newCalcFlag)
+% function [mu,varOut] = knuthMeanVar(val,newCalcFlag)
+%
+% Knuth method for mean and standard deviation (like ITK)
+%
+% Input: value to add to the existing mean and variance calculation.
+%
+% Output: Mean and variance (var(xV,1))
+%
+% Example:
+% newCalcFlag = true;
+% knuthMeanVar([],true);
+% valV = [1:10,100:105];
+% for i = 1:length(valV)
+%     [mu,varOut] = knuthMeanVar(valV(i));
+% end
+%
+% To start a new calculation:
+% newCalcFlag = true;
+% knuthMeanVar([],newCalcFlag);
+% valV = [1:5];
+% for i = 1:length(valV)
+%     [mu,varOut] = knuthMeanVar(valV(i));
+% end
+% 
+% APA, 6/24/2019
+
+if exist('newCalcFlag','var') && newCalcFlag
+    clear persistent n
+    clear persistent muPrev
+    clear persistent varN
+end
+
+persistent n;
+persistent muPrev;
+persistent varN;
+
+if isempty(muPrev)
+    muPrev = 0;
+    varN = 0;
+    n = 0;
+end
+n = n + 1;
+mu = muPrev + (val - muPrev)/n;
+varN = varN + (val-mu)*(val-muPrev);
+muPrev = mu;
+varOut = varN/n;
+
