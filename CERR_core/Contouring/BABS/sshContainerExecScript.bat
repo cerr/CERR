1651db799bd08bee3d06491e85417f84ba9391ae
rkpandya <rkpandya@syr.edu>
2019-07-01 18:01:48 -0400

    Debug print statements for executing container in client-server configuration

--- CERR_core/Contouring/BABS/sshContainerExecScript.bat
+++ CERR_core/Contouring/BABS/sshContainerExecScript.bat
@@ -9,19 +9,19 @@ set algorithm=%8
 
 @echo %algorithm%
 
-@echo here 1
+@echo Copying session dir from client to server...
 @echo %client_session_dir%
 @echo %server_session_dir%
 pscp -noagent -r %client_session_dir% %ssh_uname%@%ssh_server_name%:%server_session_dir%
-@echo "here 2"
 
+@echo Setting bind path for container
 set bindingPath=%server_session_dir%/%session_name%:/scratch
-@echo "here 3"
 
+@echo Executing container 
 plink -noagent -ssh %ssh_uname%@%ssh_server_name% (source  /admin/lsf/mph/conf/profile.lsf; bsub -R rusage[mem=30] -q research -n 1  -W 1:00  -gpu "num=1:gmem=4GB:mode=exclusive_process:mps=no:j_exclusive=yes" -Is singularity run --app %algorithm% --nv --bind  %bindingPath% %container_path% %server_session_dir%/%session_name%)
 
-@echo "here 4"
+@echo Copying result back to Client from Server
 pscp -noagent -r %ssh_uname%@%ssh_server_name%:%server_session_dir%/%session_name%/outputH5 %client_session_dir%
 
-@echo "here 5"
+@echo DONE
 
