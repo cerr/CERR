b2fd92eb848f07598b2b2b662d508991956fa943
aditiiyer <iyera@mskcc.org>
2019-09-04 15:00:29 -0400

    Added default inputs

--- CERR_core/DLSegmentationTraining/prepareSegDataset.m
+++ CERR_core/DLSegmentationTraining/prepareSegDataset.m
@@ -12,39 +12,61 @@ function errC = prepareSegDataset(paramFilename)
 % --- JSON fields ---:
 % inputFileType    : May be 'DICOM' or 'CERR'
 % inputDir         : Path to input data
-% outDir           : Path for writing intermediate files(CERR, HDF5)
+% outputDir        : Path for writing intermediate files(CERR, HDF5)
 % structList       : Names of structures to be segmented
-% dataSplit        : Train/Val/Test split passed as vector: [%train %val]  (%test = 100-%train-%val).
-% imageSizeForModel: Dimensions of output image required for model
+% dataSplit        : %Train/%Val/%Test split passed as vector [%train %val]
+%                    (%test = 100-%train-%val).
+% imageSizeForModel: Dimensions of output image required by model
+% resample         : Dictionary of resampling parameters-- resolutionXCm,
+%                    resolutionYcm, resolutionZCm and method.
+%                    Supported methods include 'sinc', 'none'.
 % resize           : Resize image as required by model
 %                    Supported methods: 'pad', 'bilinear', 'sinc', 'none'.
 % crop             : Dictionary of options for cropping with fields
 %                    'methods', 'params', 'operator'
 %                    Supported methods include 'crop_fixed_amt','crop_to_bounding_box',
-%                    'crop_to_str', 'crop_around_center', 'none'.                             :
+%                    'crop_to_str', 'crop_around_center', 'none'.
+% channels         : Dictionary with fields 'number', 'imageType' and 'append'.
+%                    Supported image types: 'original', 'coronal', 'sagittal'.
+%                    Supported methods of appending: 'repeat','2.5D','multiscan'.
+%
 % Example: See sample_train_params.json
 %---------------------------------------------------------------------------
 % AI 9/3/19 Added inputFileType, resampling
+% AI 9/4/19 Added options to populate channels
 
 %% Get user inputs from JSON
 userInS = jsondecode(fileread(paramFilename));
 inputFileType = userInS.inputFileType;
 inputDir = userInS.inputDir;
-outDir = userInS.outDir;
+outputDir = userInS.outputDir;
 strListC = userInS.structList;
 dataSplitV = userInS.dataSplit;
 
+%Set defaults for optional inputs
+defaultS = struct();
+defaultS.crop.method = 'none';
+defaultS.imageSizeForModel = [];
+defaultS.resize.method = 'none';
+defaultS.resample.method = 'none';
+defaultS.channels.imageType = 'original';
+defaultS.channels.append.method = 'none';
+
 userOptS = struct();
-userOptS.crop = userInS.crop;
-userOptS.outSize = userInS.imageSizeForModel;
-userOptS.resizeMethod = userInS.resize.method;
-userOptS.resample = userInS.resample;
+defC = fieldnames(defaultS);
+for n = 1:length(defC)
+    if isfield(userInS,defC{n})
+        userOptS.(defC{n}) = userInS.(defC{n});
+    else
+        userOptS.(defC{n}) = defaultS.(defC{n});
+    end
+end
 
 %% Create directories to write CERR, HDF5 files
 fprintf('\nCreating directories for HDF5 files...\n');
-mkdir(outDir)
+mkdir(outputDir)
 
-HDF5path = fullfile(outDir,'dataHDF5');
+HDF5path = fullfile(outputDir,'dataHDF5');
 mkdir(HDF5path)
 
 mkdir([HDF5path,filesep,'Train']);
@@ -61,7 +83,7 @@ fprintf('\nComplete\n');
 %% Import data to CERR
 if strcmpi(inputFileType,'DICOM')
     
-    CERRpath = fullfile(outDir,'dataCERR');
+    CERRpath = fullfile(outputDir,'dataCERR');
     mkdir(CERRpath)
     
     zipFlag = 'No';
