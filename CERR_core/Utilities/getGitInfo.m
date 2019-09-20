bd6eb0455731abe983266a8b5c26a70591a9bb85
adityaapte <aditya.apte@gmail.com>
2019-09-20 15:59:42 -0400

    decommissioned

--- CERR_core/Utilities/getGitInfo.m
+++ CERR_core/Utilities/getGitInfo.m
@@ -60,6 +60,9 @@ function [remoteGitHash, localGitInfo] = getGitInfo()
 localGitInfo=[];
 remoteGitHash = '';
 
+
+%================== decomissioned for now. Test in more detail.
+
 if isdeployed
     %store date and hash in file under bin
 return
@@ -106,82 +109,6 @@ localGitInfo.hash = SHA1{1}{1};
 %save branchname
 localGitInfo.branch=branchName;
 
-%Read in config file
-config=fileread(fullfile(gitPath,'config'));
-%Find everything space delimited
-temp=textscan(config,'%s','delimiter','\n');
-lines=temp{1};
-
-remote='';
-%Lets find the name of the remote corresponding to our branchName
-for k=1:length(lines)
-    
-    %Are we at the section describing our branch?
-    if strcmp(lines{k},['[branch "' branchName '"]'])
-        m=k+1;
-        %While we haven't run out of lines
-        %And while we haven't run into another section (which starts with
-        % an open bracket)
-        while (m<=length(lines) && ~strcmp(lines{m}(1),'[') )
-            temp=textscan(lines{m},'%s');
-            if length(temp{1})>=3
-                if strcmp(temp{1}{1},'remote') && strcmp(temp{1}{2},'=')
-                    %This is the line that tells us the name of the remote 
-                    remote=temp{1}{3};
-                end
-            end
-            
-            m=m+1;
-        end    
-    end
-end
-localGitInfo.remote=remote;
-
-
-url='';
-%Find the remote's url
-for k=1:length(lines)
-    
-    %Are we at the section describing our branch?
-    if strcmp(lines{k},['[remote "' remote '"]'])
-        m=k+1;
-        %While we haven't run out of lines
-        %And while we haven't run into another section (which starts with
-        % an open bracket)
-        while (m<=length(lines) && ~strcmp(lines{m}(1),'[') )
-            temp=textscan(lines{m},'%s');
-            if length(temp{1})>=3
-                if strcmp(temp{1}{1},'url') && strcmp(temp{1}{2},'=')
-                    %This is the line that tells us the name of the remote 
-                    url=temp{1}{3};
-                end
-            end
-            
-            m=m+1;
-        end
-        
-    end
-end
-%localGitInfo.url=url;
-
-% [~,repName] = strtok(url,':');
-%url could also be 'git@github.com:cerr/CERR/commits/testing'
-%get to the commits page to view the remote commit hash
-%url = 'git@github.com:cerr/CERR';
-
-%if ~isempty(url) && contains(url, '.git')
-% use "strfind" instead of "contains" for older versions of matlab
-if ~isempty(url) && strfind(url, '.git')
-    commitUrl = strrep(url,'.git','/commits/');
-    url = strcat(commitUrl,localGitInfo.branch); 
-elseif ~isempty(url)
-    commitUrl = strcat(url,'/commits/');
-    url = strcat(commitUrl,localGitInfo.branch); 
-end
-% url = ['https://github.com/',commitUrl];
-
-localGitInfo.url=url;
-
 %obtain the date when the .git directory was last updated 
 % cerrPath = getCERRPath;
 % gitPath = strrep(cerrPath, 'CERR_core\', '.git');
@@ -193,22 +120,112 @@ try
 catch
     localGitInfo.date = '';
 end
-% %check for internet connection
-% [~,b]= dos('ping -n 1 www.google.com');
-% n=strfind(b,'Lost');
-% n1=b(n+7);
-% if(n1=='0')    
-%     return;
-% else
-try
-    %read remote git information based on url
-    weburl = strcat('https://github.com/cerr/CERR/commits/',localGitInfo.branch);
-    data = webread(weburl,'term','commit:');
-    commitIdIndex = strfind(data, 'commit:');
-    remoteGitHash = data(commitIdIndex(1)+7:commitIdIndex(1)+46);
-catch
-    remoteGitHash = '';
-end
 
 
-
+% 
+% %Read in config file
+% config=fileread(fullfile(gitPath,'config'));
+% %Find everything space delimited
+% temp=textscan(config,'%s','delimiter','\n');
+% lines=temp{1};
+% 
+% remote='';
+% %Lets find the name of the remote corresponding to our branchName
+% for k=1:length(lines)
+%     
+%     %Are we at the section describing our branch?
+%     if strcmp(lines{k},['[branch "' branchName '"]'])
+%         m=k+1;
+%         %While we haven't run out of lines
+%         %And while we haven't run into another section (which starts with
+%         % an open bracket)
+%         while (m<=length(lines) && ~strcmp(lines{m}(1),'[') )
+%             temp=textscan(lines{m},'%s');
+%             if length(temp{1})>=3
+%                 if strcmp(temp{1}{1},'remote') && strcmp(temp{1}{2},'=')
+%                     %This is the line that tells us the name of the remote 
+%                     remote=temp{1}{3};
+%                 end
+%             end
+%             
+%             m=m+1;
+%         end    
+%     end
+% end
+% localGitInfo.remote=remote;
+% 
+% 
+% url='';
+% %Find the remote's url
+% for k=1:length(lines)
+%     
+%     %Are we at the section describing our branch?
+%     if strcmp(lines{k},['[remote "' remote '"]'])
+%         m=k+1;
+%         %While we haven't run out of lines
+%         %And while we haven't run into another section (which starts with
+%         % an open bracket)
+%         while (m<=length(lines) && ~strcmp(lines{m}(1),'[') )
+%             temp=textscan(lines{m},'%s');
+%             if length(temp{1})>=3
+%                 if strcmp(temp{1}{1},'url') && strcmp(temp{1}{2},'=')
+%                     %This is the line that tells us the name of the remote 
+%                     url=temp{1}{3};
+%                 end
+%             end
+%             
+%             m=m+1;
+%         end
+%         
+%     end
+% end
+% %localGitInfo.url=url;
+% 
+% % [~,repName] = strtok(url,':');
+% %url could also be 'git@github.com:cerr/CERR/commits/testing'
+% %get to the commits page to view the remote commit hash
+% %url = 'git@github.com:cerr/CERR';
+% 
+% %if ~isempty(url) && contains(url, '.git')
+% % use "strfind" instead of "contains" for older versions of matlab
+% if ~isempty(url) && strfind(url, '.git')
+%     commitUrl = strrep(url,'.git','/commits/');
+%     url = strcat(commitUrl,localGitInfo.branch); 
+% elseif ~isempty(url)
+%     commitUrl = strcat(url,'/commits/');
+%     url = strcat(commitUrl,localGitInfo.branch); 
+% end
+% % url = ['https://github.com/',commitUrl];
+% 
+% localGitInfo.url=url;
+% 
+% %obtain the date when the .git directory was last updated 
+% % cerrPath = getCERRPath;
+% % gitPath = strrep(cerrPath, 'CERR_core\', '.git');
+% 
+% try
+%     fileInfo = dir(gitPath);
+%     timeStamp = fileInfo.date;
+%     localGitInfo.date = timeStamp;
+% catch
+%     localGitInfo.date = '';
+% end
+% % %check for internet connection
+% % [~,b]= dos('ping -n 1 www.google.com');
+% % n=strfind(b,'Lost');
+% % n1=b(n+7);
+% % if(n1=='0')    
+% %     return;
+% % else
+% try
+%     %read remote git information based on url
+%     weburl = strcat('https://github.com/cerr/CERR/commits/',localGitInfo.branch);
+%     data = webread(weburl,'term','commit:');
+%     commitIdIndex = strfind(data, 'commit:');
+%     remoteGitHash = data(commitIdIndex(1)+7:commitIdIndex(1)+46);
+% catch
+%     remoteGitHash = '';
+% end
+% 
+% 
+% 
