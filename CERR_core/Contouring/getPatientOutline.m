add4f00ed92bd0bc06bd723dbc7dfdcb5c8ed0f3
aditiiyer <iyera@mskcc.org>
2019-09-26 14:05:14 -0400

    Fill in mask on slices where missing

--- CERR_core/Contouring/getPatientOutline.m
+++ CERR_core/Contouring/getPatientOutline.m
@@ -23,22 +23,26 @@ threshold = prctile(scanThreshV,5);
 sizV = size(scan3M);
 imageCenterRow = sizV(1)/2;
 
+%% Extract mask
+minMaskSiz = 1500;
 minDistV = nan([numel(slicesV),1]);
 idxC = cell(1,numel(slicesV));
 for n = 1:numel(slicesV)
     
+    %Threshold image
     y = scan3M(:,:,slicesV(n))>threshold;
     y = imdilate(y,strel('disk',4,8));
     y = imfill(y,'holes');
     
-    
+    %Identify largest connected component
     cc = bwconncomp(y);
     ccSiz = cellfun(@numel,[cc.PixelIdxList]);
     %sel = ccSiz==max(ccSiz);
     
-    % Select the component closest to image center
-    selV = find(ccSiz > 1500);
-    %maskM = false(size(y));
+    %Exclude masks with fewer voxels than minMaskSiz 
+    selV = find(ccSiz > minMaskSiz);
+    
+    %Record distance of centroid of largest CC from image center
     if ~isempty(selV)
         
         rowMedianV = nan(1,length(selV));
@@ -50,17 +54,22 @@ for n = 1:numel(slicesV)
         [minDistV(n),indMin] = min(distV);
         sel = selV(indMin);
         
+        %Record pixel indices of largest CC
         idxC{n} = cc.PixelIdxList{sel};
     end
     
 end
 
-% Compute deviance of row centroid across all the slices
+
+%If distance of row centroid of largest CC from image center is too large, 
+%it is excluded
+
+%- Compute deviance of row centroid across all the slices
 globalMeanDist = nanmean(minDistV); 
 distDev = nanstd(minDistV,1);
 
 ptMask3M = false(size(scan3M));
-% Filter slices if they deviate too much from the centroid
+%- Filter slices if they deviate too much from the centroid
 for n = 1:numel(slicesV)
     maskM = false(size(y));
     if minDistV(n) > globalMeanDist-distDev  ||  minDistV(n) < globalMeanDist+distDev    
@@ -68,5 +77,32 @@ for n = 1:numel(slicesV)
     end
     ptMask3M(:,:,slicesV(n)) = maskM;    
 end
+
+%Fill in mask on missing slices
+maskSlicesV = squeeze(sum(sum(ptMask3M))>0);
+mins = find(maskSlicesV,1,'first');
+maxs = find(maskSlicesV,1,'last');
+maskSlicesV = mins : maxs;
+
+missingIdxV = squeeze(sum(sum(ptMask3M(:,:,maskSlicesV)))==0);
+missingSlicesV = maskSlicesV(missingIdxV);
+
+for n = 1:length(missingSlicesV)
+    
+    prevMaskM  = ptMask3M(:,:,missingSlicesV(n)-1);
+    nextMaskM = ptMask3M(:,:,missingSlicesV(n)+1);
+    
+    if sum(prevMaskM(:))==0
+        maskM = nextMaskM;
+    elseif sum(nextMaskM(:))==0
+        maskM = prevMaskM;
+    else
+        maskM = prevMaskM & nextMaskM;
+    end
+    
+    ptMask3M(:,:,missingSlicesV(n)) = maskM;
+    
+end
+
 
 end
