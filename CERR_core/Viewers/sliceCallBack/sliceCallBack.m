278a2c4e3c2d78a2aaadefacf0a0b0b171ac58f7
adityaapte <aditya.apte@gmail.com>
2019-09-30 11:09:19 -0400

    increased number of plane locater handles to 20

--- CERR_core/Viewers/sliceCallBack/sliceCallBack.m
+++ CERR_core/Viewers/sliceCallBack/sliceCallBack.m
@@ -1234,7 +1234,7 @@ switch upper(instr)
         stateS.handle.CERRAxisPlnLoc{axisNum} = [];
         stateS.handle.CERRAxisPlnLocSdw{axisNum} = [];
         
-        for count = 1:10
+        for count = 1:20
             if stateS.MLVersion < 8.4
                 stateS.handle.CERRAxisPlnLocSdw{axisNum}(count) = line([0.02 0.1], [0.02 0.02], [2 2], 'parent', stateS.handle.CERRAxis(axisNum), 'Color', [0 0 0], 'tag', 'planeLocatorShadow', 'userdata', {'horz', 'trans', axisNum}, 'hittest', 'off', 'linewidth', 1, 'erasemode','xor');
                 stateS.handle.CERRAxisPlnLoc{axisNum}(count) = line([0.02 0.1], [0.02 0.02], [2 2], 'parent', stateS.handle.CERRAxis(axisNum), 'Color', [0 0 0], 'tag', 'planeLocator',  'buttondownfcn', 'sliceCallBack(''locatorClicked'')', 'userdata', {'vert', 'trans', axisNum}, 'linewidth', 1, 'erasemode','xor');
