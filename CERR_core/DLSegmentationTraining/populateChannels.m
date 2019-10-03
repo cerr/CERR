47a01bbc9a1d8f999d3e78f305ed357abd5e17d6
aditiiyer <iyera@mskcc.org>
2019-10-03 14:25:31 -0400

    Modified to populate channels with specified slice

--- CERR_core/DLSegmentationTraining/populateChannels.m
+++ CERR_core/DLSegmentationTraining/populateChannels.m
@@ -1,70 +1,46 @@
-function scanC = populateChannels(scanC,channelS)
+function channelC = populateChannels(scanC,channelS)
 %populateChannels.m
 %
-% Returns cell array of transformed images corresponding to channels.
+% Returns cell array of images corresponding to channels.
 %
 % AI 9/4/19
 
-C = channelS.number;
+numChannels = length(channelS);
 
-%% Transform images as required
-imgType = channelS.imageType;
-switch imgType
-    
-    case 'coronal'
-        for n = 1:length(scanC)
-            scan3M = scanC{n};
-            scan3M = permute(scan3M,[3,2,1]);
-            scanC{n} = scan3M;
-        end
-        
-    case 'sagittal'
-        for n = 1:length(scanC)
-            scan3M = scanC{n};
-            scan3M = permute(scan3M,[3,1,2]);
-            scanC{n} = scan3M;
-        end
-        
-    case 'original'
-        %Do nothing
-end
 
 %% Populate channels
-method = channelS.append.method;
+channelC = cell(numChannels,1);
 
-switch method
+for c = 1:numChannels
     
-    case 'repeat'
+    slice = channelS(c).slice;
+    
+    if contains(slice,'current-')
         
-        scan3M = scanC{1};
-        for c = 2:C
-            scanC{c} = scan3M;
-        end
+        scan3M = scanC{c};
+        idx = strfind(slice,'-');
+        shift = str2num(slice(idx+1:end));
+        shiftSlice3M = circshift(scan3M,shift,3);
+        shiftSlice3M(:,:,1:shift) = repmat(scan3M(:,:,1),[1,1,shift]);
+        channelC{c} = shiftSlice3M;
         
-    case '2.5D'
+    elseif contains(slice,'current+')
         
-        scan3M = scanC{1};
+        scan3M = scanC{c};
+        idx = strfind(slice,'+');
+        shift = -str2num(slice(idx+1:end));
+        shiftSlice3M = circshift(scan3M,shift,3);
+        shiftSlice3M(:,:,end+shift+1:end) = repmat(scan3M(:,:,end),[1,1,-shift]);
+        channelC{c} = shiftSlice3M;
         
-        nPad = floor(C/2);
-        shiftV = nPad:-1:-nPad;
+    elseif strcmpi(slice,'current')
         
-        for c = 1:C
-            shiftSlice3M = circshift(scan3M,shiftV(c),3);
-            if shiftV(c)>0
-                shiftSlice3M(:,:,1:shiftV(c)) = repmat(scan3M(:,:,1),[1,1,shiftV(c)]);
-            elseif shiftV(c)<0
-                shiftSlice3M(:,:,end+shiftV(c)+1:end) = repmat(scan3M(:,:,end),[1,1,-shiftV(c)]);
-            end
-            scanC{c} = shiftSlice3M;
-        end
+        channelC{c} = scanC{c};
         
+    else
         
-    case 'multiscan'
-        %Same as input
+        error(['Slice ', slice, ' not supported.']);
         
-    case 'none'
-        %Do nothing
-        
-end
-
+    end
+    
 end
