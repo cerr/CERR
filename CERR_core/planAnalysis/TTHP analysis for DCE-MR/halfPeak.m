52e087497d498731e042608069598c824eeaa375
aditiiyer <iyera@mskcc.org>
2019-11-22 15:12:58 -0500

    Added comments

--- CERR_core/planAnalysis/TTHP analysis for DCE-MR/halfPeak.m
+++ CERR_core/planAnalysis/TTHP analysis for DCE-MR/halfPeak.m
@@ -11,18 +11,21 @@ function [resampSigM,t50V,s50V,timeOutV] = halfPeak(ROIDataM,timeV,smoothFlag,re
 % resampFlag : Flag (1-Resample  0-No resampling )
 % =====================================================================================
 
-%Smooth/resample if selected
+%% Smooth/resample if selected
 nVox = size(ROIDataM,1);
 [resampSigM,timeOutV] = smoothResamp(ROIDataM,timeV,smoothFlag,resampFlag);
 normalizedBaseline = 1;
 changeSigM = resampSigM - normalizedBaseline;
 
-%Compute t50,s50
+%% Compute t50,s50
+%Identify first time point where changeSig > half-peak
 gts50 = bsxfun(@ge,changeSigM,(max(resampSigM,[],2) - normalizedBaseline)/2);
 [~,s50ColIdx] = max(gts50,[],2);
 s50rowIdx = (1:nVox).';
 s50Idx = sub2ind([nVox,numel(timeOutV)],s50rowIdx,s50ColIdx);
+%Get signal at half-peak
 s50V = resampSigM(s50Idx).';
+%Get time to half-peak
 t50V = timeOutV(1,s50ColIdx);
 
 end
