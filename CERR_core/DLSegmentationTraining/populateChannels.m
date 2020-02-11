3853cd817005306be2388158c88c5b2e6bd38bb0
aditiiyer <iyera@mskcc.org>
2020-02-11 15:39:31 -0500

    Updates to handle multiple views

--- CERR_core/DLSegmentationTraining/populateChannels.m
+++ CERR_core/DLSegmentationTraining/populateChannels.m
@@ -1,46 +1,55 @@
-function channelC = populateChannels(scanC,channelS)
+function scanViewC = populateChannels(scanViewC,channelS)
 %populateChannels.m
 %
 % Returns cell array of images corresponding to channels.
 %
 % AI 9/4/19
 
+nViews = length(scanViewC);
 numChannels = length(channelS);
 
 
 %% Populate channels
-channelC = cell(numChannels,1);
 
-for c = 1:numChannels
+for i = 1:nViews
     
-    slice = channelS(c).slice;
+    scanC = scanViewC{i};
+    channelC = cell(numChannels,1);
     
-    if contains(slice,'current-')
+    for c = 1:numChannels
         
-        scan3M = scanC{c};
-        idx = strfind(slice,'-');
-        shift = str2num(slice(idx+1:end));
-        shiftSlice3M = circshift(scan3M,shift,3);
-        shiftSlice3M(:,:,1:shift) = repmat(scan3M(:,:,1),[1,1,shift]);
-        channelC{c} = shiftSlice3M;
+        slice = channelS(c).slice;
         
-    elseif contains(slice,'current+')
-        
-        scan3M = scanC{c};
-        idx = strfind(slice,'+');
-        shift = -str2num(slice(idx+1:end));
-        shiftSlice3M = circshift(scan3M,shift,3);
-        shiftSlice3M(:,:,end+shift+1:end) = repmat(scan3M(:,:,end),[1,1,-shift]);
-        channelC{c} = shiftSlice3M;
-        
-    elseif strcmpi(slice,'current')
-        
-        channelC{c} = scanC{c};
-        
-    else
-        
-        error(['Slice ', slice, ' not supported.']);
+        if contains(slice,'current-')
+            
+            scan3M = scanC{c};
+            idx = strfind(slice,'-');
+            shift = str2num(slice(idx+1:end));
+            shiftSlice3M = circshift(scan3M,shift,3);
+            shiftSlice3M(:,:,1:shift) = repmat(scan3M(:,:,1),[1,1,shift]);
+            channelC{c} = shiftSlice3M;
+            
+        elseif contains(slice,'current+')
+            
+            scan3M = scanC{c};
+            idx = strfind(slice,'+');
+            shift = -str2num(slice(idx+1:end));
+            shiftSlice3M = circshift(scan3M,shift,3);
+            shiftSlice3M(:,:,end+shift+1:end) = repmat(scan3M(:,:,end),[1,1,-shift]);
+            channelC{c} = shiftSlice3M;
+            
+        elseif strcmpi(slice,'current')
+            
+            channelC{c} = scanC{c};
+            
+        else
+            
+            error(['Slice ', slice, ' not supported.']);
+            
+        end
         
     end
+    scanViewC{i} = channelC;
+    
     
 end
