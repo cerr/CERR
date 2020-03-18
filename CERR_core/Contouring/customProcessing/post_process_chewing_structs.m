0366676992a3ff04cf0b9382c1e83a19c51f198e
aditiiyer <iyera@mskcc.org>
2020-03-18 11:18:16 -0400

    Updated to skip empty labels

--- CERR_core/Contouring/customProcessing/post_process_chewing_structs.m
+++ CERR_core/Contouring/customProcessing/post_process_chewing_structs.m
@@ -9,47 +9,48 @@ procMask3M = zeros(size(mask3M));
 labelV = [0 1 2 3 4];
 conn = 26;
 
-for l = 2:length(labelV) 
+for l = 2:length(labelV)
     
     strMask3M = zeros(size(mask3M));
     
     label3M = mask3M == l-1;
     
     %Fill holes
-    label3M = imfill(label3M,conn,'holes');
-    
-    %Retain largest connected component
-    connCompS = bwconncomp(label3M,conn);
-    ccSiz = cellfun(@numel,[connCompS.PixelIdxList]);
-    sel = ccSiz==max(ccSiz);
-    if ~ (isempty(label3M(sel)) | max(ccSiz)< 1000) %for larynx, mm, pm
-        idx = connCompS.PixelIdxList{sel};
-        strMask3M(idx) = l-1;
-    end
-    
-    %Morph proc
-    for n = 1:size(strMask3M,3)
+    if any(label3M(:))
+        label3M = imfill(label3M,conn,'holes');
         
-        strMaskM = double(strMask3M(:,:,n)== l-1);
-        
-        
-        labelM = imopen(strMaskM,strel('disk',2)); %for larynx, mm,pm
-        labelM = imfill(labelM,'holes');
-        
-        cc = bwconncomp(labelM);
-        ccSiz = cellfun(@numel,[cc.PixelIdxList]);
+        %Retain largest connected component
+        connCompS = bwconncomp(label3M,conn);
+        ccSiz = cellfun(@numel,[connCompS.PixelIdxList]);
         sel = ccSiz==max(ccSiz);
-        if ~ (isempty(label3M(sel)) | max(ccSiz)< 5)
-            idx = cc.PixelIdxList{sel};
-            labelM = zeros(size(labelM));
-            labelM(idx) = l-1;
-            strMask3M(:,:,n) = labelM;
-        else
-            strMask3M(:,:,n) = zeros(size(labelM));
+        if ~ (isempty(label3M(sel)) | max(ccSiz)< 1000) %for larynx, mm, pm
+            idx = connCompS.PixelIdxList{sel};
+            strMask3M(idx) = l-1;
         end
         
+        %Morph proc
+        for n = 1:size(strMask3M,3)
+            
+            strMaskM = double(strMask3M(:,:,n)== l-1);
+            
+            
+            labelM = imopen(strMaskM,strel('disk',2)); %for larynx, mm,pm
+            labelM = imfill(labelM,'holes');
+            
+            cc = bwconncomp(labelM);
+            ccSiz = cellfun(@numel,[cc.PixelIdxList]);
+            sel = ccSiz==max(ccSiz);
+            if ~ (isempty(label3M(sel)) | max(ccSiz)< 5)
+                idx = cc.PixelIdxList{sel};
+                labelM = zeros(size(labelM));
+                labelM(idx) = l-1;
+                strMask3M(:,:,n) = labelM;
+            else
+                strMask3M(:,:,n) = zeros(size(labelM));
+            end
+            
+        end
     end
-    
     
     procMask3M =  procMask3M + strMask3M;
     
