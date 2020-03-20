5b907c407dfca836a26d47256f790c48ce168298
adityaapte <aditya.apte@gmail.com>
2020-03-20 01:14:02 -0400

    Retained 20 significant digits. This fixes errors in warping scans and doses using Plastimatch.

--- CERR_core/ImageRegistration/write_bspline_coeff_file.m
+++ CERR_core/ImageRegistration/write_bspline_coeff_file.m
@@ -12,20 +12,20 @@ bsp_vox_per_rgn         = bsplineParamsS.bsp_vox_per_rgn;
 bsp_direction_cosines   = bsplineParamsS.bsp_direction_cosines;
 bsp_coefficients        = bsplineParamsS.bsp_coefficients;
 
+fid = fopen(bspFileName,'wb');
 
-fileC{1,1} = 'MGH_GPUIT_BSP <experimental>';
-fileC{2,1} = ['img_origin = ', num2str(bsp_img_origin(1)),' ',num2str(bsp_img_origin(2)),' ', num2str(bsp_img_origin(3))];
-fileC{3,1} = ['img_spacing = ', num2str(bsp_img_spacing(1)),' ',num2str(bsp_img_spacing(2)),' ',num2str(bsp_img_spacing(3))];
-fileC{4,1} = ['img_dim = ', num2str(bsp_img_dim(1)),' ',num2str(bsp_img_dim(2)),' ',num2str(bsp_img_dim(3))];
-fileC{5,1} = ['roi_offset = ', num2str(bsp_roi_offset(1)),' ',num2str(bsp_roi_offset(2)),' ',num2str(bsp_roi_offset(3))];
-fileC{6,1} = ['roi_dim = ', num2str(bsp_roi_dim(1)),' ',num2str(bsp_roi_dim(2)),' ',num2str(bsp_roi_dim(3))];
-fileC{7,1} = ['vox_per_rgn = ', num2str(bsp_vox_per_rgn(1)),' ',num2str(bsp_vox_per_rgn(2)),' ',num2str(bsp_vox_per_rgn(3))];
-fileC{8,1} = ['direction_cosines = ', num2str(bsp_direction_cosines(1)),' ',num2str(bsp_direction_cosines(2)),' ',num2str(bsp_direction_cosines(3)),' ',num2str(bsp_direction_cosines(4)),' ',num2str(bsp_direction_cosines(5)),' ',num2str(bsp_direction_cosines(6)),' ',num2str(bsp_direction_cosines(7)),' ',num2str(bsp_direction_cosines(8)),' ',num2str(bsp_direction_cosines(9))];
-for i = 1:length(bsp_coefficients)
-    fileC{end+1,1} = num2str(bsp_coefficients(i));
-end
+fprintf(fid,'MGH_GPUIT_BSP <experimental>\n');
+fprintf(fid,['img_origin = ', sprintf('%0.20g ',bsp_img_origin),'\n']);
+fprintf(fid,['img_spacing = ', sprintf('%0.20g ',bsp_img_spacing),'\n']);
+fprintf(fid,['img_dim = ', sprintf('%0.20g ',bsp_img_dim),'\n']);
+fprintf(fid,['roi_offset = ', sprintf('%0.20g ',bsp_roi_offset),'\n']);
+fprintf(fid,['roi_dim = ', sprintf('%0.20g ',bsp_roi_dim),'\n']);
+fprintf(fid,['vox_per_rgn = ', sprintf('%0.20g ',bsp_vox_per_rgn),'\n']);
+fprintf(fid,['direction_cosines = ', sprintf('%0.20g ',bsp_direction_cosines),'\n']);
+fprintf(fid,sprintf('%0.20g\n',bsp_coefficients));
 
-cell2file(fileC,bspFileName);
+%cell2file(fileC,bspFileName);
+fclose(fid);
 
 success = 1;
 
