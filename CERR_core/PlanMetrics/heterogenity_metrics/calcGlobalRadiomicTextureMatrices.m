96011a42be99f0987586b7dea5c4cecc4d775929
aditiiyer <iyera@mskcc.org>
2020-04-03 18:06:46 -0400

    Updated to be consistent with calcRadiomicsForImgType.m

--- CERR_core/PlanMetrics/heterogenity_metrics/calcGlobalRadiomicTextureMatrices.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/calcGlobalRadiomicTextureMatrices.m
@@ -22,55 +22,62 @@ scanNum = getStructureAssociatedScan(structNum,planC);
     structNum, paramS, planC);
 
 %Quantize
-fieldListC = {'numGrayLevels','clipMin','clipMax','binWidth'};
+fieldListC = {'numGrLevels','clipMin','clipMax','binWidth'};
 for n = 1:length(fieldListC)
     if ~isfield(paramS.textureParamS,fieldListC{n})
         paramS.textureParamS.(fieldListC{n}) = [];
     end
 end
-quantized3M = imquantize_cerr(volToEval,paramS.textureParamS.numGrayLevels,...
+quantized3M = imquantize_cerr(volToEval,paramS.textureParamS.numGrLevels,...
     paramS.textureParamS.clipMin,paramS.textureParamS.clipMax,...
     paramS.textureParamS.binWidth);
 quantized3M(~maskBoundingBox3M) = NaN;
-
-
-%Compute texure matrices
-directionality = paramS.textureParamS.directionality;
-switch lower(directionality)
-    case '2d'
-        dirctn = 2;
-    case '3d'
-        dirctn = 1;
-    otherwise
-        error('Invalid input. Directionality must be "2D" or "3D"');
-end
-offsetsM = getOffsets(dirctn);
 
 %Calc texture matrices
 textureS = struct();
 
 if paramS.whichFeatS.glcm.flag
+    directionality = paramS.textureParamS.directionality;
+    switch lower(directionality)
+        case '2d'
+            dirctn = 2;
+        case '3d'
+            dirctn = 1;
+        otherwise
+            error('Invalid input. Directionality must be "2D" or "3D"');
+    end
+    offsetsM = getOffsets(dirctn);
     textureS.GLCM = calcCooccur(quantized3M, offsetsM, ...
-        paramS.textureParamS.numGrayLevels, paramS.textureParamS.avgType);
+        paramS.textureParamS.numGrLevels, paramS.textureParamS.avgType);
 end
 
 if paramS.whichFeatS.glrlm.flag
+    directionality = paramS.textureParamS.directionality;
+    switch lower(directionality)
+        case '2d'
+            dirctn = 2;
+        case '3d'
+            dirctn = 1;
+        otherwise
+            error('Invalid input. Directionality must be "2D" or "3D"');
+    end
+    offsetsM = getOffsets(dirctn);
     textureS.GLRLM = calcRLM(quantized3M, offsetsM,...
-        paramS.textureParamS.numGrayLevels, paramS.textureParamS.rlmType);
+        paramS.textureParamS.numGrLevels, paramS.textureParamS.rlmType);
 end
 
 if paramS.whichFeatS.gtdm.flag
-    textureS.GTDM = calcNGTDM(quantized3M, paramS.textureParamS.patchSize, ....
-        paramS.textureParamS.numGrayLevels,[]);
+    textureS.GTDM = calcNGTDM(quantized3M, paramS.textureParamS.patchRadiusVox, ....
+        paramS.textureParamS.numGrLevels,[]);
 end
 
 if paramS.whichFeatS.gldm.flag
     textureS.GLDM = calcCooccur(quantized3M, paramS.textureParamS.patchRadiusVox,...
-        paramS.textureParamS.numGrayLevels, paramS.textureParamS.imgDiffThresh, []);
+        paramS.textureParamS.numGrLevels, paramS.textureParamS.imgDiffThresh, []);
 end
 
 if paramS.whichFeatS.glszm.flag
-    textureS.GLSZM = calcCooccur(quantized3M, paramS.textureParamS.numGrayLevels,...
+    textureS.GLSZM = calcCooccur(quantized3M, paramS.textureParamS.numGrLevels,...
         paramS.textureParamS.szmType);
 end
 
