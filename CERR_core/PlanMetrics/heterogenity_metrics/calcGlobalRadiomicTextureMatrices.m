927e5d9e58f4943fa9c60113fda657c66e3dc5fd
aditiiyer <iyera@mskcc.org>
2020-04-14 13:22:13 -0400

    Bug fixes

--- CERR_core/PlanMetrics/heterogenity_metrics/calcGlobalRadiomicTextureMatrices.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/calcGlobalRadiomicTextureMatrices.m
@@ -1,18 +1,18 @@
-function [textureS,paramS] = calcGlobalRadiomicTextureMatrices(paramFilename, planC)
+function [textureS, paramS] = calcGlobalRadiomicTextureMatrices(paramFilename, planC)
 % Wrapper to extract global radiomics texture matrices using JSON config
 % file
 %--------------------------------------------------------------------------
 % INPUTS
-% paramFilename  :  PAth to JSON config file
+% paramFilename  :  Path to JSON config file
 % planC
-%
-% AI 4/2/2020 Created scripts for pre-processing & feature extraction
 %--------------------------------------------------------------------------
+% AI 4/2/2020 Created scripts for pre-processing & feature extraction
 
-%Read JSON parameter file
+
+%% Read JSON parameter file
 paramS = getRadiomicsParamTemplate(paramFilename);
 
-%Pre-processing
+%% Pre-processing
 indexS = planC{end};
 strC = {planC{indexS.structures}.structureName};
 strName = paramS.structuresC{1};
@@ -21,34 +21,31 @@ scanNum = getStructureAssociatedScan(structNum,planC);
 [volToEval,maskBoundingBox3M] =  preProcessForRadiomics(scanNum,...
     structNum, paramS, planC);
 
-%Quantization
+%% Quantization
 minClipIntensity = paramS.textureParamS.minClipIntensity;
 maxClipIntensity = paramS.textureParamS.maxClipIntensity;
 
-if quantizeFlag
-    numGrLevels = [];
-    binwidth = [];
-    if isfield(paramS.textureParamS,'numGrLevels')
-        numGrLevels = paramS.textureParamS.numGrLevels;
-    end
-    if isfield(paramS.textureParamS,'binwidth')
-        binwidth = paramS.textureParamS.binwidth;
-    end
-    % Don't use intensities outside the ROI in discretization
-    volToEval(~maskBoundingBox3M) = NaN;
-    quantized3M = imquantize_cerr(volToEval,numGrLevels,...
-        minClipIntensity,maxClipIntensity,binwidth);
-    % Reassign the number of gray levels in case they were computed for the
-    % passed binwidth
-    numGrLevels = max(quantized3M(:));
-    paramS.textureParamS.numGrLevels = numGrLevels;
-    
-else
-    quantized3M = volToEval;
+numGrLevels = [];
+binwidth = [];
+if isfield(paramS.textureParamS,'numGrLevels')
+    numGrLevels = paramS.textureParamS.numGrLevels;
 end
+if isfield(paramS.textureParamS,'binwidth')
+    binwidth = paramS.textureParamS.binwidth;
+end
+
+% Exclude intensities outside the ROI 
+volToEval(~maskBoundingBox3M) = NaN;
+quantized3M = imquantize_cerr(volToEval,numGrLevels,...
+    minClipIntensity,maxClipIntensity,binwidth);
 quantized3M(~maskBoundingBox3M) = NaN;
 
-%Calc texture matrices
+% Update number of gray levels if binwidth was specified
+numGrLevels = max(quantized3M(:));
+paramS.textureParamS.numGrLevels = numGrLevels;
+
+
+%% Calculate texture matrices
 textureS = struct();
 
 if paramS.whichFeatS.glcm.flag
@@ -71,7 +68,7 @@ if paramS.whichFeatS.glcm.flag
             %'Haralick features from separate cooccurrence matrix per direction, averaged'
             cooccurType = 2;
         otherwise
-            error('Invalid input. Directionality must be "2D" or "3D"');
+            error('Invalid input. Supported avg types: "texturematrix", "feature".');
     end
     
     offsetsM = getOffsets(dirctn);
@@ -96,16 +93,16 @@ end
 
 if paramS.whichFeatS.gtdm.flag
     textureS.GTDM = calcNGTDM(quantized3M, paramS.textureParamS.patchRadiusVox, ....
-        paramS.textureParamS.numGrLevels,[]);
+        paramS.textureParamS.numGrLevels);
 end
 
 if paramS.whichFeatS.gldm.flag
-    textureS.GLDM = calcCooccur(quantized3M, paramS.textureParamS.patchRadiusVox,...
-        paramS.textureParamS.numGrLevels, paramS.textureParamS.imgDiffThresh, []);
+    textureS.GLDM = calcNGLDM(quantized3M, paramS.textureParamS.patchRadiusVox,...
+        paramS.textureParamS.numGrLevels, paramS.textureParamS.imgDiffThresh);
 end
 
 if paramS.whichFeatS.glszm.flag
-    textureS.GLSZM = calcCooccur(quantized3M, paramS.textureParamS.numGrLevels,...
+    textureS.GLSZM = calcSZM(quantized3M, paramS.textureParamS.numGrLevels,...
         paramS.textureParamS.szmType);
 end
 
