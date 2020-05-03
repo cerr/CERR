6a793e5dfce973ce94a7ae7abf7f4b6d8d2d3b5a
adityaapte <aditya.apte@gmail.com>
2020-05-03 00:02:18 -0400

    updated to include latest changes in doseSummationMenu

--- CERR_Data_Extraction/sumDose.m
+++ CERR_Data_Extraction/sumDose.m
@@ -8,26 +8,30 @@ function planC = sumDose(doseNums,wtfactor,assocScan,newDoseName,planC)
 %APA, 12/23/2009
 %
 % Copyright 2010, Joseph O. Deasy, on behalf of the CERR development team.
-% 
+%
 % This file is part of The Computational Environment for Radiotherapy Research (CERR).
-% 
+%
 % CERR development has been led by:  Aditya Apte, Divya Khullar, James Alaly, and Joseph O. Deasy.
-% 
+%
 % CERR has been financially supported by the US National Institutes of Health under multiple grants.
-% 
-% CERR is distributed under the terms of the Lesser GNU Public License. 
-% 
+%
+% CERR is distributed under the terms of the Lesser GNU Public License.
+%
 %     This version of CERR is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
 %     the Free Software Foundation, either version 3 of the License, or
 %     (at your option) any later version.
-% 
+%
 % CERR is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 % without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 % See the GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
 % along with CERR.  If not, see <http://www.gnu.org/licenses/>.
+
+wtfactorTmp = wtfactor;
+wtfactor = ones(1,max(doseNums));
+wtfactor(doseNums) = wtfactorTmp;
 
 indexS = planC{end};
 if assocScan > 0
@@ -42,18 +46,20 @@ else %No Association
     assocTransM = eye(4);
 end
 
+% doseNums = checkedDoses;
+
 %Get the x,y,z grid for new dose
 for i = 1:length(doseNums)
     doseNum = doseNums(i);
     %Get x,y,z values for doseNum
     [xV, yV, zV] = getDoseXYZVals(planC{indexS.dose}(doseNum));
-
+    
     %Get the corners of the original dataset.
     [xCorn, yCorn, zCorn] = meshgrid([min(xV) max(xV)], [min(yV) max(yV)], [min(zV) max(zV)]);
-
+    
     %Add ones to the corners so we can apply a transformation matrix.
     corners = [xCorn(:) yCorn(:) zCorn(:) ones(prod(size(xCorn)), 1)];
-
+    
     %Apply transform to corners, so we know boundary of the slice.
     transM = getTransM(planC{indexS.dose}(doseNum),planC);
     if isempty(transM) || isequal(transM,eye(4))
@@ -70,9 +76,9 @@ for i = 1:length(doseNums)
     xRes{doseNum} = length(xV);
     yRes{doseNum} = length(yV);
     zRes{doseNum} = length(zV);
-
+    
     %Get associated scan
-    assocScanV{doseNum} = getAssociatedScan(planC{indexS.dose}(doseNum).assocScanUID, planC);
+    assocScanV{doseNum} = getAssociatedScan(planC{indexS.dose}(doseNum).assocScanUID);
 end
 newXgrid = linspace(min(cell2mat(xGrid)),max(cell2mat(xGrid)),max(cell2mat(xRes)));
 newYgrid = linspace(max(cell2mat(yGrid)),min(cell2mat(yGrid)),max(cell2mat(yRes)));
@@ -122,9 +128,9 @@ doseEmptyM = zeros([length(newYgrid),length(newXgrid)],'single');
 %Assume dose units are same as that of 1st dose
 doseUnits = getDoseUnitsStr(doseNums(1),planC);
 for i = 1:length(doseNums)
-
+    
     doseNum = doseNums(i);
-
+    
     %Check for transM
     if ~isempty(assocTransM) && ~isequal(assocTransM,eye(4))
         doseTtransM = getTransM('dose',doseNum,planC);
@@ -138,7 +144,14 @@ for i = 1:length(doseNums)
             inputTM = eye(4);
         end
     end
-
+    
+    SOPInstanceUIDv = {planC{indexS.beams}.SOPInstanceUID};
+    paramS.Tk.val = inf;         %Kick-off time of repopulation (days)
+    paramS.Tp.val = NaN;        %Potential tumor doubling time (days)
+    paramS.alpha.val = NaN;
+    paramS.abRatio.val = 10;  %alpha/beta
+    stdFractionSize = 2;
+    
     %Get the summation for this grid
     doseCombinedM = [];
     for iDoseAll = 1:length(doseIndC{doseNum})
@@ -155,26 +168,37 @@ for i = 1:length(doseNums)
         if isempty(doseOffset)
             doseOffset = 0;
         end
+        doseArray = single(getDoseArray(planC{indexS.dose}(iDose)) - doseOffset);
+        % Apply BED/EQD2 correction
+        ReferencedSOPInstanceUID = planC{indexS.dose}(iDose)...
+            .DICOMHeaders.ReferencedRTPlanSequence.Item_1.ReferencedSOPInstanceUID;
+        planNum = find(strcmpi(ReferencedSOPInstanceUID,SOPInstanceUIDv));
+        paramS.numFractions.val = planC{indexS.beams}(planNum).FractionGroupSequence...
+            .Item_1.NumberOfFractionsPlanned;
+        paramS.numFractions.val = double(paramS.numFractions.val);
+        paramS.frxSize.val = doseArray / paramS.numFractions.val;
+        doseArray = calc_BED(paramS) / (1+stdFractionSize/paramS.abRatio.val);
+        
         if ~isempty(doseCombinedM)
-            doseCombinedM = doseCombinedM + multFact * wtfactor(iDose) * single(getDoseArray(planC{indexS.dose}(iDose)) - doseOffset);
+            doseCombinedM = doseCombinedM + multFact * wtfactor(iDose) * doseArray;
         else
-            doseCombinedM = multFact * wtfactor(iDose) * single(getDoseArray(planC{indexS.dose}(iDose)) - doseOffset);
+            doseCombinedM = multFact * wtfactor(iDose) * doseArray;
         end
     end
-
+    
     %Check if this dose is on same grid as the new one
     %if isequal(xGrid{doseNum},newXgrid) && isequal(yGrid{doseNum},newYgrid) && isequal(zGrid{doseNum},newZgrid)
     chkLength = length(xGrid{doseNum})==length(newXgrid) && length(yGrid{doseNum})==length(newYgrid) && length(zGrid{doseNum})==length(newZgrid);
     if sum(sum((inputTM-eye(4)).^2)) < 1e-3 && chkLength && max(abs(xGrid{doseNum}-newXgrid)) < 1e-3 && max(abs(yGrid{doseNum}-newYgrid)) < 1e-3 && max(abs(zGrid{doseNum}-newZgrid)) < 1e-3
-
+        
         doseSumM = doseSumM + doseCombinedM;
         waitbar((i-1)/length(doseNums),hWait,['Calculating contribution from Dose ', num2str(doseNum)])
-
+        
     else %interpolation required
-
+        
         %Transform this dose
         doseTmpM = [];
-
+        
         for slcNum=1:length(newZgrid)
             [xV, yV, zV] = getDoseXYZVals(planC{indexS.dose}(doseNum));
             doseTmp = slice3DVol(doseCombinedM, xV, yV, zV, newZgrid(slcNum), 3, 'linear', inputTM, [], newXgrid, newYgrid);
@@ -185,11 +209,11 @@ for i = 1:length(doseNums)
             end
             waitbar((i-1)/length(doseNums) + (slcNum-1)/length(newZgrid)/length(doseNums) ,hWait,['Calculating contribution from Dose ', num2str(doseNum)])
         end
-
+        
         doseSumM = doseSumM + doseTmpM;
-
+        
     end
-
+    
 end
 
 delete(hWait)
@@ -230,3 +254,4 @@ planC{indexS.dose}(newDoseNum).coord2OFFirstPoint = newYgrid(1);
 planC{indexS.dose}(newDoseNum).coord3OfFirstPoint = newZgrid(1);
 planC{indexS.dose}(newDoseNum).zValues = newZgrid;
 planC{indexS.dose}(newDoseNum).doseUnits = doseUnits;
+
