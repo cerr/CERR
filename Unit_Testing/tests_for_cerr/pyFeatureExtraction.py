d2ddc1c3ad9afd70e926e22ae481effcd579ab9f
aditiiyer <iyera@mskcc.org>
2020-06-12 18:14:04 -0400

    Updated wrapper for feature extraction using pyradiomics

--- Unit_Testing/tests_for_cerr/pyFeatureExtraction.py
+++ Unit_Testing/tests_for_cerr/pyFeatureExtraction.py
@@ -4,124 +4,61 @@ from __future__ import print_function
 import SimpleITK as sitk
 import numpy
 import scipy
-#import matplotlib
 import matlab
-#import matlab.engine
 import six
+
+import radiomics
 from radiomics import getTestCase, featureextractor, imageoperations, firstorder
-from radiomics.featureextractor import RadiomicsFeaturesExtractor
 
+def extract(imagePath, maskPath, paramfilepath, tempDir):
+  """
 
-def extract(imagePath, maskPath, paramfilepath, preprocessingFilter, tempDir, dirStr):
-  r"""
   RKP - 3/20/2018
-  Wrapper class for calculation of a radiomics features using pyradiomics.
+  Wrapper function for calculation of a radiomics features using pyradiomics.
 
   """
-  # reading original image directly
-  #imageName, maskName = getTestCase('brain1')
+
+  # Read image and mask
+  print('Reading image and mask...')
   if imagePath is None or maskPath is None:  # Something went wrong, in this case PyRadiomics will also log an error
-    print('Error getting test matrix!')
-    exit()
-  # image = sitk.ReadImage(imageName)
-  # mask = sitk.ReadImage(maskName)
-
-  image = sitk.ReadImage(imagePath)
-  mask = sitk.ReadImage(maskPath)
-
-  image, mask = RadiomicsFeaturesExtractor().loadImage(imagePath, maskPath)
-
-  if imagePath is None or maskPath is None:  # Something went wrong, in this case PyRadiomics will also log an error
+    print('Error reading input image and mask')
     exit()
 
-  params = paramfilepath
+  # Instantiate the extractor using the settings file
+  print('Initializing feature extractor...')
+  extractor = featureextractor.RadiomicsFeatureExtractor(paramfilepath)
+  print('Done.')
 
-#if wavelet or LoG specified in argument, return only the preprocessed image arrays
-  if preprocessingFilter == 'LoG':
+  #Load and pre-process image and mask
+  image, mask = extractor.loadImage(imagePath, maskPath)
+  #image = sitk.ReadImage(imagePath)
+  #mask = sitk.ReadImage(maskPath)
+  
+  # Calc features
+  result = extractor.execute(image, mask)
+  
+  # Adjust fieldnames
+  newResult = adjustKeyNames(result)
+  return newResult
 
-    extractor = featureextractor.RadiomicsFeaturesExtractor(params)
-    result = extractor.execute(imagePath, maskPath)
-    sigmaValues = [3.0]
-    for logImage, imageTypeName, inputKwargs in imageoperations.getLoGImage(image, mask, sigma=sigmaValues):
-      #resultArray = sitk.GetArrayFromImage(logImage)
-      #resultArray = matlab.double(resultArray.tolist())
-      logImagePath = tempDir + imageTypeName + '.nrrd'
-      try:
-        sitk.WriteImage(logImage, logImagePath)
-      except Exception as e:
-        print("Couldn't write image to file (%s)." % e)
+def adjustKeyNames(py_dict):
+    """
 
-    #return resultArray
+    Replace hyphens with underscores in keys to produce valid matlab fieldnames 
+    AI 6/10/2020
 
-    extractor = featureextractor.RadiomicsFeaturesExtractor(params)
+    """
+    out_dict = py_dict.copy()
+    for key, val in py_dict.items():
+        strval = str(key)
+        if strval.find("-") == -1:
+         continue
+        else:
+            strval = strval.replace("-", "_")
+            strval = strval.replace("-", "_")
+            out_dict[strval] = out_dict.pop(key)
+    return out_dict
 
-    result = extractor.execute(logImagePath, maskPath)
-    for key, val in six.iteritems(result):
-      strval = str(key)
-      if strval.find("-") == -1:
-        continue
-      else:
-        strval = strval.replace("-", "_")
-        strval = strval.replace("-", "_")
-        result[strval] = result.pop(key)
-    return result
-
-  elif preprocessingFilter == 'wavelet':
-    resultdict = {}
-
-    for decompositionImage, decompositionName, inputKwargs in imageoperations.getWaveletImage(image, mask):
-      waveletName = 'wavelet-' + dirStr
-      if decompositionName == waveletName:
-
-        #resultArray = sitk.GetArrayFromImage(decompositionImage)
-        #resultArray = matlab.double(resultArray.tolist())
-        waveletImagePath = tempDir + decompositionName + '.nrrd'
-        try:
-          sitk.WriteImage(decompositionImage, waveletImagePath)
-        except Exception as e:
-          print("Couldn't write image to file (%s)." % e)
-
-
-    #return resultArray
-    extractor = featureextractor.RadiomicsFeaturesExtractor(params)
-
-    result = extractor.execute(waveletImagePath, maskPath)
-    for key, val in six.iteritems(result):
-      strval = str(key)
-      if strval.find("-") == -1:
-        continue
-      else:
-        strval = strval.replace("-", "_")
-        strval = strval.replace("-", "_")
-        result[strval] = result.pop(key)
-    return result
-
-  else:    
-    extractor = featureextractor.RadiomicsFeaturesExtractor(params)
-
-    result = extractor.execute(imagePath, maskPath)
-    for key, val in six.iteritems(result):
-      strval = str(key)
-      if strval.find("-") == -1:
-        continue
-      else:
-        strval = strval.replace("-", "_")
-        strval = strval.replace("-", "_")
-        result[strval] = result.pop(key)
-    return result
-
-#
 def main():
-
-  imagePath = 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\scan.nrrd'
-  maskPath = 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\mask.nrrd'
-  paramFilePath = 'W:\\Rutu\\CERR-testing\\Unit_Testing\\tests_for_cerr\pyradParams.yaml'
-  result = extract(imagePath, maskPath, paramFilePath, 'LoG', 'C:\\Users\\pandyar1\\AppData\\Local\\Temp\\', '')
-
-
-main()
-
-
-
-
-
+ print('Loaded modules.')
+  
