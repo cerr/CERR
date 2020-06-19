b9665bed23642b214f0f6a1596339ca34305e60a
aditiiyer <iyera@mskcc.org>
2020-06-19 12:51:10 -0400

    Function to compare radiomics features computed using  CERR vs. Pyradiomics

--- Unit_Testing/tests_for_cerr/compareCerrWithPyrad.m
+++ Unit_Testing/tests_for_cerr/compareCerrWithPyrad.m
@@ -1,41 +1,89 @@
-% compareCerrWithPyrad.m
+function [cerrFeatS,pyFeatS] = compareCerrWithPyrad(cerrParamFile,...
+    pyradParamFile,imageType,featureClass,planC)
 %
-% Compares CERR and pyRaiomics features
-%
-% APA, 9/4/2019
+% INPUTS
+% imageType: Filtered image type. E.g.: 'original','LoG',{'wavelet','HHH'}
+% featureClass: Scalar feature class. E.g.: 'firstorder', 'shape','glcm',
+%               'gldm','rlm','szm'.
+%--------------
+% Example
+%--------------
+% Load sample data
+%fpath = fullfile(fileparts(fileparts(getCERRPath)),...
+%        'Unit_Testing/data_for_cerr_tests/CERR_plans/head_neck_ex1_20may03.mat.bz2')
+%planC = loadPlanC(fpath,tempdir);
+%planC = updatePlanFields(planC);
+%planC = quality_assure_planC(fpath,planC);
 
-pradParamFileName = 'C:\Users\aptea\Desktop\PyradParamsComparison.yaml';
-cerrParamFileName = 'C:\Users\aptea\Desktop\CERR_ParamsComparison.json';
-nrrdSaveDir = 'C:\Users\aptea\Desktop';
+%pyradParamFile = 'M:\Aditi\PyradiomicsComparison\pytest1.yaml';
+%cerrParamFile = 'M:\Aditi\PyradiomicsComparison\cerrtest1.json';
+%[cerrFeatS,pyFeatS] = compareCerrWithPyrad(cerrParamFile,...
+%                      pyradParamFile,'original','firstorder',planC)
+%--------
 
-global planC
 indexS = planC{end};
 
-structNum = 1;
+% 1. Compute features using CERR
+
+%Read param file
+paramS = getRadiomicsParamTemplate(cerrParamFile);
+
+%Get structure name
+strName = paramS.structuresC;
+strName = strName{1};
+
+strC = {planC{indexS.structures}.structureName};
+structNum = getMatchingIndex(strName,strC,'exact');
 scanNum = getStructureAssociatedScan(structNum,planC);
+structFieldName = ['struct_',repSpaceHyp(strName)];
 
-scan3M = double(planC{indexS.scan}(scanNum).scanArray) - planC{indexS.scan}(scanNum).scanInfo(1).CTOffset;
-mask3M = getUniformStr(structNum,planC);
-[xV, yV, zV] = getScanXYZVals(planC{indexS.scan}(1));
-pixelspacing = [abs(mean(diff(xV))) abs(mean(diff(yV))) abs(mean(diff(zV)))]*10;
-origin = [0 0 0];
-encoding = 'raw';
-mask3M = cast(mask3M,'uint8');
-% Write scan and mask to nrrd
-scanFilename = fullfile(nrrdSaveDir,'scan.nrrd');
-maskFilename = fullfile(nrrdSaveDir,'mask.nrrd');
-ok1 = nrrdWriter(scanFilename, scan3M, pixelspacing, origin, encoding);
-ok2 = nrrdWriter(maskFilename, mask3M, pixelspacing, origin, encoding);
+%Compute features
+cerrCalcS.(structFieldName) = calcGlobalRadiomicsFeatures...
+    (scanNum, structNum, paramS, planC);
 
-pyradCallStr = ['pyradiomics ', scanFilename, ' ', maskFilename, ' --param ', pradParamFileName];
-system(pyradCallStr)
+if strcmpi(featureClass,'shape')
+    cerrFeatS = cerrCalcS.(structFieldName).shapeS;
+else
+    cerrFieldsC = fieldnames(cerrCalcS.(structFieldName));
+    if iscell(imageType)
+        imgClassIdx = true(length(cerrFieldsC),1);
+        for n = 1:length(imageType)
+            imgClassIdx = imgClassIdx & contains(lower(cerrFieldsC),lower(imageType{n}));
+        end
+    else
+        imgClassIdx = contains(lower(cerrFieldsC),lower(imageType));
+    end
+    imgClassS = cerrCalcS.(structFieldName).(cerrFieldsC{imgClassIdx});
+    featClassC = fieldnames(imgClassS);
+    featClassIdx = contains(lower(featClassC),lower(featureClass));
+    cerrFeatS = imgClassS.(featClassC{featClassIdx});
+end
 
-delete(scanFilename)
-delete(maskFilename)
+%% 2. Compute features using Pyradiomics
 
-% CERR radiomics
-paramS = getRadiomicsParamTemplate(cerrParamFileName);
-cerrFeatS = calcGlobalRadiomicsFeatures(scanNum, structNum, paramS, planC);
-cerrFeatS.Original.glcmFeatS.AvgS
+pyCalcS = calcRadiomicsFeatUsingPyradiomics(planC,strName,pyradParamFile);
+
+if strcmpi(featureClass,'shape')
+    retFieldsC = {'shape'};
+else
+    if iscell(imageType)
+        retFieldsC = [lower(imageType),{lower(featureClass)}];
+    else
+        retFieldsC = {lower(imageType),lower(featureClass)};
+    end
+end
+%Get indices of relevant fields
+pyFieldsC = fieldnames(pyCalcS);
+returnIdxV = true(length(pyFieldsC),1);
+for n = 1:length(retFieldsC)
+    returnIdxV = returnIdxV & contains(lower(pyFieldsC),retFieldsC{n});
+end
+
+outFieldsC = pyFieldsC(returnIdxV);
+pyFeatS = struct();
+for n = 1:length(outFieldsC)
+    pyFeatS.(outFieldsC{n}) = pyCalcS.(outFieldsC{n});
+end
 
 
+end
