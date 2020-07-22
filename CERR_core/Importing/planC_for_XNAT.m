38446b0ca2b91767513a5f231ffd8dda2f83b06a
locastre@mskcc.org <locastre@mskcc.org>
2020-07-22 16:18:16 -0400

    refined functionality of planC_for_XNAT

--- CERR_core/Importing/planC_for_XNAT.m
+++ CERR_core/Importing/planC_for_XNAT.m
@@ -1,11 +1,17 @@
 function planC_for_XNAT(dicomPath,cerrPath,xhost,xproj,xsubj,xexp)
+% function planC_for_XNAT(dicomPath,cerrPath,xhost,xproj,xsubj,xexp)
+%
+% This function imports DICOM files from dicomPath and adds header info
+% with XNAT addressing metadata
 
 importDICOM(dicomPath,cerrPath);
 
 cerrFile = ls([cerrPath filesep '*.mat']);
 
-load([cerrPath filesep cerrFile]);
+cerrFilePath = [cerrPath filesep cerrFile];
 
-planC{indexS.header}.xnatInfo = struct('host',xhost,'project_id',xproj,'subject_id',xsubj,'experiment_id',xexp);
+load(cerrFilePath,'planC');
 
-save([cerrPath filesep cerrFile],'planC');
+planC = annotatePlanCForXNAT(planC, xhost,xexp,xproj,xsubj);
+
+save(cerrFilePath,'planC');
