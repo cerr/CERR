8d3e8648fce5abf979ace7f3ceb1422c31ddc8ad
aditiiyer <iyera@mskcc.org>
2020-08-19 17:22:43 -0400

    Write data to HDF5 using save with '-hdf5' flag.

--- CERR_core/DLSegmentationTraining/writeHDF5ForDL.m
+++ CERR_core/DLSegmentationTraining/writeHDF5ForDL.m
@@ -34,21 +34,15 @@ switch (passedScanDim)
             if ~exist(fullfile(outDirC{1},'Masks'),'dir')
                 mkdir(fullfile(outDirC{1},'Masks'))
             end
-            mask3M = uint8(mask3M);
+            OctaveExportMask = uint8(mask3M);
             maskFilename = fullfile(outDirC{1},'Masks',[filePrefix,'_3D.h5']);
-            h5create(maskFilename,'/mask',size(mask3M));
-            pause(0.1)
-            h5write(maskFilename,'/mask',mask3M);
+            save ("-hdf5",maskFilename,"OctaveExportMask")        
         end
         
         %Write scan
-        exportScan3M = scanC{1}{1}; % one view and one scan matrix
-        %exportScan3M = scanC{1};
+        OctaveExportScan = scanC{1}{1};
         scanFilename = fullfile(outDirC{1},[filePrefix,'_scan_3D.h5']);
-        h5create(scanFilename,'/scan',size(exportScan3M));
-        pause(0.1)
-        h5write(scanFilename,'/scan',exportScan3M);
-        
+        save ("-hdf5",scanFilename,"OctaveExportScan");        
         
     case '2D'
         
@@ -67,36 +61,24 @@ switch (passedScanDim)
                             mkdir(fullfile(outDirC{i},'Masks'))
                         end
                     end
-                    maskM = uint8(mask3M(:,:,slIdx));
+                    OctaveExportMask = uint8(mask3M(:,:,slIdx));
                     maskFilename = fullfile(outDirC{i},'Masks',[filePrefix,'_slice',...
                         num2str(slIdx),'.h5']);
                     
                     
                     % Low-level h5 write
                     filename = maskFilename;
-                    fileID = H5F.create(filename,'H5F_ACC_TRUNC','H5P_DEFAULT','H5P_DEFAULT');
-                    datatypeID = H5T.copy('H5T_NATIVE_DOUBLE');
-                    dims = size(maskM);
-                    dataspaceID = H5S.create_simple(2,fliplr(dims),[]);
-                    dsetname = '/mask';
-                    datasetID = H5D.create(fileID,dsetname,datatypeID,dataspaceID,'H5P_DEFAULT');
-                    H5D.write(datasetID,'H5ML_DEFAULT','H5S_ALL','H5S_ALL',...
-                        'H5P_DEFAULT',maskM);
-                    H5D.close(datasetID);
-                    H5S.close(dataspaceID);
-                    H5T.close(datatypeID);
-                    H5F.close(fileID);
-                    
-                    
+                    save ("-hdf5",filename,"OctaveExportMask")
+                  
                 end
                 
                 %Write scan
-                exportScan3M = [];
-                exportScan3M = scanC{i}{1}(:,:,slIdx);
+                OctaveExportScan = [];
+                OctaveExportScan = scanC{i}{1}(:,:,slIdx);
                 if length(scanC{i})>1
                     for c = 2:length(scanC{i})
                         temp = scanC{i}{c};
-                        exportScan3M = cat(3,exportScan3M,temp(:,:,slIdx));
+                        OctaveExportScan = cat(3,OctaveExportScan,temp(:,:,slIdx));
                     end
                 end
                 
@@ -104,25 +86,7 @@ switch (passedScanDim)
                     num2str(slIdx),'.h5']);
                 % Low-level h5 write
                 filename = scanFilename;
-                fileID = H5F.create(filename,'H5F_ACC_TRUNC','H5P_DEFAULT','H5P_DEFAULT');
-                datatypeID = H5T.copy('H5T_NATIVE_DOUBLE');
-                dims = size(exportScan3M);
-                if length(dims)==2
-                    rank = 2;
-                else
-                    rank = 3; %Multiple channels
-                end
-                dataspaceID = H5S.create_simple(rank,fliplr(dims),[]);
-                dsetname = '/scan';
-                datasetID = H5D.create(fileID,dsetname,datatypeID,dataspaceID,'H5P_DEFAULT');
-                H5D.write(datasetID,'H5ML_DEFAULT','H5S_ALL','H5S_ALL',...
-                    'H5P_DEFAULT',exportScan3M);
-                H5D.close(datasetID);
-                H5S.close(dataspaceID);
-                H5T.close(datatypeID);
-                H5F.close(fileID);
-                
-                
+                save ("-hdf5",filename,"OctaveExportScan")
             end
         end
         
