663ebc755646e42b37d116693be44050418aa6b7
locastre@mskcc.org <locastre@mskcc.org>
2020-09-11 13:45:44 -0400

    add flag to compute_boundingbox to return mask with bbox

--- CERR_core/PlanMetrics/heterogenity_metrics/compute_boundingbox.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/compute_boundingbox.m
@@ -1,5 +1,9 @@
-function [minr, maxr, minc, maxc, mins, maxs]=compute_boundingbox(x3D);
+function [minr, maxr, minc, maxc, mins, maxs, bboxmask]= compute_boundingbox(x3D, maskFlag)
 % finding the bounding box parameters
+
+if nargin < 2
+    maskFlag = 0;
+end
 
 [iV,jV,kV]=find3d(x3D);
 minr=min(iV);
@@ -8,5 +12,12 @@ minc=min(jV);
 maxc=max(jV);
 mins=min(kV);
 maxs=max(kV);
+
+bboxmask = [];
+
+if maskFlag
+    bboxmask = zeros(size(x3D));
+    bboxmask(minr:maxr, minc:maxc, mins:maxs) = 1;
+end
 
 return
