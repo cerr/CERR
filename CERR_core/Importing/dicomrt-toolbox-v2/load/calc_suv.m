f3ad5faa384704a07dc70d3cd1b27e1fbbe2290a
adityaapte <aditya.apte@gmail.com>
2020-09-30 17:57:55 -0400

    called getSUV for SUV calculation

--- CERR_core/Importing/dicomrt-toolbox-v2/load/calc_suv.m
+++ CERR_core/Importing/dicomrt-toolbox-v2/load/calc_suv.m
@@ -1,83 +1,59 @@
-function suvM = calc_suv(headerS,imgM)
-% suvM = calc_suv(headerS,imgM)
+function planC = calc_suv(scanNum,planC,suvType)
+%function suvM = calc_suv(scanNum,planC,suvType)
 %
-% Calcualtes SUV for the passed dicom header (headerS) and PET image
-% imgM. 
+% Calcualtes SUV for the passed scanNum and suvType.
 %
-% Input: 
-% imgM must be in Bq/ml. 
+% Input:
+% planC{indexS.scan}(scanNum).scanArray must be in Bq/ml.
 %
-% Output: 
-% suvM will be in gm/ml
+% Output:
+% planC{indexS.scan}(scanNum).scanArray will be in gm/ml
+% planC{indexS.scan}(scanNum).scanInfo.imageUnits and 
+% planC{indexS.scan}(scanNum).scanInfo.suvType fields will be updated.
 %
 % Written IEN
 % APA, 9/22/2020, updated to (1) get scan time based on deay correction,
-% (2) checked imageUnits. 
+% (2) checked imageUnits.
 %  References: http://www.turkupetcentre.net/petanalysis/model_suv.html
 %              https://documentation.clearcanvas.ca/Documentation/UsersGuide/Personal/13_1/index.html?suv.htm
+% APA, 9/29/2020, updated to handle suvType
 
-% Get Patient weight in grams
-ptweight = headerS.patientWeight*1000; % in grams
+% Check required fields based on 1st slice
+headerS = planC{indexS.scan}(scanNum).scanInfo(1);
 
+% Check Patient weight
+ptweight = headerS.patientWeight; % in grams
 if isempty(ptweight) || ptweight==0
     disp('Patient Weight is missing. suvM calculation ignored.');
-    suvM=imgM;
     return
 end
+%ptweight = ptweight * 1000; % in grams
 
-% Get Decay correction
+% Check Decay correction
 correctedImage = headerS.correctedImage;
 if ~any(ismember('DECY',correctedImage))
-    disp('suvM calciulation is applicable only when petDecayCorrection = DECY');
-    suvM = imgM;
+    disp('SUV calciulation is applicable only when petDecayCorrection = DECY');
     return
 end
 
 imageUnits = headerS.imageUnits;
-switch upper(imageUnits)
-    case 'BQML'
-                
-    otherwise
-        disp('suvM calciulation not supported for ImaageUnits other than BQML');
-        suvM = imgM;
-        return;
-        
-        % 1 Ci = 3.7 x 1010 Bq = 37 GBq
-        % 1 mCi = 3.7 x 107 Bq = 37 MBq
-        % 1 µCi = 3.7 x 104 Bq = 37 kBq
-        % 1 nCi = 37 Bq
+if ~strcmpi(imageUnits,'BQML')
+    disp('SUV calciulation is applicable only when image units = BQML');
+    return
+end
+% 1 Ci = 3.7 x 1010 Bq = 37 GBq
+% 1 mCi = 3.7 x 107 Bq = 37 MBq
+% 1 µCi = 3.7 x 104 Bq = 37 kBq
+% 1 nCi = 37 Bq
+
+scan3M = planC{indexS.scan}(scanNum).scanArray;
+headerS = planC{indexS.scan}(scanNum).scanInfo;
+[suv3M,imageUnits] = getSUV(scan3M, headerS, suvType);
+for slcNum = 1:size(planC{indexS.scan}(scanNum).scanArray,3)
+    planC{indexS.scan}(scanNum).scanInfo(slcNum).imageUnits = imageUnits;
+    planC{indexS.scan}(scanNum).scanInfo(slcNum).suvType = upper(suvType);
+    planC{indexS.scan}(scanNum).scanArray(:,:,slcNum) = suv3M(:,:,slcNum);    
 end
 
-% Get Scan time based on type of decay correction
-decayCorrection = headerS.decayCorrection;
-switch upper(decayCorrection)
-    case 'START'
-        scantime = dcm_hhmmss(headerS.seriesTime);
-    case 'ADMIN'
-        scantime = dcm_hhmmss(headerS.injectionTime);
-    case 'NONE'
-        scantime = dcm_hhmmss(headerS.acquisitionTime);
-end
-
-% Start Time for Radiopharmaceutical Injection
-injection_time = dcm_hhmmss(headerS.injectionTime);
-
-% Half Life for Radionuclide
-half_life = headerS.halfLife;
-
-% Total dose injected for Radionuclide
-injected_dose = headerS.injectedDose;
-
-% Calculate the decay
-% The injected dose used to calculate suvM is corrected for the decay that
-% occurs between the time of injection and the time of scan.
-% decayFactor = e^(t1-t2/halflife);
-decay = exp(-log(2)*(scantime-injection_time)/half_life);
-
-%Calculate the dose decayed during procedure
-injected_dose_decay = injected_dose*decay; % in Bq
-
-% Calculate SUV.
-suvM = imgM*ptweight/injected_dose_decay;
 
 return
