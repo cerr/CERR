e0e2bfd0cbc1086233b296178c6e5f7d1637a374
aditiiyer <iyera@mskcc.org>
2020-10-06 15:45:02 -0400

    Updated scan offset

--- CERR_core/Utilities/nii2cerr.m
+++ CERR_core/Utilities/nii2cerr.m
@@ -1,8 +1,8 @@
-function planC = nii2cerr(filename,movScanOffset,movScanName,planC,save_flag)
+function planC = nii2cerr(filename,scanName,planC,save_flag)
 % nii2cerr.m
 %
-% Import .nii data to CERR  
-% Usage: planC = nii2cerr(filename,movScanOffset,movScanName,planC,save_flag);
+% Import .nii data to CERR
+% Usage: planC = nii2cerr(filename,scanName,planC,save_flag);
 %
 % AI 4/10/19
 
@@ -12,9 +12,14 @@ if ~exist('planC','var') || isempty(planC)
 end
 
 [vol3M,infoS] = nifti_read_volume(filename);
+scanOffset = 0;
+volMin = min(vol3M(:));
+if volMin<0
+    scanOffset = -volMin;
+end
 infoS.Offset = [0 0 0];
 infoS.PixelDimensions = infoS.pixdim;
 infoS.Dimensions = infoS.dimension(2:4);
-planC = mha2cerr(infoS,vol3M,movScanOffset,movScanName,planC,save_flag);
+planC = mha2cerr(infoS,vol3M,scanOffset,scanName,planC,save_flag);
 
 end
