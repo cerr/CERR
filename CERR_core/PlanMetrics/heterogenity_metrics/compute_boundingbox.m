83e778bb28d33df620b23047c53538190223d8ad
EM LoCastro <locastre@mskcc.org>
2020-10-09 00:04:11 -0400

    add usage instructions in comment

--- CERR_core/PlanMetrics/heterogenity_metrics/compute_boundingbox.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/compute_boundingbox.m
@@ -1,9 +1,12 @@
 function [minr, maxr, minc, maxc, mins, maxs, bboxmask]= compute_boundingbox(x3D, maskFlag)
 % finding the bounding box parameters
+% update: EML, 2020-Sep, return bounding box binary mask with optional padding
+% if maskFlag > 1, it is interpreted as padding parameter
 
 if nargin < 2
     maskFlag = 0;
 end
+maskFlag = floor(maskFlag);
 
 [iV,jV,kV]=find3d(x3D);
 minr=min(iV);
@@ -15,7 +18,7 @@ maxs=max(kV);
 
 bboxmask = [];
 
-if maskFlag
+if ~isempty(maskFlag)
     bboxmask = zeros(size(x3D));
     if maskFlag > 1
         minr = minr - maskFlag;
