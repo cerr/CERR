6ec6534a554444b3e582469ff5e518181e035b69
EM LoCastro <locastre@mskcc.org>
2020-10-14 01:45:50 -0400

    return scanUID as second output

--- CERR_core/ImageRegistration/genScanUniqName.m
+++ CERR_core/ImageRegistration/genScanUniqName.m
@@ -1,4 +1,6 @@
-function uniqName = genScanUniqName(planC, scanNum)
+function [uniqName,scanUID] = genScanUniqName(planC, scanNum)
+% previously from register_scans.m
+
     indexS = planC{end};
     scanUID = planC{indexS.scan}(scanNum).scanUID;
     uniqName = [scanUID,num2str(floor(rand*1000))];
