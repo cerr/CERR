45302420d2f287198072f99ec5c5bd1e86099d47
adityaapte <aditya.apte@gmail.com>
2020-10-23 11:28:39 -0400

    created row/col/slc at volume center

--- CERR_core/Utilities/getResampledGrid.m
+++ CERR_core/Utilities/getResampledGrid.m
@@ -15,12 +15,11 @@ if nargin<6
     perturbY = 0;
     perturbZ = 0;
 else
-    perturbV = varargin{2};
+    perturbV = varargin{1};
     perturbX = perturbV(1);
     perturbY = perturbV(2);
     perturbZ = perturbV(3);
 end
-
 
 dx = abs(median(diff(xValsV)));
 dy = abs(median(diff(yValsV)));
@@ -29,47 +28,33 @@ dz = abs(median(diff(zValsV)));
 %% Get no. output rows, cols, slices
 PixelSpacingX = resampResolutionV(1);
 PixelSpacingY = resampResolutionV(2);
-numCols = ceil((xValsV(end) - xValsV(1) + dx)/(PixelSpacingX+eps));
-numRows = ceil((yValsV(end) - yValsV(1) + dy)/(PixelSpacingY+eps));
 if ~isnan(resampResolutionV(3))
     resamp3DFlag = 1;
     PixelSpacingZ = resampResolutionV(3);
-    numSlc = ceil((zValsV(end) - zValsV(1) + dz)/(PixelSpacingZ+eps));
 else
     %Resample in-plane
     resamp3DFlag = 0;
-    numSlc = length(zValsV);
 end
 
 %% Get output grid coordinates
 % Align grid centers
-%xCtr = dx/2+size(img3M,2)*dx/2 + perturbX;
-%yCtr = dy/2+size(img3M,1)*dy/2 + perturbY;
 xCtr = (xValsV(1)+xValsV(end))/2 + perturbX;
 yCtr = (yValsV(1)+yValsV(end))/2 + perturbY;
 zCtr = (zValsV(1)+zValsV(end))/2 + perturbZ;
-% Create output grid
-% xResampleV = [flip(xCtr-PixelSpacingX/2:-PixelSpacingX:0), ...
-%     xCtr+PixelSpacingX/2:PixelSpacingX:size(img3M,2)*dx];
-% yResampleV = [flip(yCtr-PixelSpacingY/2:-PixelSpacingY:0), ...
-%     yCtr+PixelSpacingY/2:PixelSpacingY:size(img3M,1)*dy];
-xResampleV = PixelSpacingX/2:PixelSpacingX:length(xValsV)*dx;
-yResampleV = PixelSpacingY/2:PixelSpacingY:length(yValsV)*dy;
-xResampCtr = (xResampleV(1)+xResampleV(end))/2;
-yResampCtr = (yResampleV(1)+yResampleV(end))/2;
-xDeltaCtr = xResampCtr - xCtr;
-yDeltaCtr = yResampCtr - yCtr;
-xResampleV = -xDeltaCtr + xResampleV;
-yResampleV = -yDeltaCtr + yResampleV;
 
-if resamp3DFlag
-    %zCtr = dz/2+size(img3M,3)*dz/2 + perturbZ;
-    %zResampleV = [flip(zCtr-PixelSpacingZ/2:-PixelSpacingZ:0), ...
-     %  zCtr+PixelSpacingZ/2:PixelSpacingZ:size(img3M,3)*dz];
-    zResampleV = PixelSpacingZ/2:PixelSpacingZ:length(zValsV)*dz;
-    zResampCtr = (zResampleV(1)+zResampleV(end))/2;
-    zDeltaCtr = zResampCtr - zCtr;
-    zResampleV = -zDeltaCtr + zResampleV;
+% Create output grid
+xPlusV = xCtr+PixelSpacingX:PixelSpacingX:xCtr+(length(xValsV)-1)*dx/2;
+xMinusV = flip(xCtr-PixelSpacingX:-PixelSpacingX:xCtr-(length(xValsV)-1)*dx/2);
+xResampleV = [xMinusV, xCtr, xPlusV];
+
+yPlusV = yCtr+PixelSpacingY:PixelSpacingY:yCtr+(length(yValsV)-1)*dy/2;
+yMinusV = flip(yCtr-PixelSpacingY:-PixelSpacingY:yCtr-(length(yValsV)-1)*dy/2);
+yResampleV = [yMinusV, yCtr, yPlusV];
+
+if resamp3DFlag  
+    zPlusV = zCtr+PixelSpacingZ:PixelSpacingZ:zCtr+(length(zValsV)-1)*dz/2;
+    zMinusV = flip(zCtr-PixelSpacingZ:-PixelSpacingZ:zCtr-(length(zValsV)-1)*dz/2);
+    zResampleV = [zMinusV, zCtr, zPlusV];    
 else
     zResampleV = zValsV;
 end
