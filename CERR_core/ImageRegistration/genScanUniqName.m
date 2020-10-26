6b46ac953aacb1c408ba415eefcc7c5737db425d
EM LoCastro <locastre@mskcc.org>
2020-10-26 06:26:31 -0400

    fix planC referencing

--- CERR_core/ImageRegistration/genScanUniqName.m
+++ CERR_core/ImageRegistration/genScanUniqName.m
@@ -1,7 +1,20 @@
 function [uniqName,scanUID] = genScanUniqName(planC, scanNum)
 % previously from register_scans.m
+if nargin < 2
+    scanNum = 1;
+end
 
+randPart = num2str(floor(rand*1000));
+
+if iscell(planC)
     indexS = planC{end};
     scanUID = planC{indexS.scan}(scanNum).scanUID;
-    uniqName = [scanUID,num2str(floor(rand*1000))];
+    uniqName = [scanUID randPart];
+else
+    [~,f,ext] = fileparts(planC);
+    if strcmp(ext,'.gz')
+        [~,f,~] = fileparts(f);
+    end
+    scanUID = dicomuid;
+    uniqName = [f '_' scanUID];
 end
