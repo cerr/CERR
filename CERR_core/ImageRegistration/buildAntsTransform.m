eae5612e7fcda71a1d11295ba43078a03c45ec6f
EM LoCastro <locastre@mskcc.org>
2020-10-29 00:17:29 -0400

    add flag to specify applying inverse of transform from fixed to moving space

--- CERR_core/ImageRegistration/buildAntsTransform.m
+++ CERR_core/ImageRegistration/buildAntsTransform.m
@@ -1,21 +1,46 @@
-function transformParams = buildAntsTransform(antsWarpProducts)
+function transformParams = buildAntsTransform(antsWarpProducts,inverseFlag)
+
+if ~exist('inverseFlag','var')
+    inverseFlag = 0;
+end
 
 % generate transform command
 transformParams ='';
-if ~isempty(antsWarpProducts.Warp)
-    if exist(antsWarpProducts.Warp,'file')
-        transformParams = [' -t ' antsWarpProducts.Warp ' '];
-    else
-        error(['Unable to complete transform, warp product missing: ' antsWarpProducts.Warp]);
+
+if ~inverseFlag
+    if ~isempty(antsWarpProducts.Warp)
+        if exist(antsWarpProducts.Warp,'file')
+            transformParams = [' -t ' antsWarpProducts.Warp ' '];
+        else
+            error(['Unable to complete transform, warp product missing: ' antsWarpProducts.Warp]);
+        end
     end
-end
-if ~isempty(deformS.algorithmParamsS.antsWarpProducts.Affine)
-    if exist(deformS.algorithmParamsS.antsWarpProducts.Affine, 'file')
-        transformParams = [transformParams ' -t ' antsWarpProducts.Affine ' '];
-    else
-        error(['Unable to complete transform, affine product missing: ' antsWarpProducts.Affine]);
+    if ~isempty(deformS.algorithmParamsS.antsWarpProducts.Affine)
+        if exist(deformS.algorithmParamsS.antsWarpProducts.Affine, 'file')
+            transformParams = [transformParams ' -t ' antsWarpProducts.Affine ' '];
+        else
+            error(['Unable to complete transform, affine product missing: ' antsWarpProducts.Affine]);
+        end
     end
-end
-if isempty(transformParams)
-    error('ANTs: No transformation products specified');
+    if isempty(transformParams)
+        error('ANTs: No transformation products specified');
+    end
+else
+    if ~isempty(antsWarpProducts.InverseWarp)
+        if exist(antsWarpProducts.InverseWarp,'file')
+            transformParams = [' -t ' antsWarpProducts.InverseWarp ' '];
+        else
+            error(['Unable to complete transform, warp product missing: ' antsWarpProducts.Warp]);
+        end
+    end
+    if ~isempty(deformS.algorithmParamsS.antsWarpProducts.Affine)
+        if exist(deformS.algorithmParamsS.antsWarpProducts.Affine, 'file')
+            transformParams = [transformParams ' -t  [ ' antsWarpProducts.Affine ',1] '];
+        else
+            error(['Unable to complete transform, affine product missing: ' antsWarpProducts.Affine]);
+        end
+    end
+    if isempty(transformParams)
+        error('ANTs: No transformation products specified');
+    end
 end
