d354f08aec55a448fc5cfd6717d412ad82d03f34
EM LoCastro <locastre@mskcc.org>
2020-11-09 01:41:27 -0500

    additional cases for algorithm descr

--- CERR_core/ImageRegistration/buildAntsCommand.m
+++ CERR_core/ImageRegistration/buildAntsCommand.m
@@ -1,29 +1,25 @@
 function antsCommand = buildAntsCommand(algorithm,userCmdFile,baseScanFileName,movScanFileName,outPrefix,baseMaskFileName,movMaskFileName,deformBaseMaskFileName,deformMovMaskFileName)
 
-if isunix
-    antsCommand = 'sh ';
-else
-    antsCommand = '';
-end
+antsCommand = '';
 
 [~,nproc_str] = system('nproc');
 nproc = nproc_str(1:end-1);
 
 switch upper(algorithm)
-    case 'QUICKSYN ANTS'
-    antsParams = [' -d 3 -f ' baseScanFileName ' -m ' movScanFileName ' -o ' outPrefix '  -n ' nproc ' '];
+    case {'QUICKSYN ANTS','QUICKSYN'}
+        antsParams = [' -d 3 -f ' baseScanFileName ' -m ' movScanFileName ' -o ' outPrefix '  -n ' nproc ' '];
         
-    %get additional flags for processing
-    if exist('userCmdFile','var') && ~isempty(userCmdFile)
-        if ~iscell(userCmdFile)
-            userCmdFile = {userCmdFile};
-        end
-        for i = 1:numel(userCmdFile)
-            antsParams = strrep(strrep([antsParams ' ' fileread(userCmdFile{i})],'baseMask3M',baseMaskFileName),'movMask3M',movMaskFileName);
+        %get additional flags for processing
+        if exist('userCmdFile','var') && ~isempty(userCmdFile)
+            if ~iscell(userCmdFile)
+                userCmdFile = {userCmdFile};
+            end
+            for i = 1:numel(userCmdFile)
+                antsParams = strrep(strrep([antsParams ' ' fileread(userCmdFile{i})],'baseMask3M',baseMaskFileName),'movMask3M',movMaskFileName);
+            end
         end
         antsCommand = [antsCommand ' antsRegistrationSyNQuick.sh ' antsParams];
-    end        
-    case 'LDDMM ANTS'
+    case {'LDDMM ANTS','LDDMM'}
         antsCmdString = fileread(userCmdFile);
         antsCommand = strrep(strrep(strrep(strrep(strrep(strrep(strrep(antsCmdString(1:end-1),'baseScan',baseScanFileName), ...
             'movScan',movScanFileName), ...
