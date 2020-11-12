d18b6f112fb3576137a0d510df539835709619ae
EM LoCastro <locastre@mskcc.org>
2020-11-12 02:03:46 -0500

    function to determine color of next structure adding to planC

---
+++ CERR_core/Utilities/setStructureColor.m
@@ -0,0 +1,29 @@
+function color = setStructureColor(planC,colorArr)
+%  function colorNum = setStructureColor(planC,colorArr)
+% function to ensure effective color cycling of structure hues in planC
+%
+% EML 2020/11/11
+%
+
+indexS = planC{end};
+colorsTotal = size(colorArr,1);
+
+if ~isempty(planC{indexS.structures})
+    colorReserve = cell2mat({planC{indexS.structures}.structureColor}');
+    vacantList = find(~ismember(colorArr,colorReserve,'rows'));
+    if ~isempty(vacantList)
+        nextColorNum = vacantList(1);
+    else
+        lastUsed = colorReserve(end,:);
+        lastIdx = find(ismember(colorArr,lastUsed,'rows'));
+        if lastIdx ~= colorsTotal
+            nextColorNum = lastIdx + 1;
+        else
+            nextColorNum = 2;
+        end
+    end
+else
+    nextColorNum = 1;
+end
+
+color = colorArr(nextColorNum,:);
