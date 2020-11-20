354183f479cf9c8eec9d3b6f89b8f343abf4577d
aditiiyer <iyera@mskcc.org>
2020-11-20 12:06:01 -0500

    Added modules with subsets of tags needed to group images in dcmdir_add.m

---
+++ ML_Dicom/modules/module_tags/patient_module_tags_subset.m
@@ -0,0 +1,54 @@
+function tagS = patient_module_tags_subset
+%"patient_module_tags_subset"
+%   Return a subset of the tags specified in C.7.1.1 in PS3.3 of the
+%   2006 DICOM specification required for distinguishing patients.
+%
+% Tags are returned in a struct array with 3 fields:  
+%   Tag: String containing hex DICOM tag of a field.
+%  Type: String describing type of field, with 5 options:
+%         '1' Field must exist, data must exist and be valid.
+%         '2' Field must exist, data can exist and be valid, or be NULL.
+%         '3' Field is optional, if the field exists data can exist and be
+%             valid or be NULL.
+%         '1C' Field must exist under certain conditions and contain valid
+%             data.
+%         '2C' Field must exist under certain conditions and can contain 
+%             valid data or be NULL.
+%Children: For sequences, a tagS with the same format as this struct array.
+%
+% AI 11/20/2020
+%
+%Usage:
+%   tagS = patient_module_tags_subset
+%
+% Copyright 2010, Joseph O. Deasy, on behalf of the CERR development team.
+% 
+% This file is part of The Computational Environment for Radiotherapy Research (CERR).
+% 
+% CERR development has been led by:  Aditya Apte, Divya Khullar, James Alaly, and Joseph O. Deasy.
+% 
+% CERR has been financially supported by the US National Institutes of Health under multiple grants.
+% 
+% CERR is distributed under the terms of the Lesser GNU Public License. 
+% 
+%     This version of CERR is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+% CERR is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+% without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+% See the GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with CERR.  If not, see <http://www.gnu.org/licenses/>.
+
+% Tags: {Patient's Name, PatientID}
+
+tagC = {'00100010','00100020'};
+
+tagTypeC = {'2','2'};
+
+childC = {[],[]};
+
+tagS = struct('tag', tagC, 'type', tagTypeC, 'children', childC);
