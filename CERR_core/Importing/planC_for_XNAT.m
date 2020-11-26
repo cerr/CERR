c40f74486c60c7d57051c79ca4734dc6ac2ab93f
EM LoCastro <locastre@mskcc.org>
2020-11-26 15:51:40 -0500

    updates for reviveRS

--- CERR_core/Importing/planC_for_XNAT.m
+++ CERR_core/Importing/planC_for_XNAT.m
@@ -4,18 +4,20 @@ function planC_for_XNAT(dicomPath,cerrPath,xhost,xproj,xsubj,xexp, rebuildRS)
 % This function imports DICOM files from dicomPath and adds header info
 % with XNAT addressing metadata
 
+if ~exist('rebuildRS','var')
+    rebuildRS = 0;
+end
+
 importDICOM(dicomPath,cerrPath);
 
-cerrFile = ls([cerrPath filesep '*.mat']);
-
-%cerrFilePath = [cerrPath filesep cerrFile];
+cerrFile = dir(fullfile(cerrPath, '*.mat'));
 
 planC = loadPlanC(cerrFile);
 
 planC = annotatePlanCForXNAT(planC, xhost,xexp,xproj,xsubj);
 
 if rebuildRS
-    planC = reviveRS(planC);
+    planC = reviveRS(planC,cerrPath);
 end
 
 save_planC(planC,[],'passed',cerrFile);
