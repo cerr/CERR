553ef25e1be7ddd8fb7c4e94ce34e4f8710ef855
locastre <locastre@mskcc.org>
2020-11-26 20:37:35 -0500

    add status messages

--- CERR_core/Extras/reviveRS.m
+++ CERR_core/Extras/reviveRS.m
@@ -1,8 +1,8 @@
-function reviveRS(planCFileName)
+function planC = reviveRS(planC,planCDir)
 
-[planCDir,~,~] = fileparts(planCFileName); % = '/cluster/home/xnat_pipeline/results/20201126021900';
+% [planCDir,~,~] = fileparts(planCFileName); % = '/cluster/home/xnat_pipeline/results/20201126021900';
 
-planC = loadPlanC(planCFileName);
+% planC = loadPlanC(planCFileName);
 indexS = planC{end};
 structureListC = {planC{indexS.structures}.structureName};
 
@@ -10,6 +10,7 @@ scanNum = 1;
 
 for i = 1:numel(structureListC)
     strName = structureListC{i};
+    disp(['Processing structure: ' strName]);
     updatedStructureListC = {planC{indexS.structures}.structureName};
     structNum = getMatchingIndex(lower(strName),lower(updatedStructureListC),'exact');
     strMask3M = getStrMask(structNum, planC);
@@ -17,7 +18,8 @@ for i = 1:numel(structureListC)
     planC = deleteStructure(planC,structNum);
     planC = maskToCERRStructure(strMask3M,0,scanNum,strName,planC);
 end
-
+disp('Generating DICOM UID Relationships');
 planC = generate_DICOM_UID_Relationships(planC);
+disp('Exporting RTSTRUCT');
 export_RS_IOD(planC,planCDir,0);
-save_planC(planC,[],'passed',planCFileName);
+% save_planC(planC,[],'passed',planCFileName);
