18021ad2ec926ce886f463f639d87a4d66fd15bf
aditiiyer <iyera@mskcc.org>
2020-12-16 13:53:23 -0500

    Function to set ROE plot mode

---
+++ CERR_core/PlanAnalysis/ROE_support/setPlotModeROE.m
@@ -0,0 +1,25 @@
+function setPlotModeROE(hObj,hEvt,hFig)
+% Set plot mode for ROE
+%
+% AI 12/15/2020  
+
+ud = guidata(hFig);
+sel = get(hObj,'Value');
+ud.plotMode = sel - 1;
+if ud.plotMode==3
+    set(ud.handle.modelsAxis(11),'String','Enter scale factor');
+    set(ud.handle.modelsAxis(10),'Visible','On','Enable','On');
+    set(ud.handle.modelsAxis(11),'Visible','On');
+elseif ud.plotMode==4
+    txt = sprintf('Enter\n \x0394nfrx');
+    set(ud.handle.modelsAxis(11),'String',txt);
+    set(ud.handle.modelsAxis(10),'Visible','On','Enable','On');
+    set(ud.handle.modelsAxis(11),'Visible','On');
+else
+    set(ud.handle.modelsAxis(10),'Visible','Off');
+    set(ud.handle.modelsAxis(11),'Visible','Off');
+end
+
+guidata(hFig,ud);
+
+end
