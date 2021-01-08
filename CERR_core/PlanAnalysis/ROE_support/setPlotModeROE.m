1320b68591d19dd6aeb37380dc6482e79e3c3a95
aditiiyer <iyera@mskcc.org>
2021-01-08 09:32:17 -0500

    Fixed alignment

--- CERR_core/PlanAnalysis/ROE_support/setPlotModeROE.m
+++ CERR_core/PlanAnalysis/ROE_support/setPlotModeROE.m
@@ -7,11 +7,12 @@ ud = guidata(hFig);
 sel = get(hObj,'Value');
 ud.plotMode = sel - 1;
 if ud.plotMode==3
-    set(ud.handle.modelsAxis(11),'String','Enter scale factor');
+    txt = sprintf('Enter\n scale factor');
+    set(ud.handle.modelsAxis(11),'String',txt);
     set(ud.handle.modelsAxis(10),'Visible','On','Enable','On');
     set(ud.handle.modelsAxis(11),'Visible','On');
 elseif ud.plotMode==4
-    txt = sprintf('Enter\n \x0394nfrx');
+    txt = sprintf('Enter\n\delta nfrx');
     set(ud.handle.modelsAxis(11),'String',txt);
     set(ud.handle.modelsAxis(10),'Visible','On','Enable','On');
     set(ud.handle.modelsAxis(11),'Visible','On');
