ea11fc064f11e9dbf61dca89fa04be5ef7c9348b
adityaapte <aditya.apte@gmail.com>
2021-01-19 14:25:34 -0500

    used vrCode instead of character comparison.

--- ML_Dicom/util/data2dcmElement.m
+++ ML_Dicom/util/data2dcmElement.m
@@ -33,10 +33,11 @@ function el = data2dcmElement(el, data, tag)
 
 %Create an empty attr to act as a temporary container for the new element.
 if isempty(el)
-    el = org.dcm4che3.data.Attributes;
+    el = javaObject('org.dcm4che3.data.Attributes');
 end
     
-vr = org.dcm4che3.data.ElementDictionary.vrOf(tag, []);
+vr = javaMethod('vrOf','org.dcm4che3.data.ElementDictionary',tag, []);
+vrCode = vr.code;
 %vrString = char(vr);
 %Get the tag for this element.
 %tag = el.tag;
@@ -45,14 +46,17 @@ vr = org.dcm4che3.data.ElementDictionary.vrOf(tag, []);
 %vr = char(vr.toString);
 
 %%
-%switch upper(vrString)
-if vr.equals(org.dcm4che3.data.VR.AE) %strcmpi(vr,'AE')
+switch upper(vrCode)
+%if vr.equals(org.dcm4che3.data.VR.AE) %strcmpi(vr,'AE')
+    case 16709
     %case 'AE'
         %Needs implementation
-elseif vr.equals(org.dcm4che3.data.VR.AS) %strcmpi(vr,'AS')
+%elseif vr.equals(org.dcm4che3.data.VR.AS) %strcmpi(vr,'AS')
+    case 16723
     %case 'AS'
         %Needs implementation
-elseif vr.equals(org.dcm4che3.data.VR.AT) %strcmpi(vr,'AT')
+%elseif vr.equals(org.dcm4che3.data.VR.AT) %strcmpi(vr,'AT')
+    case 16724
     %case 'AT'
         %Attribute tags may be passed as a hex string, ie '000A003E', or an
         %integer that would be the result of hex2dec('000A003E');
@@ -60,21 +64,24 @@ elseif vr.equals(org.dcm4che3.data.VR.AT) %strcmpi(vr,'AT')
             data = hex2dec(data);
         end
         el.setInt(tag, vr, data);
-elseif vr.equals(org.dcm4che3.data.VR.CS) %strcmpi(vr,'CS')
+%elseif vr.equals(org.dcm4che3.data.VR.CS) %strcmpi(vr,'CS')
+    case 17235
         %case 'CS'
         if ~isempty(data)
             data = upper(strtrim(data));
         end
         el.setString(tag, vr, data);
-elseif vr.equals(org.dcm4che3.data.VR.LO) || vr.equals(org.dcm4che3.data.VR.ST) %any(strcmpi(vr,{'LO', 'ST'}))
+% elseif vr.equals(org.dcm4che3.data.VR.LO) || vr.equals(org.dcm4che3.data.VR.ST) %any(strcmpi(vr,{'LO', 'ST'}))
+    case {19535,21332}
     %case {'LO', 'ST'}
         el.setString(tag, vr, data);
-elseif vr.equals(org.dcm4che3.data.VR.DA) %strcmpi(vr,'DA')
+%elseif vr.equals(org.dcm4che3.data.VR.DA) %strcmpi(vr,'DA')
+    case 17473
     %case 'DA' 
         %Use builtin dcm4che Date functions.
         el.setDate(tag, vr, []);
         if ~isempty(data)
-            jDate = org.dcm4che3.util.DateUtils;
+            jDate = javaObject('org.dcm4che3.util.DateUtils');
             tz = el.getTimeZone();
             date = jDate.parseDA(tz, data, 1);
             el.setString(tag, vr, jDate.formatDA(tz, date));
@@ -82,48 +89,58 @@ elseif vr.equals(org.dcm4che3.data.VR.DA) %strcmpi(vr,'DA')
         %setDate(privateCreator, tmTag, VR.TM, org.dcm4che3.data.DatePrecision;, date);
         %el.setDate(tag, date);
 
-elseif vr.equals(org.dcm4che3.data.VR.DS) %strcmpi(vr,'DS')
+%elseif vr.equals(org.dcm4che3.data.VR.DS) %strcmpi(vr,'DS')
+    case 17491
     %case 'DS'
         if ~isempty(data)
             el.setFloat(tag, vr, data);
         else
             el.setFloat(tag, vr, []);
         end
-elseif vr.equals(org.dcm4che3.data.VR.DT) %strcmpi(vr,'DT')
+%elseif vr.equals(org.dcm4che3.data.VR.DT) %strcmpi(vr,'DT')
+    case 17492
     %case 'DT'
         %Needs implementation     
-elseif vr.equals(org.dcm4che3.data.VR.FL) %strcmpi(vr,'FL')
+%elseif vr.equals(org.dcm4che3.data.VR.FL) %strcmpi(vr,'FL')
+    case 17996
     %case 'FL'        
         el.setFloat(tag, vr, data); 
-elseif vr.equals(org.dcm4che3.data.VR.FD) %strcmpi(vr,'FD')
+%elseif vr.equals(org.dcm4che3.data.VR.FD) %strcmpi(vr,'FD')
+    case 17988
     %case 'FD'
         %Needs implementation   
-elseif vr.equals(org.dcm4che3.data.VR.IS) %strcmpi(vr,'IS')
+%elseif vr.equals(org.dcm4che3.data.VR.IS) %strcmpi(vr,'IS')
+    case 18771
     %case 'IS'
         if isnumeric(data)
             el.setInt(tag, vr, data);
         else
             error('The input should be numeric for "IS" ');
         end
-elseif vr.equals(org.dcm4che3.data.VR.LT) %strcmpi(vr,'LT')
+%elseif vr.equals(org.dcm4che3.data.VR.LT) %strcmpi(vr,'LT')
+    case 19540
     %case 'LT'
         %Needs implementation     
-elseif vr.equals(org.dcm4che3.data.VR.OB) %strcmpi(vr,'OB')
+%elseif vr.equals(org.dcm4che3.data.VR.OB) %strcmpi(vr,'OB')
+    case 20290
     %case 'OB'
         %Needs implementation  
-elseif vr.equals(org.dcm4che3.data.VR.OF) %strcmpi(vr,'OF')
+%elseif vr.equals(org.dcm4che3.data.VR.OF) %strcmpi(vr,'OF')
+    case 20294
     %case 'OF'
         %Needs implementation  
-elseif vr.equals(org.dcm4che3.data.VR.OW) %strcmpi(vr,'OW')
+%elseif vr.equals(org.dcm4che3.data.VR.OW) %strcmpi(vr,'OW')
+    case 20311
     %case 'OW'
          el.setInt(tag, vr, double(data));  %Incorrect. Requires putBytes unless LUT data.
 %          el.putBytes(tag, vr, 1, data);         
-elseif vr.equals(org.dcm4che3.data.VR.PN) %strcmpi(vr,'PN')
-    %case 'PN' 
-         %nameObj = org.dcm4che3.data.PersonName(el.getString(tag));
-         nameObj = org.dcm4che3.data.PersonName(dec2hex(tag));
-         
-         %DCM4CHE3 now uses enum 'Component' instead of an array
+%elseif vr.equals(org.dcm4che3.data.VR.PN) %strcmpi(vr,'PN')
+    case 20558
+        %case 'PN'
+        %nameObj = org.dcm4che3.data.PersonName(el.getString(tag));
+        nameObj = javaObject('org.dcm4che3.data.PersonName','');
+        
+        %DCM4CHE3 now uses enum 'Component' instead of an array
         
         compFamilyName = javaMethod('valueOf','org.dcm4che3.data.PersonName$Component','FamilyName');
         compGivenName = javaMethod('valueOf','org.dcm4che3.data.PersonName$Component','GivenName');
@@ -131,15 +148,20 @@ elseif vr.equals(org.dcm4che3.data.VR.PN) %strcmpi(vr,'PN')
         compNamePrefix = javaMethod('valueOf','org.dcm4che3.data.PersonName$Component','NamePrefix');
         compNameSuffix = javaMethod('valueOf','org.dcm4che3.data.PersonName$Component','NameSuffix');
         
-         %The # in get(#) as defined by dcm4che2, PersonName class.
-         nameObj.set(compFamilyName, data.FamilyName);
-         nameObj.set(compGivenName, data.GivenName);
-         nameObj.set(compMiddleName, data.MiddleName);
-         nameObj.set(compNamePrefix, data.NamePrefix);
-         nameObj.set(compNameSuffix, data.NameSuffix);         
-         
+        %The # in get(#) as defined by dcm4che2, PersonName class.
+        if isfield(data,'FamilyName')
+            nameObj.set(compFamilyName, data.FamilyName);
+            nameObj.set(compGivenName, data.GivenName);
+            nameObj.set(compMiddleName, data.MiddleName);
+            nameObj.set(compNamePrefix, data.NamePrefix);
+            nameObj.set(compNameSuffix, data.NameSuffix);
+        else
+            nameObj.setValue(data); % check this
+        end
+        
          el.setString(tag, vr, nameObj.toString);
-elseif vr.equals(org.dcm4che3.data.VR.SH) %strcmpi(vr,'SH')
+%elseif vr.equals(org.dcm4che3.data.VR.SH) %strcmpi(vr,'SH')
+    case 21320
     %case 'SH'
         %SH requires that all strings are <= 16 characters.
         switch class(data)
@@ -160,22 +182,26 @@ elseif vr.equals(org.dcm4che3.data.VR.SH) %strcmpi(vr,'SH')
             
         end                  
         el.setString(tag, vr, data);
-elseif vr.equals(org.dcm4che3.data.VR.SL) %strcmpi(vr,'SL')
+%elseif vr.equals(org.dcm4che3.data.VR.SL) %strcmpi(vr,'SL')
+    case 21324
     %case 'SL'
         %Needs implementation       
         el.setInt(tag, vr, data);
-elseif vr.equals(org.dcm4che3.data.VR.SQ) %strcmpi(vr,'SQ')
+%elseif vr.equals(org.dcm4che3.data.VR.SQ) %strcmpi(vr,'SQ')
+    case 21329
     %case 'SQ'
         %Implementation currently unnecessary.
-elseif vr.equals(org.dcm4che3.data.VR.SS) %strcmpi(vr,'SS')
+%elseif vr.equals(org.dcm4che3.data.VR.SS) %strcmpi(vr,'SS')
+    case 21331
     %case 'SS'
         %Needs implementation
-elseif vr.equals(org.dcm4che3.data.VR.TM) %strcmpi(vr,'TM')
+%elseif vr.equals(org.dcm4che3.data.VR.TM) %strcmpi(vr,'TM')
+    case 21581
     %case 'TM'   
         %Use builtin dcm4che Time functions.
-        jDate = org.dcm4che3.util.DateUtils;
+        jDate = javaObject('org.dcm4che3.util.DateUtils');
         tz = el.getTimeZone();
-        precision = org.dcm4che3.data.DatePrecision;
+        precision = javaObject('org.dcm4che3.data.DatePrecision');
         try
             date = jDate.parseTM(tz, data, 1, precision);
             el.setDate(tag, vr, []);
@@ -183,23 +209,28 @@ elseif vr.equals(org.dcm4che3.data.VR.TM) %strcmpi(vr,'TM')
         catch
             el.setDate(tag,vr,[]);
         end
-elseif vr.equals(org.dcm4che3.data.VR.UI) %strcmpi(vr,'UI')
+%elseif vr.equals(org.dcm4che3.data.VR.UI) %strcmpi(vr,'UI')
+    case 21833
     %case 'UI'
           el.setString(tag, vr, data);
-elseif vr.equals(org.dcm4che3.data.VR.UL) %strcmpi(vr,'UL')
+%elseif vr.equals(org.dcm4che3.data.VR.UL) %strcmpi(vr,'UL')
+    case 21836
     %case 'UL'
         %Needs implementation
-elseif vr.equals(org.dcm4che3.data.VR.UN) %strcmpi(vr,'UN')
+%elseif vr.equals(org.dcm4che3.data.VR.UN) %strcmpi(vr,'UN')
+    case 21838
     %case 'UN'
         %Needs implementation
-elseif vr.equals(org.dcm4che3.data.VR.US) %strcmpi(vr,'US')
+%elseif vr.equals(org.dcm4che3.data.VR.US) %strcmpi(vr,'US')
+    case 21843
     %case 'US'
         el.setInt(tag, vr, data);
-elseif vr.equals(org.dcm4che3.data.VR.UT) %strcmpi(vr,'UT')
+%elseif vr.equals(org.dcm4che3.data.VR.UT) %strcmpi(vr,'UT')
+    case 21844
     %case 'UT'
         %Needs implementation 
-else
-    %otherwise
+%else
+    otherwise
         error('Unrecognized VR type.'); %%Consider more gracious exit.
 end
 
