5e2d96c25b1cfee71a2ebd439cfd3ebadeb5bbf5
EM LoCastro <locastre@mskcc.org>
2021-08-13 16:44:54 -0400

    alternate script to add subfolders to MATLAB/Octave path

---
+++ addToPath2.m
@@ -0,0 +1,14 @@
+function addToPath2(cerrPath)
+
+G = dir(cerrPath);
+
+for i = 1:numel(G)
+    Gi = G(i);
+    if Gi.isdir
+        if ~strcmp(Gi.name(1),'.')
+            subpath = fullfile(Gi.folder,Gi.name);
+            disp(['Adding to path ... ' subpath]);
+            addpath(genpath(subpath));
+        end
+    end
+end
