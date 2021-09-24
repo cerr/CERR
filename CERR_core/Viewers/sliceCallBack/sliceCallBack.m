862aa8fd7ed7972651e2622bdb00142d014a1bc7
adityaapte <aditya.apte@gmail.com>
2021-09-24 15:24:46 -0400

    checked whether planLoaded field exists

--- CERR_core/Viewers/sliceCallBack/sliceCallBack.m
+++ CERR_core/Viewers/sliceCallBack/sliceCallBack.m
@@ -1617,7 +1617,7 @@ switch upper(instr)
 
     case 'OPENNEWPLANC'
         %DK
-        if stateS.planLoaded
+        if isfield(stateS,'planLoaded') && stateS.planLoaded
             %exit structure comparison mode if active
             if isfield(stateS,'structCompare')
                 structCompare({'exit'})
