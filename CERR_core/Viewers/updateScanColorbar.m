ca4730d3f27e2604fe3ba79ba98d3f7ebb16c902
adityaapte <aditya.apte@gmail.com>
2021-09-28 21:15:54 -0400

    set stateS.handle.scanColorbar 'nextplot' property to 'add'

--- CERR_core/Viewers/updateScanColorbar.m
+++ CERR_core/Viewers/updateScanColorbar.m
@@ -18,6 +18,8 @@ cB      = ind2rgb(tmpV, cM);
 %offset = planC{indexS.scan}(scanSet).scanInfo(1).CTOffset;
 minScanVal = stateS.scanStats.CTLevel.(scanUID) - stateS.scanStats.CTWidth.(scanUID)/2;
 maxScanVal = stateS.scanStats.CTLevel.(scanUID) + stateS.scanStats.CTWidth.(scanUID)/2;
+xTickV = [minScanVal,maxScanVal];
 imagesc([minScanVal maxScanVal],[0.5 0.5],cB,'parent',stateS.handle.scanColorbar)
-set(stateS.handle.scanColorbar,'YTick',[],'YTickLabel',[], 'visible','on');
+set(stateS.handle.scanColorbar,'YTick',[],'YTickLabel',[], 'visible','on',...
+    'XTick',xTickV,'XTickLabel',strsplit(num2str(xTickV)),'xLim',[minScanVal maxScanVal]);
 
