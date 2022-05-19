0ea7707c4272c21afc57a5e1e7f349b36256bcfe
adityaapte <aditya.apte@gmail.com>
2022-05-19 12:07:20 -0400

    converted volHistV to row vector

--- CERR_core/PlanMetrics/calc_Vx.m
+++ CERR_core/PlanMetrics/calc_Vx.m
@@ -45,7 +45,7 @@ end
 
 % Add 0 to the beginning to account for the fact that the first bin must
 % correspond to the entire volume.
-volsHistV = [0 volsHistV];
+volsHistV = [0 volsHistV(:)'];
 cumVolsV = cumsum(volsHistV);
 cumVols2V  = cumVolsV(end) - cumVolsV;
 ind = find(doseBinsV >= doseCutoff, 1 );
