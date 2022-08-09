4b86b0e6958f642ef418ed1e2ccacf5ee28f994d
aditiiyer <iyera@mskcc.org>
2022-08-09 11:54:42 -0400

    Accept planC or path to dir containing CERR files

--- CERR_core/Contouring/BABS/processAndImportSeg.m
+++ CERR_core/Contouring/BABS/processAndImportSeg.m
@@ -1,15 +1,14 @@
-function planC = processAndImportSeg(planC,scanNumV,fullSessionPath,userOptS)
-% planC = processAndImportSeg(userOptS,fullSessionPath);
+function [planC,origScanNum,success] = processAndImportSeg(planC,scanNumV,...
+                                       fullSessionPath,userOptS)
+% planC = processAndImportSeg(planC,scanNumV,fullSessionPath,userOptS);
 %-------------------------------------------------------------------------
 % INPUTS
-% planC
+% planC           : planC OR path to directory containing CERR files.
 % scanNumV        : Scan nos required by model
 % fullSessionPath : Full path to session dir
 % userOptS        : Dictionary of post-processing parameters.
 %-------------------------------------------------------------------------
 % AI 9/21/21
-
-indexS = planC{end};
 
 %-For structname-to-label map
 labelPath = fullfile(fullSessionPath,'outputLabelMap');
@@ -19,30 +18,75 @@ outFmt = userOptS.modelOutputFormat;
 passedScanDim = userOptS.passedScanDim;
 outC = stackDLMaskFiles(fullSessionPath,outFmt,passedScanDim);
 
-% Import to planC
-tic
-identifierS = userOptS.structAssocScan.identifier;
-if ~isempty(fieldnames(userOptS.structAssocScan.identifier))
-    origScanNum = getScanNumFromIdentifiers(identifierS,planC);
+% Import to CERR
+success = 0;
+if ~iscell(planC)
+    cerrPath = planC;
+    % Load from file
+    planCfileS = dir(fullfile(cerrPath,'*.mat'));
+    planCfilenameC = {planCfileS.name};
+
+    for nFile = 1:length(planCfilenameC)
+        tic
+        planCfilename = fullfile(cerrPath, planCfilenameC{nFile});
+        planC = loadPlanC(planCfilename,tempdir);
+
+        %ptIdx = strcmp(fullSessionPath,planCfilename)
+        ptIdx = nFile;
+        segMask3M = outC{ptIdx};
+
+        [origScanNum,planC] = importLabelMap(userOptS,scanNumV,...
+            segMask3M,labelPath,planC);
+
+        %Save planC
+        optS = [];
+        saveflag = 'passed';
+        save_planC(planC,optS,saveflag,planCfilename);
+        toc
+    end
 else
-    origScanNum = 1; %Assoc with first scan by default
-end
-outScanNum = scanNumV(origScanNum);
-userOptS.scan(outScanNum) = userOptS(origScanNum).scan;
-userOptS.scan(outScanNum).origScan = origScanNum;
-planC  = joinH5planC(outScanNum,outC{1},labelPath,userOptS,planC); % only 1 file
-toc
-
-% Post-process segmentation
-planC = postProcStruct(planC,userOptS);
-
-%Delete intermediate (resampled) scans if any
-scanListC = arrayfun(@(x)x.scanType, planC{indexS.scan},'un',0);
-resampScanName = ['Resamp_scan',num2str(origScanNum)];
-matchIdxV = ismember(scanListC,resampScanName);
-if any(matchIdxV)
-    deleteScanNum = find(matchIdxV);
-    planC = deleteScan(planC,deleteScanNum);
+    segMask3M = outC{1};
+    tic
+    [origScanNum,planC] = importLabelMap(userOptS,scanNumV,...
+        segMask3M,labelPath,planC);
+    toc
 end
 
+success = 1;
+
+
+%% ----- Supporting functions ----
+    function [origScanNum,planC] = importLabelMap(userOptS,scanNumV,...
+            segMask3M,labelPath,planC)
+
+        indexS = planC{end};
+        identifierS = userOptS.structAssocScan.identifier;
+        if ~isempty(fieldnames(userOptS.structAssocScan.identifier))
+            origScanNum = getScanNumFromIdentifiers(identifierS,planC);
+        else
+            origScanNum = 1; %Assoc with first scan by default
+        end
+        outScanNum = scanNumV(origScanNum);
+        userOptS.scan(outScanNum) = userOptS(origScanNum).scan;
+        userOptS.scan(outScanNum).origScan = origScanNum;
+        planC  = joinH5planC(outScanNum,segMask3M,labelPath,userOptS,planC);
+
+        % Post-process segmentation
+        if sum(segMask3M(:))>0
+            fprintf('\nPost-processing results...\n');
+            tic
+            planC = postProcStruct(planC,userOptS);
+            toc
+        end
+
+        % Delete intermediate (resampled) scans if any
+        scanListC = arrayfun(@(x)x.scanType, planC{indexS.scan},'un',0);
+        resampScanName = ['Resamp_scan',num2str(origScanNum)];
+        matchIdxV = ismember(scanListC,resampScanName);
+        if any(matchIdxV)
+            deleteScanNum = find(matchIdxV);
+            planC = deleteScan(planC,deleteScanNum);
+        end
+
+    end
 end
