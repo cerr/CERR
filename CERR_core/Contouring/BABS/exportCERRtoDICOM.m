67badd4428bc0230a272a0b0e73e3b6315f86992
aditiiyer <iyera@mskcc.org>
2022-08-12 16:40:15 -0400

    Extneded for batch export

--- CERR_core/Contouring/BABS/exportCERRtoDICOM.m
+++ CERR_core/Contouring/BABS/exportCERRtoDICOM.m
@@ -1,19 +1,22 @@
-function exportCERRtoDICOM(cerrPath,origScanNum,allLabelNamesC,...
+function exportCERRtoDICOM(cerrPath,origScanNumV,allLabelNamesC,...
     outputCERRPath,outputDicomPath,dcmExportOptS,savePlancFlag)
-% function exportCERRtoDICOM(cerrPath,origScanNum,allLabelNamesC,...
+% function exportCERRtoDICOM(cerrPath,origScanNumV,allLabelNamesC,...
 %     outputCERRPath,outputDicomPath,dcmExportOptS,savePlancFlag)
 %
 % This function exports selected structures from CERR format to DICOM RTSTRUCT.
-%
+%--------------------------------------------------------------------------
 % INPUTS:
-% cerrPath           - directory containing CERR files with initial
+% cerrPath          - directory containing CERR files with initial
 %                      sementation
-% allLabelNamesC     - Cell array of labelnames for export
+% origScanNumV      - Indices of scans to be segmented (one per CERR file).
+% allLabelNamesC    - Cell array of labelnames for export
 %
-% outputCERRPath     - directory to save CERR file with segmentation copied
+% outputCERRPath    - directory to save CERR file with segmentation copied
 %                      to original CERR file.
-% outputDicomPath    - directory to export DICOM RTSTRUCT
-%
+% outputDicomPath   - directory to export DICOM RTSTRUCT
+% dcmExportOptS     - Custom settings for dicom export
+% savePlancFlag     - Set to true to save planC to file.
+%--------------------------------------------------------------------------
 % APA, 8/14/2018
 % AI, 2/7/2020
 
@@ -21,22 +24,22 @@ dirS = dir(cerrPath);
 dirS(1:2) = [];
 init_ML_DICOM
 
-for indBase = 1:length(dirS)
+for nFile = 1:length(dirS)
     
     
     %Load planC
-    origFileName = fullfile(cerrPath,dirS(indBase).name);
+    origFileName = fullfile(cerrPath,dirS(nFile).name);
     planC = loadPlanC(origFileName);
     
     %Save planC to outputDicomPath
-    [~,fname,~] = fileparts(dirS(indBase).name);
+    [~,fname,~] = fileparts(dirS(nFile).name);
     %fname = [fname,'_',algorithm];
     if savePlancFlag
         planC = save_planC(planC,[],'passed',...
             fullfile(outputCERRPath,[fname,'.mat']));
     end
     
-    exportAISegToDICOM(planC,origScanNum,outputDicomPath,fname,...
+    exportAISegToDICOM(planC,origScanNumV(nFile),outputDicomPath,fname,...
         dcmExportOptS,allLabelNamesC)
 
     
