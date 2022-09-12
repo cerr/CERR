e656cb86d2b4dd75940e62887fffbc463a30bfd9
adityaapte <aditya.apte@gmail.com>
2022-09-12 11:17:44 -0400

    updated to access planC from global

--- CERR_core/Utilities/createIntersectionStructure.m
+++ CERR_core/Utilities/createIntersectionStructure.m
@@ -1,7 +1,11 @@
 function planC = createIntersectionStructure(structNum1,structNum2,planC)
-% function createIntersectionStructure(structNum1,structNum2,planC)
+% function planC = createIntersectionStructure(structNum1,structNum2,planC)
 %
 % APA, 01/09/2011
+
+if ~exist('planC','var')
+    global planC
+end
 
 indexS = planC{end};
 
@@ -10,9 +14,9 @@ rasterSegs1 = getRasterSegments(structNum1, planC);
 rasterSegs2 = getRasterSegments(structNum2, planC);
 
 % Get associated scanNum 
-scanNum = 1;
+scanNum = getStructureAssociatedScan(structNum1,planC);
 
-% Get Union of the two rasterSegments
+% Get Intersection of the two rasterSegments
 rasterSegs = structIntersect(rasterSegs1, rasterSegs2, scanNum, planC);
 
 % Get Contours from rasterSegs
@@ -21,7 +25,7 @@ contourS = rasterToPoly(rasterSegs, scanNum, planC);
 % Generate Structure Name
 strName1 = planC{indexS.structures}(structNum1).structureName;
 strName2 = planC{indexS.structures}(structNum2).structureName;
-strName = [strName1,'_U_',strName2];
+strName = [strName1,'_I_',strName2];
 
 % Create New structure
 %Make an empty structure, assign name/contour.
@@ -42,3 +46,9 @@ end
 %Update uniformized data.
 planC = updateStructureMatrices(planC, numStructs+1);
 
+% Refresh View
+global stateS
+if ~isempty(stateS) && isfield(stateS,'handle') && isfield(stateS.handle,'CERRSliceViewer') && ishandle(stateS.handle.CERRSliceViewer)
+    stateS.structsChanged = 1;
+    CERRRefresh
+end
