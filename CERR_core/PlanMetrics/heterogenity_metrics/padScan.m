65f3a988723dd49c9cb26621146cf4039e02af3d
aditiiyer <iyera@mskcc.org>
2022-12-01 11:00:11 -0500

    Added flag to crop scan to mask bounding box (on by default)

--- CERR_core/PlanMetrics/heterogenity_metrics/padScan.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/padScan.m
@@ -1,4 +1,5 @@
-function [outScan3M,outMask3M, outLimitsV] = padScan(scan3M,mask3M,method,marginV)
+function [outScan3M,outMask3M, outLimitsV] = padScan(scan3M,mask3M,...
+    method,marginV,cropFlag)
 % padScan.m
 % Crop scan around ROI and pad using specified method.
 % ----------------------------------------------------------------------
@@ -9,33 +10,58 @@ function [outScan3M,outMask3M, outLimitsV] = padScan(scan3M,mask3M,method,margin
 %           'periodic','nearest','mirror', and 'none'.
 % margin  : 3-element vector specifying amount of padding (in voxels) along
 %           each dimension.
+%cropFlag : Set to 1 to crop to mask bounds prior to padding (default)
+%           or zero to use original scan extents.
+%-----------------------------------------------------------------------
 % AI 06/05/20
 % ----------------------------------------------------------------------
 
-%% Compute ROI bounding box extents
-[minr, maxr, minc, maxc, mins, maxs] = compute_boundingbox(mask3M);
+if ~exist('cropFlag','var')
+    cropFlag = 1;
+end
 
-%% Crop scan
-croppedScan3M = scan3M(minr:maxr,minc:maxc,mins:maxs);
-croppedMask3M = mask3M(minr:maxr,minc:maxc,mins:maxs);
+if strcmpi(method,'none')
+    marginV = [0,0,0];
+end
+
+%% Crop scan & mask
+if cropFlag
+    [minr, maxr, minc, maxc, mins, maxs] = compute_boundingbox(mask3M);
+    minr = max(minr-marginV(1),1);
+    maxr = min(maxr+marginV(1),size(mask3M,1));
+    minc = max(minc-marginV(2),1);
+    maxc = min(maxc+marginV(2),size(mask3M,2));
+    mins = max(mins-marginV(3),1);
+    maxs = min(maxs+marginV(3),size(mask3M,3));
+    croppedScan3M = scan3M(minr:maxr,minc:maxc,mins:maxs);
+    croppedMask3M = mask3M(minr:maxr,minc:maxc,mins:maxs);
+else
+    inputSizeV = size(scan3M);
+    [minr,minc,mins] = deal(1);
+    maxr = inputSizeV(1);
+    maxc = inputSizeV(2);
+    maxs = inputSizeV(3);
+    minr = minr-marginV(1);
+    maxr = maxr+marginV(1);
+    minc = minc-marginV(2);
+    maxc = maxc+marginV(2);
+    mins = mins-marginV(3);
+    maxs = maxs+marginV(3);
+    croppedScan3M = scan3M;
+    croppedMask3M = mask3M;
+end
 
 %% Compute padded image size
-if strcmpi(method,'none')
-    marginV=[0,0,0];
-end
-minr = max(minr-marginV(1),1);
-maxr = min(maxr+marginV(1),size(mask3M,1));
-minc = max(minc-marginV(2),1);
-maxc = min(maxc+marginV(2),size(mask3M,2));
-mins = max(mins-marginV(3),1);
-maxs = min(maxs+marginV(3),size(mask3M,3));
 outLimitsV = [minr,maxr,minc,maxc,mins,maxs];
 
 %% Apply padding
 switch lower(method)
     
     case 'expand'
-       
+        %requires cropFlag=1
+        if ~cropFlag
+            error('padScan.m: Set cropFlag=1 to use method ''expand''');
+        end
         outScan3M = scan3M(minr:maxr,minc:maxc,mins:maxs);
         outMask3M = mask3M(minr:maxr,minc:maxc,mins:maxs);
         
