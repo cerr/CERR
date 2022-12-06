aa1b6e7da91c5948dd1d2225255c381c3b2a0ea7
adityaapte <aditya.apte@gmail.com>
2022-12-06 17:59:01 -0500

    Fixed orientation to RAS

--- CERR_core/Contouring/BABS/cerrToNii.m
+++ CERR_core/Contouring/BABS/cerrToNii.m
@@ -5,26 +5,13 @@ function cerrToNii(scanNum,structNumV,niiOutDir,planC)
 
 indexS = planC{end};
 
-[affineOutM,~,voxSizV] = getPlanCAffineMat(planC, scanNum, 1);
-originV = affineOutM(1:3,4);
-
-
-%Assumes single scan, with passedScanDim '3D'.
-vol3M = double(planC{indexS.scan}(scanNum).scanArray) - planC{indexS.scan}(scanNum).scanInfo(1).CTOffset;
-scanFileName = fullfile(niiOutDir,'scan.nii');
-vol2nii(vol3M,affineOutM,originV,voxSizV,[],scanFileName);
-
-for strInd = 1:length(structNumV)
-    
-    mask3M = getStrMask(structNumV(strInd));
-    if ~isempty(mask3M)
-        maskDir = fullfile(niiOutDir,'Masks');
-        if ~exist(maskDir,'dir')
-            mkdir(maskDir)
-        end
-        maskFileName = [planC{indexS.structures}(structNumV(strInd)).structureName,'.nii'];
-        maskFileName = fullfile(maskDir,maskFileName);
-        vol2nii(mask3M,affineOutM,originV,voxSizV,[],maskFileName);
-    end
-    
+reorientFlag = 1; %realign image to RAS
+niiDataType = 'int16';
+if ismember(class(planC{indexS.scan}(scanNum)),{'single','double'})
+    niiDataType = 'double';
 end
+scanFileNameC = scan2imageOut(planC,scanNum,niiOutDir,reorientFlag,'nii',niiDataType);
+if ~isempty(structNumV)
+    maskFileNameC = mask2imageOut(planC,scanNum,structNumV,niiOutDir,reorientFlag,'nii');
+end
+
