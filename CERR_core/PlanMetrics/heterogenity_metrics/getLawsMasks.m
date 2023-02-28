9b664e2d795ac529ea6c17eadd96679406dad540
aditiiyer <iyera@mskcc.org>
2023-02-28 17:11:40 -0500

    Removed duplicate normalization factor

--- CERR_core/PlanMetrics/heterogenity_metrics/getLawsMasks.m
+++ CERR_core/PlanMetrics/heterogenity_metrics/getLawsMasks.m
@@ -22,25 +22,11 @@ E5   =  [-1,  -2,   0,   2,   1]; % GRadient: ReSpondS to RoW oR coLumn Step edg
 S5   =  [-1,   0,   2,   0,  -1]; % LOG: detectS SpotS
 R5   =  [ 1,  -4,   6,  -4,   1]; % GaboR: detectS RippLeS
 W5  =   [-1,   2,   0,  -2,   1];
- 
-if normFlag
-    %Normalization ensures average pixel in filtered image
-    %is as bright as the average pixel in the original image
-    
-    L3 = L3./sqrt(6);
-    E3 = E3./sqrt(2);
-    S3 = S3./sqrt(6);
-    L5 = L5./sqrt(70);
-    E5 = E5./sqrt(10);
-    S5 = S5./sqrt(6);
-    R5 = R5./sqrt(70);
-    W5 = W5./sqrt(10);
-end
 
-%% Normalize
 if normFlag
     %Normalization ensures average pixel in filtered image
     %is as bright as the average pixel in the original image
+
     L3 = L3./sqrt(6);
     E3 = E3./sqrt(2);
     S3 = S3./sqrt(6);
@@ -52,7 +38,7 @@ if normFlag
 end
 
 
-%% Create masks 
+%% Create masks
 
 if ~any(strcmp(type,{'3','5','all'}))
     %Return specified filter
@@ -69,16 +55,16 @@ else
         % 2-d (Length 3)
         lawsMasksS.L3E3 = L3'*E3;
         lawsMasksS.L3S3 = L3'*S3;
-        
+
         lawsMasksS.E3E3 = E3'*E3;
         lawsMasksS.E3L3 = E3'*L3;
         lawsMasksS.E3S3 = E3'*S3;
-        
+
         lawsMasksS.S3S3 = S3'*S3;
         lawsMasksS.S3L3 = S3'*L3;
         lawsMasksS.S3e3 = S3'*E3;
     end
-    
+
     if any(strcmp(type,{'5','all'})) && any(strcmp(direction,{'2d','all'}))
         % 2-d (Length 5)
         lawsMasksS.L5L5 = L5'*L5;
@@ -86,32 +72,32 @@ else
         lawsMasksS.L5S5 = L5'*S5;
         lawsMasksS.L5R5 = L5'*R5;
         lawsMasksS.L5W5 = L5'*W5;
-        
+
         lawsMasksS.E5E5 = E5'*E5;
         lawsMasksS.E5L5 = E5'*L5;
         lawsMasksS.E5S5 = E5'*S5;
         lawsMasksS.E5R5 = E5'*R5;
         lawsMasksS.E5W5 = E5'*W5;
-        
+
         lawsMasksS.S5S5 = S5'*S5;
         lawsMasksS.S5L5 = S5'*L5;
         lawsMasksS.S5e5 = S5'*E5;
         lawsMasksS.S5R5 = S5'*R5;
         lawsMasksS.S5W5 = S5'*W5;
-        
+
         lawsMasksS.R5R5 = R5'*R5;
         lawsMasksS.R5S5 = R5'*S5;
         lawsMasksS.R5L5 = R5'*L5;
         lawsMasksS.R5e5 = R5'*E5;
         lawsMasksS.R5W5 = R5'*W5;
-        
+
         lawsMasksS.W5W5 = W5'*W5;
         lawsMasksS.W5R5 = W5'*R5;
         lawsMasksS.W5S5 = W5'*S5;
         lawsMasksS.W5L5 = W5'*L5;
         lawsMasksS.W5e5 = W5'*E5;
     end
-    
+
     if any(strcmp(type,{'3','all'})) && any(strcmp(direction,{'3d','all'}))
         % 3-d (Length 3)
         lawsMasksS.E3E3E3 = get3dLawsText(E3,E3,E3);
@@ -142,7 +128,7 @@ else
         lawsMasksS.S3S3L3 = get3dLawsText(S3,S3,L3);
         lawsMasksS.S3S3S3 = get3dLawsText(S3,S3,S3);
     end
-    
+
     if any(strcmp(type,{'5','all'})) && any(strcmp(direction,{'3d','all'}))
         % 3-d (Length 5)
         lawsMasksS.L5L5L5 = get3dLawsText(L5,L5,L5);
@@ -271,7 +257,7 @@ else
         lawsMasksS.W5W5R5 = get3dLawsText(W5,W5,R5);
         lawsMasksS.W5W5W5 = get3dLawsText(W5,W5,W5);
     end
-    
+
 end
 
 function conved3M = get3dLawsText(x,y,z)
