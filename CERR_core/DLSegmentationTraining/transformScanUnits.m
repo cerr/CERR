2dcb21897c31ee66a58105d0ae4996745b4aa778
aditiiyer <iyera@mskcc.org>
2023-06-21 17:13:19 -0400

    Transform image units

--- CERR_core/DLSegmentationTraining/transformScanUnits.m
+++ CERR_core/DLSegmentationTraining/transformScanUnits.m
@@ -18,14 +18,19 @@ if ~isempty(imageUnits)
     switch(imageUnits)
 
         case '10^-3 mm^2/s'
-            currUnits =  planC{indexS.scan}(scanNumV(scanIdx)).scanInfo(1).philipsImageUnits;
-            sepIdx = strfind(currUnits,' ');
-            val = currUnits(1:sepIdx-1);
-            units = currUnits(sepIdx+1:end);
-            if ~strcmp(units,'mm^2/s')
-                error('Invalid intensity units %s. Expected units: mm^2/s',units);
+            if isfield(planC{indexS.scan}(scanIdx).scanInfo(1),'philipsImageUnits')
+                currUnits =  planC{indexS.scan}(scanIdx).scanInfo(1).philipsImageUnits;
+                sepIdx = strfind(currUnits,' ');
+                val = str2num(currUnits(1:sepIdx-1));
+                units = currUnits(sepIdx+1:end);
+                if ~strcmp(units,'mm^2/s')
+                    error('Invalid intensity units %s. Expected units: mm^2/s',units);
+                else
+                    scaleFactor = 10^-3/val;
+                end
             else
-                scaleFactor = 10^-3/val;
+                warning('Image units unknown. Skipping transformation.Expected ''10^-3 mm^2/s''.')
+                scaleFactor = 1;
             end
             scan3M = scan3M .* scaleFactor;
 
